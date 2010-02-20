49f7961a7bd9138e0e1a6ebc3ca12b2bbacab9d4
Nathanael Jones <nathanael.jones@gmail.com>
2010-02-20 11:22:21 -0500

    VPPTest
    Former-commit-id: 7315b6ee64874777bda2a65fa4192508cd55f260

---
+++ VPPTest/Images/Default.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SampleProject
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
