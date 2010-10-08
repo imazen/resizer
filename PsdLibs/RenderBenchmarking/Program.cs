6f8297341981612a2f2a64deeeab2b9f595592f2
Nathanael Jones <nathanael.jones@gmail.com>
2010-10-08 12:36:29 -0400

    Added basic benchmark project.

---
+++ PsdLibs/RenderBenchmarking/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PsdRenderer;
+using System.Diagnostics;
+using System.Collections.Specialized;
+
+namespace RenderBenchmarking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Debug.Write("Loop " + i);
+                PsdProvider.getStream("~/1001.psd", new NameValueCollection());
+            }
+        }
+    }
+}
