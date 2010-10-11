3cc0e688508b1e97210eeac4e3e5151330adafe1
Nathanael Jones <nathanael.jones@gmail.com>
2010-10-11 14:27:09 -0400

    Added RenderMill selector and averaging code to RenderBenchmarking. Added docs to memcachedfile.
    Former-commit-id: aeb75c3e3bd0f1f5d8642ae6215f9963d8549ce9

--- PsdLibs/RenderBenchmarking/Program.cs
+++ PsdLibs/RenderBenchmarking/Program.cs
@@ -12,16 +12,22 @@ namespace RenderBenchmarking
     {
         static void Main(string[] args)
         {
+            NameValueCollection qs = new NameValueCollection();
+            qs.Add("renderer", "GraphicsMill");
+            long total = 0;
             Stopwatch sw = new Stopwatch();
             for (int i = 0; i < 50; i++)
             {
                 Debug.Write("Loop " + i);
                 sw.Reset();
                 sw.Start();
-                PsdProvider.getStream("~/1001.psd", new NameValueCollection());
+                PsdProvider.getStream("~/1001.psd", qs);
                 sw.Stop();
+                total += sw.ElapsedMilliseconds;
                 Console.WriteLine("Loop " + i + " took " + sw.ElapsedMilliseconds + "ms");
             }
+            Console.WriteLine("Average time is " + total/50 + "ms");
+            Console.ReadKey();
         }
     }
 }
