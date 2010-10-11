9195485aa3beb55108e576d439cb0241a085516c
Nathanael Jones <nathanael.jones@gmail.com>
2010-10-11 13:24:54 -0400

    Added timing statements to the console in Render Benchmarking
    Former-commit-id: 9fb5a57526992d1c0a51931cbb987158466a89fb

--- PsdLibs/RenderBenchmarking/Program.cs
+++ PsdLibs/RenderBenchmarking/Program.cs
@@ -12,10 +12,15 @@ namespace RenderBenchmarking
     {
         static void Main(string[] args)
         {
+            Stopwatch sw = new Stopwatch();
             for (int i = 0; i < 50; i++)
             {
                 Debug.Write("Loop " + i);
+                sw.Reset();
+                sw.Start();
                 PsdProvider.getStream("~/1001.psd", new NameValueCollection());
+                sw.Stop();
+                Console.WriteLine("Loop " + i + " took " + sw.ElapsedMilliseconds + "ms");
             }
         }
     }
