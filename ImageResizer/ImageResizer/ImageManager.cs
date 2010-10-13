3efdfec280b1387209a4ae2d0d597b9926e0e594
Nathanael Jones <nathanael.jones@gmail.com>
2010-10-13 17:20:31 -0400

    Added Size GetFinalSize() methods to ImageManager.cs for determining the resulting size of an image.
    Former-commit-id: dfcee37ed1352d04249406eebcbbb5d29b3d4289

--- ImageResizer/ImageResizer/ImageManager.cs
+++ ImageResizer/ImageResizer/ImageManager.cs
@@ -586,8 +586,78 @@ namespace fbs.ImageResizer
 
         }
 
+        /// <summary>
+        /// Gets the final size of an image
+        /// </summary>
+        /// <returns></returns>
+        public virtual Size GetFinalSize(SizeF originalSize, NameValueCollection q)
+        {
+            return GetFinalSize(originalSize, new ResizeSettings(q), new ImageSettings(q), new ImageFilter(q), new ImageOutputSettings(ImageFormat.Jpeg, q), new WatermarkSettings(q));
+        }
+
+        /// <summary>
+        /// Doesn't support rotation or flipping. Translate a point on the original bitmap to a point on the new bitmap. If the original point no longer exists, returns Empty
+        /// </summary>
+        /// <returns></returns>
+        public virtual Size GetFinalSize( SizeF originalSize, ResizeSettings resize, ImageSettings opts, ImageFilter adjustments, ImageOutputSettings output, WatermarkSettings watermark)
+        {
+            if (watermark != null) watermark.ModifySettings(resize, opts, adjustments, output);
 
 
+            ResizeSettings.ImageSizingData size = resize.CalculateSizingData(originalSize, new SizeF((float)DiskCache.GetMaxWidth(), (float)DiskCache.GetMaxHeight()));
+
+            //Calculate required space for everything
+            PointF[] all = size.targetArea;
+
+            //Add required space for border and padding
+            if (opts.borderWidth > 0) all = PolygonMath.InflatePoly(all, opts.borderWidth);
+            if (opts.paddingWidth > 0) all = PolygonMath.InflatePoly(all, opts.paddingWidth);
+
+            //shadow is trickier
+            if (opts.shadowWidth > 0) all = PolygonMath.InflatePoly(all, new float[]{
+                Math.Max(0, opts.shadowWidth - opts.shadowOffset.Y),
+                Math.Max(0, opts.shadowWidth + opts.shadowOffset.X),
+                Math.Max(0, opts.shadowWidth + opts.shadowOffset.Y),
+                Math.Max(0, opts.shadowWidth - opts.shadowOffset.X)
+            });
+
+            //Find how much we need to move imageArea (and imageTarget) so that all is at 0,0.
+            PointF shadowBorderOffset = PolygonMath.GetBoundingBox(all).Location;
+            shadowBorderOffset.X *= -1;
+            shadowBorderOffset.Y *= -1;
+
+
+            //Rebase things so we are starting at 0,0;
+            size.imageTarget = PolygonMath.MovePoly(size.imageTarget, shadowBorderOffset);
+            size.targetArea = PolygonMath.MovePoly(size.targetArea, shadowBorderOffset);
+            //Inflate for padding
+            if (opts.paddingWidth > 0) size.targetArea = PolygonMath.InflatePoly(size.targetArea, opts.paddingWidth);
+
+
+            //Find the size of the new image
+            RectangleF box = PolygonMath.GetBoundingBox(all);
+
+            //Create new bitmap using calculated size. 
+            //OLD idea: Potential problem, rounding max crop or leave space. However, floating-point values should only occur on rotations.
+            //4-23-09, FALSE, floating point values can occur anytime scaling takes place. 
+            //Fixed by using Floor instead of Round
+            if (PolygonMath.GetBoundingBox(size.imageTarget).Size.Equals(box))
+            {
+                //The image is taking the entire space. Round down, as System.Drawing does.
+                //June 3: added Math.Max(1,) to prevent Invalid Parameter error on < 1px images. Also need to fix in ResizeSettings.. otherwise
+                //the image data won't be put in the right place either.
+                return new Size((int)Math.Max(1, Math.Floor(box.Width)), (int)Math.Max(1, Math.Floor(box.Height)));
+
+            }
+            else
+            {
+                //June 3: added Math.Max(1,) to prevent Invalid Parameter error on < 1px images.
+                //It isn't taking up all the space - Space around the image is expected. Leaving normal rounding in place - flooring would do as much harm as good on average.
+                return new Size((int)Math.Max(1, Math.Round(box.Width)), (int)Math.Max(1, Math.Round(box.Height)));
+            }
+        }
+
+       
 
 
         /// <summary>
