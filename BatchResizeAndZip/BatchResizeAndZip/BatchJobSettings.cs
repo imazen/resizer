cb6ce549b700c1af64d10b0abd808e70573abebf
Nathanael Jones <nathanael.jones@gmail.com>
2011-02-27 21:04:39 +0100

    Added a DLL that wraps the resizer and provides both sync and async batch resizing and zip archive creation.
    Former-commit-id: b747b9e5499f7eafb12d67f011a20484c60503f7

---
+++ BatchResizeAndZip/BatchResizeAndZip/BatchJobSettings.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using Ionic.Zip;
+using System.Text.RegularExpressions;
+using System.Collections.Specialized;
+
+namespace fbs.ImageResizer.BatchZipper
+{
+    public delegate void ItemCallback(ItemEventArgs e);
+
+    public delegate void JobCallback(JobEventArgs e);
+
+    /// <summary>
+    /// Represents a file to resize/re-encode, the resize/encoding settings
+    /// </summary>
+    public class BatchResizeItem
+    {
+        /// <summary>
+        /// Represents a file to resize/re-encode, the resize/encoding settings
+        /// </summary>
+        /// <param name="physicalPath">Filesystem path, such as C:\Web\Project\Image\file.jpg</param>
+        /// <param name="targetFilename">The target filename to use in the compressed folder. If left null, the filename of physicalPath will be used.
+        /// Should not include the file extension, that will be assigned based on resizeQuerystring and the original extension.</param>
+        /// <param name="resizeQuerystring">The resize/crop settings applied to the file, ex. ?width=100&height=100&crop=auto&format=png</param>
+         public BatchResizeItem(string physicalPath, string targetFilename, string resizeQuerystring)
+        {
+            this.physicalPath = physicalPath;
+            this.resizeQuerystring = resizeQuerystring;
+            this.targetFilename = targetFilename;
+        }
+        /// <summary>
+        /// Represents a file to resize/re-encode, the resize/encoding settings
+        /// </summary>
+        /// <param name="physicalPath">Filesystem path, such as C:\Web\Project\Image\file.jpg</param>
+        /// <param name="targetFilename">The target filename to use in the compressed folder. If left null, the filename of physicalPath will be used.
+        /// Should not include the file extension, that will be assigned based on resizeQuerystring and the original extension.</param>
+        /// <param name="resizeQuerystring">The resize/crop settings applied to the file, ex. ?width=100&height=100&crop=auto&format=png</param>
+        /// <param name="mutable">If false, an InvalidOperationException exception will be thrown when you attempt to modify the instance. Set this to true when building a job.</param>
+        protected internal BatchResizeItem(string physicalPath, string targetFilename, string resizeQuerystring, bool mutable)
+        {
+            this.physicalPath = physicalPath;
+            this.resizeQuerystring = resizeQuerystring;
+            this.targetFilename = targetFilename;
+            this.mutable = mutable;
+        }
+
+        private string physicalPath;
+        /// <summary>
+        /// Filesystem path, such as C:\Web\Project\Image\file.jpg. Throws an InvalidOperationException if you attempt to modify an immutable instance. 
+        /// </summary>
+        public string PhysicalPath
+        {
+            get { return physicalPath; }
+            set { ThowIfInvalid(); physicalPath = value; }
+        }
+
+
+        private string resizeQuerystring;
+        /// <summary>
+        /// The resize/crop settings applied to the file, ex. ?width=100&height=100&crop=auto&format=png. Throws an InvalidOperationException if you attempt to modify an immutable instance. 
+        /// </summary>
+        public string ResizeQuerystring
+        {
+            get { return resizeQuerystring; }
+            set { ThowIfInvalid(); resizeQuerystring = value; }
+        }
+
+
+        private string targetFilename;
+        /// <summary>
+        /// The target filename to use in the compressed folder. If left null, the filename of physicalPath will be used.
+        /// Should not include the file extension, that will be assigned based on resizeQuerystring and the original extension.
+        /// Throws an InvalidOperationException if you attempt to modify an immutable instance. 
+        /// </summary>
+        public string TargetFilename
+        {
+            get { return targetFilename; }
+            set { ThowIfInvalid(); targetFilename = value; }
+        }
+
+        protected void ThowIfInvalid()
+        {
+            if (!mutable) throw new InvalidOperationException("This instance is marked as immutable.");
+        }
+
+        protected bool mutable = true;
+
+        /// <summary>
+        /// Copies the class, useful for preventing threading issues. Lightweight, only 4 pointers plus class instance.
+        /// </summary>
+        /// <returns></returns>
+        public BatchResizeItem Copy()
+        {
+            return new BatchResizeItem(PhysicalPath, TargetFilename, ResizeQuerystring, mutable);
+        }
+        /// <summary>
+        /// Blocks any future changes to the instance, throwing an InvalidOperationException.
+        /// </summary>
+        public void SetImmutable()
+        {
+            mutable = false;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "{PhysicalPath=\"" + PhysicalPath + "\", TargetFilename=\"" + TargetFilename + "\", ResizeQuerystring=\"" + ResizeQuerystring + "\", mutable=" + mutable + "}";
+        }
+    }
+
+    /// <summary>
+    /// Use this to configure the a resize job. After you start the job, do not modify this instance.
+    /// </summary>
+    public class BatchResizeSettings
+    {
+        /// <summary>
+        /// Use this to configure a batch resize job. Mutable.
+        /// Remember to assign at least a JobEvent handler so you know the fate of the job.
+        /// </summary>
+        /// <param name="jobId">The job ID, can be generated with Guid.NewGuid() </param>
+        /// <param name="destinationFile">The physical path to the destination archive</param>
+        /// <param name="files">A List<BatchResizeItem> of items to to resize and place in the folder.</param>
+        public BatchResizeSettings(string destinationFile, Guid jobId, IList<BatchResizeItem> files)
+        {
+            this.jobId = jobId;
+            this.destinationFile = destinationFile;
+            this.files = files;
+        }
+        //Input settings
+        public Guid jobId;
+        public IList<BatchResizeItem> files;
+        public string destinationFile;
+
+        //Progress/failure callbacks
+        /// <summary>
+        /// Fired when a file is successfully written to the zip file, or if an item fails to be added to the zip file.
+        /// Will execute on a thread pool thread. Catch all your exeptions, or they will cause the jobe to fail.
+        /// Set e.Cancel to cancel the job. 
+        /// Uses the same thread the job is processing on - I/O bound tasks in handlers should be async if possible.
+        /// </summary>
+        public event ItemCallback ItemEvent; //Called when a item is sucessfully written to the zip file, or when it fails
+        /// <summary>
+        /// Fires when the Zip file has successfully been written to disk, when the job fails.
+        /// Will execute on a thread pool thread. Catch all your exeptions, or they will cause the asp.net proccess to restart!
+        /// </summary>
+        public event JobCallback JobEvent;
+
+        protected internal void FireItemEvent(ItemEventArgs e)
+        {
+            if (ItemEvent != null) ItemEvent(e);
+        }
+        protected internal void FireJobEvent(JobEventArgs e)
+        {
+            if (JobEvent != null) JobEvent(e);
+        }
+
+        /// <summary>
+        /// Loops through all files, assigning targetFilenames if they are null,
+        /// and eliminating duplicate names by adding _1, _2, etc.
+        /// Also normalizes filenames for use in zip folder.
+        /// Used internally. May be used externally also if calling code wishes to to know the final file names.
+        /// </summary>
+        public void FixDuplicateFilenames(string prefix = "_")
+        {
+            HashSet<string> names = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            foreach (BatchResizeItem i in files)
+            {
+                //Assign default if null
+                if (i.TargetFilename == null) i.TargetFilename = System.IO.Path.GetFileNameWithoutExtension(i.PhysicalPath);
+
+                //See if it is a duplicate
+                if (names.Contains(i.TargetFilename))
+                {
+                    //Generate a unique filename by appending a number.
+                    int index = 1;
+                    string next = i.TargetFilename + prefix + index;
+                    while (names.Contains(next))
+                    {
+                        index++;
+                        next = i.TargetFilename + prefix + index;
+                    }
+                    i.TargetFilename = next;
+                }
+
+                //Normalize for zip usage
+                i.TargetFilename = NormalizePathForUseInZipFile(i.TargetFilename);
+
+                //Add to names set, so we can verify future filename uniqueness.
+                names.Add(i.TargetFilename);
+
+            }
+        }
+        /// <summary>
+        /// Utility routine for transforming path names from filesystem format (on Windows that means backslashes) to
+        /// a format suitable for use within zipfiles. This means trimming the volume letter and colon (if any) And
+        /// swapping backslashes for forward slashes.
+        /// </summary>
+        /// <param name="pathName">source path.</param>
+        /// <returns>transformed path</returns>
+        public static string NormalizePathForUseInZipFile(string pathName)
+        {
+            // boundary case
+            if (String.IsNullOrEmpty(pathName)) return pathName;
+
+            // trim volume if necessary
+            if ((pathName.Length >= 2) && ((pathName[1] == ':') && (pathName[2] == '\\')))
+                pathName = pathName.Substring(3);
+
+            // swap slashes
+            pathName = pathName.Replace('\\', '/');
+
+            // trim all leading slashes
+            while (pathName.StartsWith("/")) pathName = pathName.Substring(1);
+
+            return SimplifyFwdSlashPath(pathName);
+        }
+
+        private static string SimplifyFwdSlashPath(string path)
+        {
+            if (path.StartsWith("./")) path = path.Substring(2);
+            path = path.Replace("/./", "/");
+            // Replace foo/anything/../bar with foo/bar
+            var re = new System.Text.RegularExpressions.Regex(@"^(.*/)?([^/\\.]+/\\.\\./)(.+)$");
+            path = re.Replace(path, "$1$3");
+            return path;
+        }
+
+
+        /// <summary>
+        /// Adds the correct, final extension  to each target file name.Internal use only.
+        /// </summary>
+        internal void AppendFinalExtensions()
+        {
+            foreach (BatchResizeItem i in files)
+            {
+                string originalName = System.IO.Path.GetFileName(i.PhysicalPath);
+                //Can we resize it?
+                if (fbs.ImageResizer.ImageOutputSettings.IsAcceptedImageType(originalName))
+                {
+                    //Add the correct (possibly changed) file extension.
+                    i.TargetFilename += "." + new fbs.ImageResizer.ImageOutputSettings(new yrl(originalName + i.ResizeQuerystring)).GetFinalExtension();
+                }
+                else
+                {
+                    //Just keep the same extension.
+                    i.TargetFilename += System.IO.Path.GetExtension(originalName); //(includes the ".")
+                }
+
+            }
+        }
+
+
+        internal void MarkItemsImmutable()
+        {
+            foreach (BatchResizeItem i in files)
+            {
+                i.SetImmutable();
+            }
+        }
+    }
+}
---
+++ BatchResizeAndZip/BatchResizeAndZip/BatchResizeWorker.cs
@@ -0,0 +1,335 @@
+﻿
+
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using Ionic.Zip;
+using System.Text.RegularExpressions;
+using System.Collections.Specialized;
+using System.Diagnostics;
+
+namespace fbs.ImageResizer.BatchZipper
+{
+
+    /// <summary>
+    /// Synchronous worker class, not thread safe. Should be executed and touched by a single thread.
+    /// </summary>
+    public class BatchResizeWorker
+    {
+        /// <summary>
+        /// Creates a new worker instance. Do not modify the settings while executing the Work() method.
+        /// </summary>
+        /// <param name="s"></param>
+        public BatchResizeWorker(BatchResizeSettings s){
+            this.s = s;
+        }
+        /// <summary>
+        /// Stores the job settings. 
+        /// </summary>
+        protected BatchResizeSettings s;
+        /// <summary>
+        /// Used to map ZIP item names to instances. Required do to DotNetZipLib's callback system.
+        /// </summary>
+        protected Dictionary<string,BatchResizeItem> items;
+        /// <summary>
+        /// Used to store the results of each resized item.
+        /// </summary>
+        protected List<ItemResult> results = null;
+        protected Stopwatch jobTimer;
+        /// <summary>
+        /// When the job was started
+        /// </summary>
+        protected DateTime startedAt = DateTime.MaxValue;
+
+        /// <summary>
+        /// When the job was finished
+        /// </summary>
+        protected DateTime finishedAt = DateTime.MaxValue;
+        /// <summary>
+        /// Executes the job, and returns when it is complete or has failed.
+        /// Exceptions are delivered by events registered in the BatchResizeSetting instance - 
+        /// throws no execptions, unless you throw an execption inside a job event handler.
+        /// </summary>
+        public void Work(){
+            Work(s);
+        }
+
+        /// <summary>
+        /// Not for external use. Assumes 'state' is a BatchResizeSettings instance, and stores in in a class member.
+        /// Next, it completes (or fails at) the job. Throws exceptions only if (a) state is not a BatchResizeSettings instance, or (b) an exception is thrown by and handler of the JobEvent (Failed) event.
+        /// </summary>
+        /// <param name="state"></param>
+        protected internal void Work(object state)
+        {
+            s = (BatchResizeSettings)state;
+            try
+            {
+                //Time the job.
+                jobTimer = new Stopwatch();
+                jobTimer.Start();
+
+                //1) Eliminate duplicate target filenames, sanitize, and add appropriate extensions, then mark all items as immutable
+                s.FixDuplicateFilenames();
+                s.AppendFinalExtensions();
+                s.MarkItemsImmutable();
+
+                //2) Build dictionary based on target filenames.
+                items = BuildDict(s.files);
+
+                //3) Verify destination folder exists, otherwise create it. (early detection of security failures)
+                CreateDestinationFolder();
+
+                //4) Open zip file (writes to a temporary location, then moves the file when it is done)
+                using (Ionic.Zip.ZipFile z = new Ionic.Zip.ZipFile(s.destinationFile))
+                {
+                    //Compress for speed.
+                    z.CompressionLevel = Ionic.Zlib.CompressionLevel.BestSpeed;
+                    //Fire events instead of throwing exceptions
+                    z.ZipErrorAction = ZipErrorAction.InvokeErrorEvent;
+                    z.ZipError +=new EventHandler<ZipErrorEventArgs>(z_ZipError);
+                    z.SaveProgress +=new EventHandler<SaveProgressEventArgs>(z_SaveProgress);
+                    //5) Create a list to store item resize/compression results
+                    results = new List<ItemResult>();
+
+                    
+                    //6) Queue the files that will be included in the archive, specifying a callback for items
+                    foreach (BatchResizeItem i in s.files){
+                        try{
+                            ZipEntry ze = z.AddEntry(i.TargetFilename, new WriteDelegate(WriteItemCallback));
+                            //Don't try to compress files we have already resized, it won't help, just slow things down and possibly use more space.
+                            if (wouldResize(i)) ze.CompressionMethod = CompressionMethod.None;
+                            else
+                            {
+                                //Set the times
+                                //ze.SetEntryTimes(System.IO.File.GetLastWriteTimeUtc(i.PhysicalPath),
+                                //    System.IO.File.GetLastAccessTimeUtc(i.PhysicalPath),
+                                //    System.IO.File.GetCreationTimeUtc(i.PhysicalPath));
+                                //Nevermind, not desired behavior.
+                            }
+                        }catch (Exception ex){
+                            ItemFailed(i,ex);
+                        }
+                    }
+                    //7) Begin proccessing the queue.
+                    //As each file is written to the archive, a callback is invoked which actually provides the resized stream. 
+                    z.Save();
+                    
+
+                } //8) Close zip file
+                
+                jobTimer.Stop();
+
+                //9) Fire the event that says "We're done!"
+                JobCompleted(s);
+            }
+            catch (Exception ex)
+            {
+                //Admit we failed to whoever is listening.
+                JobFailed(s,ex);
+            }
+        }
+        /// <summary>
+        /// Returns true if the specified item is going to be resized.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public bool wouldResize(BatchResizeItem i)
+        {
+            //Can we resize it?
+            bool resize = ImageOutputSettings.IsAcceptedImageType(System.IO.Path.GetFileName(i.PhysicalPath));
+            //Are we doing anything to it?
+            if (String.IsNullOrEmpty(i.ResizeQuerystring)) resize = false;
+            return resize;
+        }
+        /// <summary>
+        /// Called from ZipFile.Save as each queued entry is processed.
+        /// </summary>
+        /// <param name="entryName"></param>
+        /// <param name="stream"></param>
+        protected void WriteItemCallback(string entryName, System.IO.Stream stream)
+        {
+            BatchResizeItem i = items[entryName];
+            if (wouldResize(i))
+            {
+               
+                NameValueCollection queryString = new yrl(i.ResizeQuerystring).QueryString;
+                //Resize the image based on the querystring
+                using (System.Drawing.Bitmap resized = fbs.ImageResizer.ImageManager.getBestInstance().BuildImage(i.PhysicalPath, queryString))
+                {
+                    //Determines output format, includes code for saving in a variety of formats.
+                    ImageOutputSettings ios = new ImageOutputSettings(ImageOutputSettings.GetImageFormatFromPhysicalPath(i.PhysicalPath), queryString);
+                    //Save, assuming we can't seek within the zip output stream.
+                    ios.SaveImageToNonSeekableStream(stream, resized);
+                }
+                return; //We're done!
+                
+            }
+
+
+            //For non-resizeable items, Just copy the stream.
+            using (System.IO.FileStream s = System.IO.File.OpenRead(i.PhysicalPath)) CopyStreamTo(s, stream);
+        }
+        /// <summary>
+        /// Copies an read stream to a write stream.
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="dest"></param>
+        public static void CopyStreamTo(System.IO.Stream src, System.IO.Stream dest)
+        {
+            int size = (src.CanSeek) ? Math.Min((int)(src.Length - src.Position), 0x2000) : 0x2000;
+            byte[] buffer = new byte[size];
+            int n;
+            do
+            {
+                n = src.Read(buffer, 0, buffer.Length);
+                dest.Write(buffer, 0, n);
+            } while (n != 0);
+        }
+        /// <summary>
+        /// Stores the results of the Zip file saving betweent he time Saving_Completed fires, and we close the ZipFile instance and fire our own event
+        /// </summary>
+        protected SaveProgressEventArgs savingCompletedEventArgs = null;
+
+        protected void  z_SaveProgress(object sender, SaveProgressEventArgs e)
+        {
+            if (e.EventType == ZipProgressEventType.Saving_Completed) savingCompletedEventArgs = e;
+            if (e.EventType == ZipProgressEventType.Saving_AfterWriteEntry) ItemCompleted(e);
+        }
+        protected void z_ZipError(object sender, ZipErrorEventArgs e)
+        {
+            //We can only deal with item errors here. We ignore job errors, they will throw an execption that bubbles up through Save()
+            if (e.CurrentEntry != null) ItemFailed(e);
+        }
+
+        private void JobCompleted(BatchResizeSettings s)
+        {
+            JobEventArgs args = new JobEventArgs(s.jobId, new JobResult(results, true,null, GetJobStats()));
+            s.FireJobEvent(args);
+        }
+
+        private void JobFailed(BatchResizeSettings s,Exception ex)
+        {
+            JobEventArgs args = new JobEventArgs(s.jobId, new JobResult(results,false, ex, GetJobStats()));
+            s.FireJobEvent(args);
+        }
+
+        /// <summary>
+        /// How many items have been successfully resized and zipped
+        /// </summary>
+        protected int successfulItems = 0;
+        /// <summary>
+        /// How many items failed to be resized and zipped
+        /// </summary>
+        protected int failedItems = 0;
+
+        /// <summary>
+        /// Returns a snapshots of the job statistics
+        /// </summary>
+        /// <returns></returns>
+        public JobStats GetJobStats()
+        {
+            return new JobStats(items.Count, successfulItems, failedItems, jobTimer != null ? jobTimer.ElapsedMilliseconds : 0);
+        }
+
+
+        private void ItemCompleted(SaveProgressEventArgs e)
+        {
+            successfulItems++;
+
+            BatchResizeItem i = items[e.CurrentEntry.FileName];
+
+            //Fire off the event again..
+            ItemEventArgs args = new ItemEventArgs(s.jobId, new ItemResult(i, true, null), GetJobStats());
+            s.FireItemEvent(args);
+
+            //Store for later
+            results.Add(args.Result);
+
+            //If an event handler wants to cancel the job, do so.
+            if (args.Cancel) e.Cancel = true;
+        }
+
+        /// <summary>
+        /// Called when an error occurs during Save() (Whitch executes WriteItemCallback)
+        /// </summary>
+        /// <param name="e"></param>
+        private void ItemFailed(ZipErrorEventArgs e)
+        {
+            failedItems++;
+
+            e.CurrentEntry.ZipErrorAction = ZipErrorAction.Skip; //Prevent the item from crashing the job.
+
+            BatchResizeItem i = items[e.CurrentEntry.FileName];
+            Exception ex = e.Exception;
+            //Fire off the event again..
+            ItemEventArgs args = new ItemEventArgs(s.jobId, new ItemResult(i,false, ex), GetJobStats());
+            s.FireItemEvent(args);
+
+            //Store for later
+            results.Add(args.Result);
+
+            //If an event handler wants to cancel the job, do so.
+            if (args.Cancel) e.Cancel = true;
+        }
+        /// <summary>
+        /// Called when ZipFile.AddEntry fails. (Most errors should NOT happen here).
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="ex"></param>
+        private void ItemFailed(BatchResizeItem i, Exception ex)
+        {
+            failedItems++;
+
+            //Fire off the event
+            ItemEventArgs args = new ItemEventArgs(s.jobId, new ItemResult(i,false, ex), GetJobStats());
+            s.FireItemEvent(args);
+
+            //Store for later
+            results.Add(args.Result);
+
+            //If a cancel is requested, fire an execption for Work to catch.
+            throw new JobCancelledException("An event handler requested that the job be cancelled. The event handler was sent notification " +
+                "that an item (\"" + i.TargetFilename +"\") could not be added to the zip file.",ex);
+        }
+
+
+
+  
+        /// <summary>
+        /// If the parent folder of the destination archive is missing, this method will create it.
+        /// </summary>
+        protected void CreateDestinationFolder(){
+            string dir = System.IO.Path.GetDirectoryName(s.destinationFile);
+            if (!System.IO.Directory.Exists(dir)) System.IO.Directory.CreateDirectory(dir);
+        }
+
+        /// <summary>
+        /// Builds a dictionary from a collection of BatchResizeItems, using .targetFilename as the key.
+        /// </summary>
+        /// <param name="files"></param>
+        /// <returns></returns>
+        public Dictionary<string,BatchResizeItem> BuildDict(IEnumerable<BatchResizeItem> files){
+            Dictionary<string,BatchResizeItem> d = new Dictionary<string,BatchResizeItem>(StringComparer.OrdinalIgnoreCase);
+            foreach(BatchResizeItem i in files) d.Add(i.TargetFilename,i);
+            return d;
+        }
+
+        
+    }
+    /// <summary>
+    /// Handles the threading of a batch resize procedure. Use the static method to queue a resize on a thread pool thread
+    /// </summary>
+    public class BatchResizeManager
+    {
+        //Can initiate resizes, specifying a callback for error and progress reporting
+        /// <summary>
+        /// Begins a batch resize operation on a background thread. If the ASP.NET process recycles, or the server reboots, the process will be aborted.
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        public static void BeginBatchResize(BatchResizeSettings s)
+        {
+            ThreadPool.QueueUserWorkItem(new BatchResizeWorker(s).Work, s);
+        }
+    }
+}
