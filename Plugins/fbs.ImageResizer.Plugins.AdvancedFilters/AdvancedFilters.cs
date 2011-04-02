836f73f53a27130c872534e24a8955d963f5111d
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-02 19:34:42 +0200

    Started work on AdvancedFilters plugin and SeamCarving plugin

---
+++ Plugins/fbs.ImageResizer.Plugins.AdvancedFilters/AdvancedFilters.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using fbs.ImageResizer.Resizing;
+namespace fbs.ImageResizer.Plugins.AdvancedFilters {
+    public class AdvancedFilters:ImageBuilderExtension, IPlugin {
+
+    }
+}
