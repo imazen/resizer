24057ec8dad1b4e9674b45e78b9be1169223579f
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-04 17:42:24 +0200

    Forgot to commit part of the diagnostic plugin..
    Former-commit-id: 817db7e4625c1af60bee7b198eabfa14b0458640

---
+++ Core/Plugins/Diagnostic/DiagnosticPageHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using fbs.ImageResizer.Configuration;
+using fbs.ImageResizer.Configuration.Issues;
+using System.Web.Hosting;
+using System.Reflection;
+
+namespace fbs.ImageResizer.Plugins.Diagnostic {
+    public class DiagnosticPageHandler : IHttpHandler {
+        Config c;
+        public DiagnosticPageHandler(Config c) {
+            this.c = c;
+        }
+
+        public bool IsReusable {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context) {
+            context.Response.StatusCode = 200;
+            context.Response.ContentType = "text/plain";
+            context.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            context.Response.Write(GenerateOutput(context, c));
+        }
+
+        public string GenerateOutput(HttpContext context, Config c) {
+            List<IIssue> issues = new List<IIssue>(c.AllIssues.GetIssues());
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Image resizier diagnostic sheet\t\t" + DateTime.UtcNow.ToString() + "\n");
+            sb.AppendLine(issues.Count + " Issues detected:\n");
+            foreach (IIssue i in issues)
+                sb.AppendLine(i.Source + "(" + i.Severity.ToString() + "):\t" + i.Summary  +
+                    ("\n" + i.Details).Replace("\n","\n\t\t\t") + "\n");
+
+            sb.AppendLine("\nRegistered plugins:\n");
+            foreach (IPlugin p in c.Plugins.AllPlugins)
+                sb.AppendLine(p.ToString());
+
+            sb.AppendLine("\nConfiguration:\n");
+            sb.AppendLine(c.getConfigXml().ToString());
+
+            //Echo server assembly, iis version, OS version, and CLR version.
+            sb.AppendLine("\nEnvironment information:\n");
+            string iis = context.Request.ServerVariables["SERVER_SOFTWARE"];
+            if (!string.IsNullOrEmpty(iis)) iis += " on ";
+            sb.AppendLine("Running " + iis +
+                System.Environment.OSVersion.ToString() + " and CLR " +
+                System.Environment.Version.ToString());
+            sb.AppendLine("Executing assembly: " + System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+            sb.AppendLine("IntegratedPipeline=" + (HttpRuntime.UsingIntegratedPipeline).ToString());
+
+
+            sb.AppendLine("\nLoaded assemblies:\n");
+            Assembly[] asms = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (Assembly a in asms) {
+                sb.AppendLine(a.FullName);
+            }
+            return sb.ToString();
+
+        }
+    }
+}
