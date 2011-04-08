d89386ed59fe6737cfcb9fb0335243a7cbd43a87
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-08 18:45:27 +0200

    Added test project for the Core, removed the Misc directory and old ImageOutputSettings.cs file.

---
+++ Tests/Core/TestPolygonMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+
+namespace ImageResizer.Tests {
+    [TestFixture]
+    public class TestPolygonMath {
+        [Test]
+        public void Test() {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
