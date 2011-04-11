4777cfd6f5d8a09cb92c8c07e67c6ac485f3c569
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-11 17:20:37 +0200

    Added LitS3 to the libs directory.
    Former-commit-id: 60335dcfd34d528656a9ee4170e761bb6d823929

---
+++ Plugins/Libs/LitS3-1.0.1/LitS3/AccessControl.cs
@@ -0,0 +1,30 @@
+﻿
+namespace LitS3
+{
+    /// <summary>
+    /// Access Control Lists that are predefined by Amazon and supported when adding objects.
+    /// </summary>
+    public enum CannedAcl
+    {
+        /// <summary>
+        /// Owner gets FULL_CONTROL. No one else has access rights (default).
+        /// </summary>
+        Private,
+        /// <summary>
+        /// Owner gets FULL_CONTROL and the anonymous principal is granted READ access. 
+        /// If this policy is used on an object, it can be read from a browser with no authentication.
+        /// </summary>
+        PublicRead,
+        /// <summary>
+        /// Owner gets FULL_CONTROL, the anonymous principal is granted READ and WRITE access. This is 
+        /// a useful policy to apply to a bucket, if you intend for any anonymous user to PUT 
+        /// objects into the bucket.
+        /// </summary>
+        PublicReadWrite,
+        /// <summary>
+        /// Owner gets FULL_CONTROL, and any principal authenticated as a registered 
+        /// Amazon S3 user is granted READ access.
+        /// </summary>
+        AuthenticatedRead
+    }
+}
---
+++ Plugins/Libs/LitS3-1.0.1/LitS3/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LitS3")]
+[assembly: AssemblyDescription("Small and fast library for Amazon S3")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nick Farina @ Spotlight Mobile")]
+[assembly: AssemblyProduct("LitS3")]
+[assembly: AssemblyCopyright("Copyright © Spotlight Mobile 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("E1B855AD-28BE-49a9-BDAA-BAE7ACF6020E")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
