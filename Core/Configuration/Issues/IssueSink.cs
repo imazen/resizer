280fe56c4a288e30e81d0076d09ea74f46d50de5
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-17 16:51:17 +0200

    Updated documentation. Moved hash function outside lock in IssueSink.Add

--- Core/Configuration/Issues/IssueSink.cs
+++ Core/Configuration/Issues/IssueSink.cs
@@ -13,6 +13,10 @@ namespace ImageResizer.Configuration.Issues {
         IDictionary<int, IIssue> _issueSet = new Dictionary<int,IIssue>();
         IList<IIssue> _issues = new List<IIssue>();
         object issueSync = new object();
+        /// <summary>
+        /// Returns a copy of the list of reported issues.
+        /// </summary>
+        /// <returns></returns>
         public virtual IEnumerable<IIssue> GetIssues() {
             lock (issueSync) {
                 return new List<IIssue>(_issues);
@@ -24,11 +28,11 @@ namespace ImageResizer.Configuration.Issues {
         /// <param name="i"></param>
         public virtual void AcceptIssue(IIssue i) {
             //Set default source value
-            if (i.Source == null) ((Issue)i).Source = defaultSource;
+            if (i.Source == null && i as Issue != null) ((Issue)i).Source = defaultSource;
 
-            //Perform duplicate checking, then add item
-            lock (issueSync) {
-                int hash = i.GetHashCode();
+            //Perform duplicate checking, then add item if unique.
+            int hash = i.GetHashCode();
+            lock (issueSync) {    
                 if (!_issueSet.ContainsKey(hash)) {
                     _issueSet[hash] = i;
                     _issues.Add(i);
