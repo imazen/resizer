f5306acd240a9263df1d063525eacfc742a13088
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-18 16:24:38 +0200

    Made the Diagnostic plugin installed by default. Without configuration, it will mimic the CustomErrors behavior. It can be configured to <diagnostics enableFor="None|Localhost|AllHosts|" />

--- Core/Plugins/Basic/Diagnostic.cs
+++ Core/Plugins/Basic/Diagnostic.cs
@@ -3,8 +3,24 @@ using System.Collections.Generic;
 using System.Text;
 using ImageResizer.Configuration;
 using ImageResizer.Plugins;
+using System.Web;
 
 namespace ImageResizer.Plugins.Basic {
+    public enum DiagnosticMode {
+        /// <summary>
+        /// Diagnostics are disabled
+        /// </summary>
+        None,
+        /// <summary>
+        /// Only local requests can access diagnostics
+        /// </summary>
+        Localhost,
+        /// <summary>
+        /// All hosts can view diagnostics.
+        /// </summary>
+        AllHosts
+
+    }
     public class Diagnostic :IPlugin{
         Config c;
         public IPlugin Install(Configuration.Config c) {
@@ -15,9 +31,32 @@ namespace ImageResizer.Plugins.Basic {
         }
 
         void Pipeline_PostAuthorizeRequestStart(System.Web.IHttpModule sender, System.Web.HttpContext context) {
-            if (context.Request.FilePath.EndsWith("/resizer.info", StringComparison.OrdinalIgnoreCase)) {
+            
+            if (context.Request.FilePath.EndsWith("/resizer.info", StringComparison.OrdinalIgnoreCase) &&
+                AllowResponse(context)) {
                 context.RemapHandler(new DiagnosticPageHandler(c));
             } 
+        }
+
+        /// <summary>
+        /// True if diagnostics can be displayed to the current user.
+        /// If &lt;diagnostics enableFor="None" /&gt;, returns false.
+        /// If &lt;diagnostics enableFor="Localhost" /&gt;, returns false for remote requests
+        /// If &lt;diagnostics enableFor="AllHosts" /&gt;, returns true.
+        /// If unspecified, uses the same behavior as ASP.NET Custom Errors.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public bool AllowResponse(HttpContext context) {
+            
+            bool detailedErrors = !context.IsCustomErrorEnabled;
+            DiagnosticMode def = (detailedErrors) ? DiagnosticMode.AllHosts : DiagnosticMode.None;
+
+            DiagnosticMode mode = c.get<DiagnosticMode>("diagnostics.enableFor", def);
+            if (mode == DiagnosticMode.None) return false;
+            if (mode == DiagnosticMode.AllHosts) return false;
+            return context.Request.IsLocal;
+            
         }
 
         public bool Uninstall(Configuration.Config c) {
