3706e0af5fcccc06e85db3f568cef576e0da89d5
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-19 17:41:02 +0200

    Updated all Core source code files to point to license.txt, and added license.txt with the current revision of the Resizer Freedom license
    Former-commit-id: d91492dfc3a2e5e0e2c53ea0d87ff1dcc170db48

--- Core/Configuration/ReverseEnumerator.cs
+++ Core/Configuration/ReverseEnumerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/* Copyright (c) 2011 Nathanael Jones. See license.txt */
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Collections.ObjectModel;
