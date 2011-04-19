ecc4901d8e19dce5fee5058519cc1b0bd7f4389e
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-19 16:48:09 +0200

    Added ServerFactory() for automatically creating a server on a random port. Created ClientRequest and ClientResponse for simplifying unit testing.
    Former-commit-id: 50f176412b87aa8d862036a3d5fd52c8dcfaff9e

---
+++ Tests/LibCassini35/Client/CustomWebClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace LibCassini.Client {
+    public class CustomWebClient:WebClient {
+        public CustomWebClient() : base() { }
+        protected override WebRequest GetWebRequest(Uri address) {
+            WebRequest r =  base.GetWebRequest(address);
+            r.Timeout = 5000;
+            return r;
+        }
+    }
+}
---
+++ Tests/LibCassini35/ServerFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibCassini {
+    public class ServerFactory {
+        public ServerFactory(){
+        }
+
+        public Server CreateAndStart(string physicalPath, string virtualPath, int retryCount = 2){
+            //49152 through 65535 are valid port numbers
+            //Try 2 port numbers, then fail if we still can't start the server.
+            for (int i = 0; i < retryCount; i++){
+                int port = new Random().Next(49152, 65535);
+                try {
+                    Server server = new Server(port, virtualPath, physicalPath);
+                    server.Start();
+                    return server;
+                }catch(Exception e){
+                    if (i + 1 == retryCount) throw e;//On the last iteration, rethrow the exception.
+                }
+            }
+            return null;
+        }
+  
+    }
+}
