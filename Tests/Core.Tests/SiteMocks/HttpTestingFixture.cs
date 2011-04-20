c7c2701fe37fae91c38db21f08cc44a9cec3bb3c
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-20 18:53:48 +0200

    Moved Web.Config functionality to the WebConfigBuilder

--- Tests/Core.Tests/SiteMocks/HttpTestingFixture.cs
+++ Tests/Core.Tests/SiteMocks/HttpTestingFixture.cs
@@ -22,41 +22,14 @@ namespace ImageResizer.Core.Tests.SiteMocks {
 
         public Server server = null;
 
-        public string webConfigTop = "<?xml version='1.0' encoding='utf-8' ?><configuration><configSections>" + 
-            "<section name='resizer' restartOnExternalChanges='true' requirePermission='false' type='ImageResizer.ResizerSection,ImageResizer' />" + 
-            "</configSections>";
-
-        public string resizerTop = 
-            "<resizer xmlns='http://imageresizingin.net/resizer.xsd'  xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:noNamespaceSchemaLocation='resizer.xsd'>\n\n";
-
-        public string resizerContents = "";
-
-        public string resizerBottom = "\n</resizer>\n";
-
-        public string compilationTop = "<system.web><compilation debug='true'><assemblies>";
-        public string webConfigBottom =
-            "</assemblies></compilation><httpModules> <add name='ImageResizingModule' type='ImageResizer.InterceptModule'/>\n" +
-            "</httpModules></system.web>\n" + 
-            "<system.webServer><validation validateIntegratedModeConfiguration='false'/>\n" + 
-            "<modules> <add name='ImageResizingModule' type='ImageResizer.InterceptModule'/>\n" +
-            "</modules></system.webServer></configuration>\n";
-
-        public virtual string buildWebConfig(){
-            return webConfigTop + resizerTop + resizerContents + resizerBottom + compilationTop + webConfigBottom;
+        public virtual string ResizerSectionContents() {
+            return "";
         }
-        //public virtual string GetAssemblies(string binDir) {
-        //    string[] paths = Directory.GetFiles(binDir, "*.dll");
-        //    StringBuilder sb = new StringBuilder();
-        //    foreach (string path in paths) {
-        //        sb.AppendLine("<add assembly='" + Path.GetFileNameWithoutExtension(path) + "'/>");
-        //    }
-        //    return sb.ToString();
-        //}
         
         [FixtureSetUp()]
         public void StartServer() {
             SiteCreator site = new SiteCreator().Create();
-            site.WriteWebConfig(buildWebConfig());
+            site.WriteWebConfig(new WebConfigBuilder(ResizerSectionContents()).Build());
             string path = site.dir;
             server = new ServerFactory().CreateAndStart(path, "/");
             Debug.WriteLine("Located at " + path);
@@ -74,13 +47,6 @@ namespace ImageResizer.Core.Tests.SiteMocks {
         public void TestOn() {
             Debug.WriteLine("IsListening=" + server.IsListening.ToString());
             Assert.AreEqual<HttpStatusCode>(HttpStatusCode.OK, this.Request("image.jpg").StatusCode);
-        }
-
-        [Test()]
-        public void TestExternal() {
-            Debug.WriteLine("IsListening=" + server.IsListening.ToString());
-            //Process.Start(server.RootUrl + "image.jpg");
-            //Thread.Sleep(10000);
         }
 
         public ClientResponse Request(string relativeUrl) {
