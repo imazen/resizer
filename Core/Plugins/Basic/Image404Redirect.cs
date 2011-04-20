50f83bdffe678224fb70e02a3299b13955c1659c
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-20 22:12:13 +0200

    Added stub for Image404Redirect plugin
    Former-commit-id: 66cfbcbebcff2a1c7f2001d79aa8d7d283e77c7f

---
+++ Core/Plugins/Basic/Image404Redirect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageResizer.Plugins.Basic {
+    /// <summary>
+    /// Stub class, plan to implement later
+    /// </summary>
+    public class Image404Redirect {
+
+    }
+}
