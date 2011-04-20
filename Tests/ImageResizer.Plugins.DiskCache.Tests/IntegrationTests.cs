3f544d33e204c61ec265afe60207bf6b76652432
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-20 18:54:04 +0200

    Testing app domain communication

---
+++ Tests/ImageResizer.Plugins.DiskCache.Tests/IntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ImageResizer.Core.Tests.SiteMocks;
+using MbUnit.Framework;
+using LibCassini.Client;
+using System.Web;
+using System.Diagnostics;
+
+namespace ImageResizer.Plugins.DiskCache.Tests {
+    public class IntegrationTests : HttpTestingFixture {
+        public override string ResizerSectionContents() {
+            return "<plugins><add name='DiskCache' /></plugins>";
+        }
+
+        [Test]
+        public void TestAppDomains() {
+            ClientResponse r = this.Request("image.jpg?height=200");
+            Debug.WriteLine(AppDomain.CurrentDomain.FriendlyName);
+            Debug.WriteLine(server.GetHost().ExecuteDelegate(delegate() {
+                return AppDomain.CurrentDomain.FriendlyName;
+            }));
+        }
+    }
+}
