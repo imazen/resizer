18719a46d02d84a43db2403f93b06b6226337b4e
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-26 15:46:40 +0200

    First draft complete
    Former-commit-id: dc3a932c752e9775b57f413f9da46163d6512034

--- Tests/Builder/Build.cs
+++ Tests/Builder/Build.cs
@@ -23,5 +23,35 @@ namespace ImageResizer.ReleaseBuilder {
             d.Run("/Build Trial");
         }
 
+        public void RemoveUselessFiles() {
+            var f = new Futile(Console.Out);
+            var q = new FsQuery(this.f.corePath);
+
+            
+            //delete /Tests/binaries  *.pdb, *.xml, *.dll
+            //delete /samples/ * /bin/ *.pdb, *.xml, *.dll
+            f.DelFiles(q.files(new Pattern("^/Tests/binaries/*.(pdb|xml|dll)$"),
+                                new Pattern("^/Samples/*/bin/*.(pdb|xml|dll)$")));
+            //delete /tests/   /bin and /obj folders
+            //delete /samples/ /imagecache
+            //delete /core/obj
+            //delete Plugins */obj* and */bin
+            f.DelFolders(q.folders(new Pattern("^/Tests/*/(bin|obj)$"),
+                                   new Pattern("^/Samples/*/imagecache$"),
+                                   new Pattern("^/Plugins/*/(bin|obj)$"),
+                                   new Pattern("^/Core/obj$")));
+
+
+            //delete Thumbs.db
+            //delete */.DS_Store
+            q.exclusions = null;
+            f.DelFiles(q.files(new Pattern("/Thumbs.db$"),
+                                new Pattern("/.DS_Store$")));
+            
+            
+
+
+
+        }
     }
 }
--- Tests/Builder/FsQuery.cs
+++ Tests/Builder/FsQuery.cs
@@ -6,17 +6,57 @@ using System.IO;
 
 namespace ImageResizer.ReleaseBuilder {
     public class FsQuery {
-        string[] files;
-        string[] folders;
+        string[] _files;
+        string[] _folders;
+        string baseDir = null;
         public FsQuery(string dir) {
-            files = Directory.GetFiles(dir, "", SearchOption.AllDirectories);
-            folders = Directory.GetDirectories(dir, "", SearchOption.AllDirectories);
+            baseDir = dir.TrimEnd('\\','/');
+            _files = Directory.GetFiles(dir, "", SearchOption.AllDirectories);
+            _folders = Directory.GetDirectories(dir, "", SearchOption.AllDirectories);
+
+            exclusions.Add(new Pattern("/.git"));
+            exclusions.Add(new Pattern("^/Releases"));
+            exclusions.Add(new Pattern("^/Legacy"));
+            exclusions.Add(new Pattern("/thumbs.db$"));
+            exclusions.Add(new Pattern("/.DS_Store$"));
         }
 
         public List<Pattern> exclusions = new List<Pattern>();
 
 
+        public List<string> files(params Pattern[] p){
+            return query(_files, exclusions, p);
+        }
+        public List<string> folders(params Pattern[] p) {
+            return query(_folders, exclusions, p);
+        }
 
+        protected List<String> query(string[] items, IEnumerable<Pattern> exclusions, IEnumerable<Pattern> queries) {
+            List<string> results = new List<string>();
+            foreach (string item in items) {
+                //Trim to basedir
+                if (!item.StartsWith(baseDir)) throw new Exception("Paths don't match: " + item + " , " + baseDir);
+                string s = item.Substring(baseDir.Length);
+
+                bool excluded = false;
+                if (exclusions != null)
+                    foreach (Pattern e in exclusions)
+                        if (e.IsMatch(s)) {
+                            excluded = true;
+                            break;
+                        }
+
+                if (!excluded) {
+                    foreach (Pattern q in queries) {
+                        if (q.IsMatch(s)) {
+                            results.Add(item);
+                            break;
+                        }
+                    }
+                }
+            }
+            return results;
+        }
 
 
     }
