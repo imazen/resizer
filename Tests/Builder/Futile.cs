a76578c5aa32890ada835cbfe86c1b2595cbdf5a
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-26 15:46:40 +0200

    First draft complete

---
+++ Tests/Builder/Futile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ImageResizer.ReleaseBuilder {
+    /// <summary>
+    /// File utilities
+    /// </summary>
+    public class Futile {
+        TextWriter err;
+        public Futile(TextWriter errStream){
+            this.err = errStream;
+        }
+        public void DelFiles(IEnumerable<string> paths) {
+            foreach(string s in paths){
+                try {
+                    File.Delete(s);
+                } catch (Exception ex) {
+                    err.WriteLine("Failed to delete " + s + " " + ex.ToString());
+                }
+            }
+        }
+        public void DelFolders(IEnumerable<string> paths) {
+            foreach (string s in paths) {
+                try {
+                    Directory.Delete(s, true);
+                } catch (Exception ex) {
+                    err.WriteLine("Failed to delete " + s + " " + ex.ToString());
+                }
+            }
+        }
+    }
+}
--- Tests/Builder/Pattern.cs
+++ Tests/Builder/Pattern.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ImageResizer.ReleaseBuilder {
-    public class Pattern {
+    public class Pattern:Regex {
+        public Pattern(string pattern):base(pattern.Replace("/","\\\\").Replace(".","\\.").Replace("*","."), RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.ExplicitCapture | RegexOptions.IgnoreCase | RegexOptions.Singleline) {
+
+        }
+
+
     }
 }
