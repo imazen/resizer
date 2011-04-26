404d3277ec666c829b11eb29980326179247e893
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-26 15:05:00 +0200

    Adding builder project.

---
+++ Tests/Builder/Build.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageResizer.ReleaseBuilder {
+    public class Build {
+        SolutionFinder f = new SolutionFinder();
+        Devenv d = null;
+        public Build() {
+            d = new Devenv(f.solutionPath);
+        }
+
+        public void CleanAll(){
+            d.Run("/Clean Debug");
+            d.Run("/Clean Release");
+            d.Run("/Clean Trial");
+        }
+
+        public void BuildAll() {
+            d.Run("/Build Debug");
+            d.Run("/Build Release");
+            d.Run("/Build Trial");
+        }
+
+    }
+}
