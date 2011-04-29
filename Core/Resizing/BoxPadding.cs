64da5834b980db127bf3860529d49ffd93915e48
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-29 15:39:25 +0200

    Added docs
    Former-commit-id: 2ebb78579ba2166da250a24e5e5843dffc53ce30

--- Core/Resizing/BoxPadding.cs
+++ Core/Resizing/BoxPadding.cs
@@ -4,18 +4,37 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace ImageResizer.Resizing {
+    /// <summary>
+    /// Represents the widths of edges of a box.
+    /// </summary>
     public class BoxPadding {
+        
+        /// <summary>
+        /// Create a box with all edges the same width.
+        /// </summary>
+        /// <param name="all"></param>
+        public BoxPadding(double all) {
+            this.all = all;
+        }
+        /// <summary>
+        /// Create a box, specifying individual widths for each size
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="top"></param>
+        /// <param name="right"></param>
+        /// <param name="bottom"></param>
+        public BoxPadding(double left, double top, double right, double bottom) {
+            this.top = top; this.left = left; this.bottom = bottom; this.right = right;
+        }
+        /// <summary>
+        /// Copies the specified BoxPadding instance
+        /// </summary>
+        /// <param name="original"></param>
         public BoxPadding(BoxPadding original) {
             this.top = original.top;
             this.bottom = original.bottom;
             this.right = original.right;
             this.left = original.left;
-        }
-        public BoxPadding(double all) {
-            this.all = all;
-        }
-        public BoxPadding(double left, double top, double right, double bottom) {
-            this.top = top; this.left = left; this.bottom = bottom; this.right = right;
         }
 
         protected double top = 0, left = 0, bottom =0, right = 0;
@@ -25,6 +44,11 @@ namespace ImageResizer.Resizing {
         public double Right { get { return right; } }
         public double Bottom { get { return bottom; } }
 
+        /// <summary>
+        /// Sets the width of all edges, returning a new instance
+        /// </summary>
+        /// <param name="all"></param>
+        /// <returns></returns>
         public BoxPadding SetAll(double all) {
             BoxPadding b = new BoxPadding(this); b.all = all;
             return b;
@@ -46,6 +70,9 @@ namespace ImageResizer.Resizing {
             return b;
         }
 
+        /// <summary>
+        /// Returns -1 unless all edges are the same width, in which case that width is returned
+        /// </summary>
         public double All { get { return this.all; } }
 
         protected double all {
@@ -57,8 +84,13 @@ namespace ImageResizer.Resizing {
                 top = left = bottom = right = value;
             }
         }
-
+        /// <summary>
+        /// Returns an instance with a width of 0
+        /// </summary>
         public static BoxPadding Empty { get { return new BoxPadding(0); } }
+        /// <summary>
+        /// Returns true if th
+        /// </summary>
         public bool IsEmpty { get { return all == 0; } }
         /// <summary>
         /// Gets edge offsets as a clockwise array, starting with Top.
