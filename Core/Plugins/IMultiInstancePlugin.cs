0e212fe355f46e387774829a1ae60721b2db3e66
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-29 15:23:00 +0200

    Added IMultiInstancePlugin interface for allowing plugins to have multiple instances.

---
+++ Core/Plugins/IMultiInstancePlugin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageResizer.Plugins {
+    /// <summary>
+    /// Tag your plugin with this interface if it supports having multiple instances registered at once
+    /// </summary>
+    public interface IMultiInstancePlugin {
+    }
+}
