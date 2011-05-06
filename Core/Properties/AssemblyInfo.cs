e43de99f9c37a37bd494fdc66dece9ce369c81f0
Nathanael Jones <nathanael.jones@gmail.com>
2011-05-06 17:34:37 +0200

    Migrated all (non-testing) projects to use a shared assembly info file, allowing for easy version increments.
    Former-commit-id: 9fd2f43374d49e92edb422ba5bfac30c51cfcdfa

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -1,39 +1,18 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
-using System;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Image Resizing Library")]
-[assembly: AssemblyDescription("Allows server-side resizing, cropping, and manipulation of images")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Nathanael Jones")]
-[assembly: AssemblyProduct("ImageResizer")]
-[assembly: AssemblyCopyright("Copyright 2011 Nathanael Jones")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+[assembly: AssemblyTitle("ImageResizer.Core")]
+[assembly: AssemblyDescription("The core HttpModule - Allows server-side resizing, cropping, and manipulation of images")]
+
+
+[assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2df254c3-25f6-48c3-b45f-8d59e0d27077")]
 
-[assembly: CLSCompliant(true)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:/*
-[assembly: AssemblyVersion("3.0.3.*")]
-[assembly: AssemblyFileVersion("3.0.3.*")]
-[assembly: AssemblyInformationalVersion("Resizer3-alpha-3")]
---
+++ Core/SharedAssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+//[assembly: AssemblyDescription("Allows server-side resizing, cropping, and manipulation of images")]
+[assembly: AssemblyProduct("Image Resizer")]
+
+[assembly: AssemblyCompany("Nathanael Jones")]
+[assembly: AssemblyCopyright("Copyright © 2011 Nathanael Jones")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:/*
+[assembly: AssemblyVersion("3.0.4.*")]
+[assembly: AssemblyFileVersion("3.0.4.*")]
+[assembly: AssemblyInformationalVersion("3.0.4alpha4")]
