f9dda40d289a0711ef005908d9ba958dfb720de8
Nathanael Jones <nathanael.jones@gmail.com>
2011-05-06 17:34:37 +0200

    Migrated all (non-testing) projects to use a shared assembly info file, allowing for easy version increments.

--- Plugins/AdvancedFilters/AssemblyInfo.cs
+++ Plugins/AdvancedFilters/AssemblyInfo.cs
@@ -1,10 +1,13 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImageResizer.Plugins.SimpleFilters")]
+[assembly: AssemblyTitle("ImageResizer.Plugins.AdvancedFilters")]
+
+
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6f33ccc4-021c-4810-8d9e-abfd3b066fb3")]
+[assembly: Guid("5cfe2b47-083e-4684-ab10-b58e18ee8c1f")]
--- Samples/ComplexWebApplication/Properties/AssemblyInfo.cs
+++ Samples/ComplexWebApplication/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ComplexWebApplication")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright © 2011 Nathanael Jones")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
