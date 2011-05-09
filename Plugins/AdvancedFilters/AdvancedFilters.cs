3f33a9ea8a4e70b29a3ea65d704ca584c659cee6
Nathanael Jones <nathanael.jones@gmail.com>
2011-05-09 12:40:08 +0200

    Added first draft of AdvancedFilters plugin

--- Plugins/AdvancedFilters/AdvancedFilters.cs
+++ Plugins/AdvancedFilters/AdvancedFilters.cs
@@ -4,15 +4,37 @@ using System.Collections.Generic;
 using System.Text;
 using ImageResizer;
 using ImageResizer.Resizing;
+using AForge.Imaging.Filters;
 namespace ImageResizer.Plugins.AdvancedFilters {
     public class AdvancedFilters:BuilderExtension, IPlugin {
+        public AdvancedFilters() {
+        }
 
         public IPlugin Install(Configuration.Config c) {
-            throw new NotImplementedException();
+            c.Plugins.add_plugin(this);
+            return this;
         }
 
         public bool Uninstall(Configuration.Config c) {
-            throw new NotImplementedException();
+            c.Plugins.remove_plugin(this);
+            return true;
+        }
+        protected override RequestedAction PostRenderImage(ImageState s) {
+            string blurRad = s.settings["blur"];
+            int blurRadius = 0;
+            if (!string.IsNullOrEmpty(blurRad) && int.TryParse(blurRad, out blurRadius)) {
+                GaussianBlur b = new GaussianBlur(1.4, blurRadius);
+                b.ApplyInPlace(s.destBitmap);
+            }
+
+            string sharpRad = s.settings["sharpen"];
+            int sharpRadius = 0;
+            if (!string.IsNullOrEmpty(sharpRad) && int.TryParse(sharpRad, out sharpRadius)) {
+                GaussianSharpen gs = new GaussianSharpen(1.4, sharpRadius);
+                gs.ApplyInPlace(s.destBitmap);
+            }
+            
+            return RequestedAction.None;
         }
     }
 }
