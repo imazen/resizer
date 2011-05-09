82dbf04eb22da05b61d74eb60d8177c9cdf9d605
Nathanael Jones <nathanael.jones@gmail.com>
2011-05-09 14:29:07 +0200

    Added first drat of CloudFrontPlugin

---
+++ Plugins/CloudFront/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImageResizer.Plugins.CloudFront")]
+
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("37d47581-9650-4d83-b885-ef987e058034")]
