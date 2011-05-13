aade6a880c50c481c86c03b2cbdea5c55a160931
Nathanael Jones <nathanael.jones@gmail.com>
2011-05-13 16:34:07 +0200

    Simplified sample plugin
    Former-commit-id: 6d372f91b78c6d467dd668f65c6df83640843858

--- Samples/ComplexWebApplication/SamplePlugin.cs
+++ Samples/ComplexWebApplication/SamplePlugin.cs
@@ -12,31 +12,38 @@ namespace MyCode.MyPlugins {
         
         protected override RequestedAction OnProcess(ImageState s) {
             return base.OnProcess(s);
-            //If we wanted to modify the querystring or settings before the pipleine started, we could do it here (like the ModifySettings method of WatermarkSettings.cs in V2)
+            // If we wanted to modify the querystring or settings before the pipleine started, 
+            // we could do it here (like the ModifySettings method of WatermarkSettings.cs in V2)
         }
  
         protected override RequestedAction RenderOverlays(ImageState s) {
-            string sample = s.settings["sample"]; //from the querystring
-            if (string.IsNullOrEmpty(sample) || s.destGraphics == null) return RequestedAction.None; //Don't try to draw the string if it is empty, or if this is a 'simulation' render
+            // Get 'sample' from the querystring
+            string sample = s.settings["sample"]; 
 
-            s.destGraphics.DrawString(sample, new System.Drawing.Font("Arial", (float)(s.destBitmap.Width / (sample.Length * 1.5f)), System.Drawing.GraphicsUnit.Pixel),
-                System.Drawing.Brushes.Black, 0, 0);
+            //Don't try to draw the string if it is empty, or if this is a 'simulation' render
+            if (string.IsNullOrEmpty(sample) || s.destGraphics == null) return RequestedAction.None;
+
+            //Let's use Arial, and make sure the text fits inside the bitmap width.
+            System.Drawing.Font font = new System.Drawing.Font("Arial", 
+                (float)(s.destBitmap.Width / (sample.Length * 1.5f)), System.Drawing.GraphicsUnit.Pixel);
+
+            //Draw the text at the top left corner of the resulting image.
+            s.destGraphics.DrawString(sample, font, System.Drawing.Brushes.Black, 0, 0);
 
             return RequestedAction.None;
         }
 
-        public SamplePlugin() {
+        public SamplePlugin() { 
         }
 
         Config c;
-
-        public IPlugin Install(Config c) {
+        public IPlugin Install(Config c) { //Allows '<add name="MyCode.MyPlugins.SamplePlugin" />' to work
             c.Plugins.add_plugin(this);
             this.c = c;
             return this;
         }
 
-        public bool Uninstall(Config c) {
+        public bool Uninstall(Config c) { //Allows '<remove name="MyCode.MyPlugins.SamplePlugin" />' to work
             c.Plugins.remove_plugin(this);
             return true;
         }
