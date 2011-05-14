7b397af1bd3be34ea1000449a32ee092e03c68da
Nathanael Jones <nathanael.jones@gmail.com>
2011-05-14 19:17:05 +0200

    Added CommitAttribute to SharedAssemblyInfo.cs. Added full version listing capabilities to DiagnosticPageHandler.

---
+++ Core/Util/CommitAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageResizer.Util {
+
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class CommitAttribute : Attribute {
+          
+       string guid;
+       public CommitAttribute() { guid = string.Empty; }
+       public CommitAttribute(string txt) { guid = txt; }
+
+       public string Value { get { return guid; } }
+       public override string ToString() {
+           return guid;
+       }
+        
+    }
+
+}
