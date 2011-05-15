de4222ef0eff4f800a3207ffc4d1618b9ba05c3b
Nathanael Jones <nathanael.jones@gmail.com>
2011-05-15 17:56:02 +0200

    Moved version number to 3.0.6

--- Core/SharedAssemblyInfo.cs
+++ Core/SharedAssemblyInfo.cs
@@ -23,9 +23,9 @@ using ImageResizer.Util;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:/*
-[assembly: AssemblyVersion("3.0.5.*")]
-[assembly: AssemblyFileVersion("3.0.5.*")]
-[assembly: AssemblyInformationalVersion("3-alpha-5")]
+[assembly: AssemblyVersion("3.0.6.*")]
+[assembly: AssemblyFileVersion("3.0.6.*")]
+[assembly: AssemblyInformationalVersion("3-alpha-6")]
 
 [assembly: Commit("git-commit-guid-here")]
 
