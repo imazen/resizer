1ebd7acd78948d4ccf1561e6e05af3b26d3755b5
Nathanael Jones <nathanael.jones@gmail.com>
2011-05-25 15:39:51 +0200

    Updated COM usage examples and docs.
    Former-commit-id: 0a540ab984d0f4ada1dbe5f31ebfe5e8bd49eed4

--- Samples/ScriptAccess/advancedScript.js
+++ Samples/ScriptAccess/advancedScript.js
@@ -4,15 +4,12 @@ var p = new ActiveXObject("ImageResizer.Plugins.PrettyGifs.PrettyGifs");
 
 p.Install(c);
 
-//c.setConfigXmlText("<resizer><plugins><add name=\"PrettyGifs\" /></plugins></resizer>");
-
-//c.Plugins.LoadPlugins()
 
 c.BuildImage("..\\Images\\quality-original.jpg","grass.gif", "rotate=3&width=600&format=gif&colors=128");
-c.WriteDiagnosticsTo("advancedScript-1.txt");
+//c.WriteDiagnosticsTo("advancedScript-1.txt");
 
 
 var c2 = new ActiveXObject("ImageResizer.Configuration.Config");
 
 c2.BuildImage("..\\Images\\quality-original.jpg","grass-ugly.gif", "rotate=3&width=600&format=gif");
-c2.WriteDiagnosticsTo("advancedScript-2.txt");
+//c2.WriteDiagnosticsTo("advancedScript-2.txt");
--- Samples/ScriptAccess/readme.txt
+++ Samples/ScriptAccess/readme.txt
@@ -5,3 +5,9 @@ JScript, VBScript and COM clients can access the ImageResizer.dll if it is regis
 2) Create an instance of ImageResizer.Configuration.Config using new ActiveXObject() or CreateObject()
 
 3) Call .BuildImage on that instance, passing 3 variables: the source path, the destination path, and the querystring (settings)
+
+4) You can use plugins by creating them and calling .Install(config):
+
+ new ActiveXObject("ImageResizer.Plugins.PrettyGifs.PrettyGifs").Install(c);
+ 
+See advancedScript.js for examples
