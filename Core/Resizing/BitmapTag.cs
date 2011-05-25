c9c65bd5309a682f2dd3b4059f1860010dd4f390
Nathanael Jones <nathanael.jones@gmail.com>
2011-05-25 14:27:38 +0200

    Added first draft of BitmapTag class, for storing path and stream information with a Bitmap instance.

---
+++ Core/Resizing/BitmapTag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.IO;
+
+namespace ImageResizer.Resizing {
+    public class BitmapTag {
+
+        public BitmapTag(string path, Stream source) {
+            this._path = path;
+            this._source = source;
+        }
+        private string _path = null;
+
+        public string Path {
+            get { return _path; }
+            set { _path = value; }
+        }
+        private Stream _source = null;
+
+        public Stream Source {
+            get { return _source; }
+            set { _source = value; }
+        }
+       
+    }
+}
