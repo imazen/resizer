0ae8c628bd5c06449daac38fe00c1ca5f0f31383
Nathanael Jones <nathanael.jones@gmail.com>
2011-05-25 22:34:49 +0200

    Watermark: Now works outside ASP.NET environment. Properly handles simulation layouts. Supports physical paths for watermarkDir. Now produces integer dimensions and position for watermark. No longer incorrectly upscales watermark to fill entire image when keepAspectRatio is true. No longer acts incorrectly when padding and size add up to > 1 and valuesPercentages=true.
    Former-commit-id: a085abcdee015394c5f98c4491cea222f82505d0

--- Plugins/Watermark/Watermark.cs
+++ Plugins/Watermark/Watermark.cs
@@ -40,11 +40,14 @@ namespace ImageResizer.Plugins.Watermark
         /// <returns></returns>
         public Bitmap GetMemCachedBitmap(string virtualPath)
         {
+            //If not ASP.NET, don't cache.
+            if (HttpContext.Current == null) return c.CurrentImageBuilder.LoadImage(virtualPath, new ResizeSettings());
+
             string key = virtualPath.ToLowerInvariant();
             Bitmap b = HttpContext.Current.Cache[key] as Bitmap;
             if (b != null) return b;
 
-            b = ImageBuilder.Current.LoadImage(virtualPath, new ResizeSettings());
+            b = c.CurrentImageBuilder.LoadImage(virtualPath, new ResizeSettings());
             //Query VPPs for cache dependency. TODO: Add support for IVirtualImageProviders to customize cache dependencies.
             CacheDependency cd = null;
             if (HostingEnvironment.VirtualPathProvider != null) cd = HostingEnvironment.VirtualPathProvider.GetCacheDependency(virtualPath, new string[] { }, DateTime.UtcNow);
@@ -57,25 +60,59 @@ namespace ImageResizer.Plugins.Watermark
         protected override RequestedAction RenderOverlays(ImageState s) {
             string watermark = s.settings["watermark"]; //from the querystring
             Graphics g = s.destGraphics;
-            RectangleF imageBox = PolygonMath.GetBoundingBox(s.layout["image"]);
+            if (string.IsNullOrEmpty(watermark) || g == null) return RequestedAction.None;
 
-            if (string.IsNullOrEmpty(watermark)) return RequestedAction.None;
+            RectangleF imageBox = PolygonMath.GetBoundingBox(s.layout["image"]);
+            //Floor and cieling values to prevent fractional placement.
+            imageBox.Width = (float)Math.Floor(imageBox.Width);
+            imageBox.Height = (float)Math.Floor(imageBox.Height);
+            imageBox.X = (float)Math.Ceiling(imageBox.X);
+            imageBox.Y = (float)Math.Ceiling(imageBox.Y);
 
             if (watermark.IndexOfAny(System.IO.Path.GetInvalidFileNameChars()) > -1 ||
                 watermark.IndexOfAny(new char[] { '\\', '/' }) > -1)
                 throw new ArgumentException("Watermark value contained invalid file name characters: " + watermark);
 
             //Combine the directory with the base dir
-            watermark = watermarkDir.TrimEnd('/') + '/' + watermark.TrimStart('/');
 
-            //Verify the file exists. If the watermark doesn't exist, skip watermarking.
-            if (!c.Pipeline.FileExists(watermark, s.settings)) return RequestedAction.None;
+            //Is watermarkDir a physical path?
+            char slash = watermarkDir.Contains("/") ? '/' : '\\';
+
+            watermark = watermarkDir.TrimEnd(slash) + slash + watermark.TrimStart(slash);
+
+            //Verify the file exists if we're in ASP.NET. If the watermark doesn't exist, skip watermarking.
+            if (!c.Pipeline.FileExists(watermark, s.settings) && slash == '/') return RequestedAction.None;
 
             //Load the file specified in the querystring,
             Bitmap wb = GetMemCachedBitmap(watermark);
             lock(wb){
                 //If percentages, resolve to pixels
                 if (valuesPercentages) {
+                    //Force into 0..1 range, inclusive.
+                    watermarkSize.Height = Math.Max(0,Math.Min(1, watermarkSize.Height));
+                    watermarkSize.Width = Math.Max(0, Math.Min(1, watermarkSize.Width));
+                    topLeftPadding.Height = Math.Max(0, Math.Min(1, topLeftPadding.Height));
+                    topLeftPadding.Width = Math.Max(0, Math.Min(1, topLeftPadding.Width));
+                    bottomRightPadding.Height = Math.Max(0, Math.Min(1, bottomRightPadding.Height));
+                    bottomRightPadding.Width = Math.Max(0, Math.Min(1, bottomRightPadding.Width));
+
+                    //Make sure everything adds up to 1
+                    double totalWidth = watermarkSize.Width + topLeftPadding.Width + bottomRightPadding.Width;
+                    if (totalWidth > 1) {
+                        totalWidth = 1 / totalWidth; //Turn it into the factor we have to multiple by to make everything fit.
+                        watermarkSize.Width *= (float)totalWidth;
+                        topLeftPadding.Width *= (float)totalWidth;
+                        bottomRightPadding.Width *= (float)totalWidth;
+                    }
+                    double totalHeight = watermarkSize.Height + topLeftPadding.Height + bottomRightPadding.Height;
+                    if (totalHeight > 1) {
+                        totalHeight = 1 / totalHeight; //Turn it into the factor we have to multiple by to make everything fit.
+                        watermarkSize.Height *= (float)totalHeight;
+                        topLeftPadding.Height *= (float)totalHeight;
+                        bottomRightPadding.Height *= (float)totalHeight;
+                    }
+
+                    //Now, we can resolve the percentages to pixels.
                     watermarkSize.Height *= imageBox.Height;
                     watermarkSize.Width *= imageBox.Width;
                     topLeftPadding.Height *= imageBox.Height;
@@ -84,11 +121,11 @@ namespace ImageResizer.Plugins.Watermark
                     bottomRightPadding.Width *= imageBox.Width;
                 }
 
-                //Keep aspect ratio
-                if (keepAspectRatio) watermarkSize = PolygonMath.ScaleInside(wb.Size, watermarkSize);
+                //Keep aspect ratio, shrinking further if needed.
+                if (keepAspectRatio) watermarkSize = PolygonMath.DownScaleInside(wb.Size, watermarkSize);
 
 
-                //Floor all values
+                //Floor all values to avoid rounding errors and blurry lines.
                 watermarkSize = new SizeF((float)Math.Floor(watermarkSize.Width), (float)Math.Floor(watermarkSize.Height));
                 topLeftPadding = new SizeF((float)Math.Floor(topLeftPadding.Width), (float)Math.Floor(topLeftPadding.Height));
                 bottomRightPadding = new SizeF((float)Math.Floor(bottomRightPadding.Width), (float)Math.Floor(bottomRightPadding.Height));
@@ -103,7 +140,7 @@ namespace ImageResizer.Plugins.Watermark
                     if (hideIfTooSmall) return RequestedAction.None;
                     else {
                         SizeF oldSize = watermarkBoundingBox;
-                        watermarkBoundingBox = PolygonMath.ScaleInside(watermarkBoundingBox, imageBox.Size);
+                        watermarkBoundingBox = PolygonMath.DownScaleInside(watermarkBoundingBox, imageBox.Size);
                         watermarkSize.Width -= (oldSize.Width - watermarkBoundingBox.Width);
                         watermarkSize.Height -= (oldSize.Height - watermarkBoundingBox.Height);
                     }
