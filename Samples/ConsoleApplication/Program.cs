7d32f006b024b0637cd857aa924e4210159ae951
Nathanael Jones <nathanael.jones@gmail.com>
2011-05-25 22:35:02 +0200

    Updated console app to test Watermark plugin

--- Samples/ConsoleApplication/Program.cs
+++ Samples/ConsoleApplication/Program.cs
@@ -9,10 +9,24 @@ namespace ConsoleApplication {
     class Program {
         static void Main(string[] args) {
             Config c = new Config();
-            c.Plugins.Get<ImageResizer.Plugins.Basic.SizeLimiting>().Uninstall(c);
             new PrettyGifs().Install(c);
+
+            ImageResizer.Plugins.Watermark.WatermarkPlugin w = new ImageResizer.Plugins.Watermark.WatermarkPlugin();
+            w.align = System.Drawing.ContentAlignment.MiddleCenter;
+            w.hideIfTooSmall = false;
+            w.keepAspectRatio = true;
+            w.valuesPercentages = true;
+            w.watermarkDir = "..\\..\\Samples\\Images\\"; //Where the watermark plugin looks for the image specifed in the querystring ?watermark=file.png
+            w.bottomRightPadding = new System.Drawing.SizeF(1, 1);
+            w.topLeftPadding = new System.Drawing.SizeF(1, 1);
+            w.watermarkSize = new System.Drawing.SizeF(1, 1); //The desired size of the watermark, maximum dimensions (aspect ratio maintained if keepAspectRatio = true)
+            //Install the plugin
+            w.Install(c);
+
+
+
             string s = c.GetDiagnosticsPage();
-            c.BuildImage("..\\..\\Samples\\Images\\quality-original.jpg", "grass.gif", "rotate=3&width=600&format=gif&colors=128");
+            c.BuildImage("..\\..\\Samples\\Images\\quality-original.jpg", "grass.gif", "rotate=3&width=600&format=gif&colors=128&watermark=Sun_256.png");
 
         }
     }
