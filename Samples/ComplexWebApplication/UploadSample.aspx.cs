bfba11bd2675b67e6a629426d429328e8ea98311
Nathanael Jones <nathanael.jones@gmail.com>
2011-05-27 23:27:10 +0200

    Improved UploadSample to work with multiple upload controls, and added docs
    Former-commit-id: d584be40794174de61ffb8dd0c0f60947d0f10bb

--- Samples/ComplexWebApplication/UploadSample.aspx.cs
+++ Samples/ComplexWebApplication/UploadSample.aspx.cs
@@ -14,30 +14,32 @@ namespace ComplexWebApplication {
             btnUpload.Click +=new EventHandler(btnUpload_Click);
         }
 
-        ResizeSettings resizeCropSettings = new ResizeSettings("width=200&height=200&format=jpg&crop=auto");
 
         void  btnUpload_Click(object sender, EventArgs e)
         {
-            //Quit if no file was uploaded
-            if (!fileUpload.HasFile) return; 
+            //Loop through each uploaded file
+            foreach (string fileKey in HttpContext.Current.Request.Files.Keys) {
+                HttpPostedFile file = HttpContext.Current.Request.Files[fileKey];
 
-            //Get the physical path for the uploads folder
- 	        string uploadFolder = MapPath("~/uploads"); 
+                //Get the physical path for the uploads folder
+                string uploadFolder = MapPath("~/uploads");
 
-            //Create the upload folder if it is missing
-            if (!Directory.Exists(uploadFolder)) Directory.CreateDirectory(uploadFolder);
+                //Create the upload folder if it is missing
+                if (!Directory.Exists(uploadFolder)) Directory.CreateDirectory(uploadFolder);
 
+                //The resizing settings can specify any of 30 commands.. See http://imageresizing.net for details.
+                ResizeSettings resizeCropSettings = new ResizeSettings("width=200&height=200&format=jpg&crop=auto");
 
-            //Generate a filename (GUIDs are best).
-            string fileName = Path.Combine(uploadFolder, System.Guid.NewGuid().ToString());
-            
-            //What final type of file will we have? This can depend on whether the resizeCropSettings sepcifies a format,
-            // and what the original format was (if resizecropSettings doesn't specify one)
-            string extension = ImageBuilder.Current.EncoderProvider.GetEncoder(resizeCropSettings,fileUpload.PostedFile.FileName).Extension;
-            fileName += "." + extension;
+                //Generate a filename (GUIDs are safest).
+                string fileName = Path.Combine(uploadFolder, System.Guid.NewGuid().ToString());
 
-            //Resize the image
-            ImageBuilder.Current.Build(fileUpload.PostedFile,fileName,resizeCropSettings);
+                //What final type of file will we have? This can depend on whether the resizeCropSettings sepcifies a format,
+                // and what the original format was (if resizecropSettings doesn't specify one)
+                fileName += "." + ImageBuilder.Current.EncoderProvider.GetEncoder(resizeCropSettings, fileUpload.PostedFile.FileName).Extension;
+
+                //Resize the image
+                ImageBuilder.Current.Build(file, fileName, resizeCropSettings);
+            }
         }
     }
 }
