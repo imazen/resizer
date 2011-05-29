819c616dfecc1b2610ba9d9334f184f953c0c1b9
Nathanael Jones <nathanael.jones@gmail.com>
2011-05-29 23:20:55 +0200

    Added commented-out example of acquiring byte[] arrays of a resized upload for use with SQL.
    Former-commit-id: 2046272fce042fe748c4fb07c262eb077898a1f5

--- Samples/ComplexWebApplication/UploadSample.aspx.cs
+++ Samples/ComplexWebApplication/UploadSample.aspx.cs
@@ -15,8 +15,7 @@ namespace ComplexWebApplication {
         }
 
 
-        void  btnUpload_Click(object sender, EventArgs e)
-        {
+        void btnUpload_Click(object sender, EventArgs e) {
             //Loop through each uploaded file
             foreach (string fileKey in HttpContext.Current.Request.Files.Keys) {
                 HttpPostedFile file = HttpContext.Current.Request.Files[fileKey];
@@ -40,6 +39,25 @@ namespace ComplexWebApplication {
                 //Resize the image
                 ImageBuilder.Current.Build(file, fileName, resizeCropSettings);
             }
+
+            //Here's an example of getting a byte array for sending to SQL
+
+            ////Loop through each uploaded file
+            //foreach (string fileKey in HttpContext.Current.Request.Files.Keys) {
+            //    HttpPostedFile file = HttpContext.Current.Request.Files[fileKey];
+
+            //    //The resizing settings can specify any of 30 commands.. See http://imageresizing.net for details.
+            //    ResizeSettings resizeCropSettings = new ResizeSettings("width=200&height=200&format=jpg&crop=auto");
+
+            //    using (MemoryStream ms = new MemoryStream()) {
+            //        //Resize the image
+            //        ImageBuilder.Current.Build(file, ms, resizeCropSettings);
+
+            //        //Upload the byte array to SQL: ms.ToArray();
+            //    }
+            //}
         }
+
+      
     }
 }
