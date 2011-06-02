aca79712e4c769b5062fe3950cb8087c770f8560
Nathanael Jones <nathanael.jones@gmail.com>
2011-06-02 10:39:26 +0200

    Samples: added null check to img1.Value
    Former-commit-id: 72d7ea79d24b499c5ae8912ed94bf626d7bf6a1a

--- Samples/ComplexWebApplication/CropExample.aspx.cs
+++ Samples/ComplexWebApplication/CropExample.aspx.cs
@@ -9,7 +9,7 @@ namespace ComplexWebApplication {
     public partial class CropExample : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
             //Here we just do a redirect, but we could store the URL in SQL just as easily, saving the crop settings for future pages.
-            if (Page.IsPostBack) Response.Redirect(ResolveUrl(img1.Value));
+            if (Page.IsPostBack && !string.IsNullOrEmpty(img1.Value)) Response.Redirect(ResolveUrl(img1.Value));
             //We could even write the cropped image to disk, if we wanted
             ImageBuilder.Current.Build(ResolveUrl(ImageResizer.Util.PathUtils.RemoveQueryString(img1.Value)), "~/savedimages/cropped.jpg", new ResizeSettings(img1.Value));
         }
