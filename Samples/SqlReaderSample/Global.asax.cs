3ade05864d22064b8477058ecd15a6ba2227ceb0
Nathanael Jones <nathanael.jones@gmail.com>
2011-06-02 10:51:08 +0200

    Added SqlReaderSample
    Former-commit-id: 97352ce5d64a99a78dcea1f1e2142d0d48b15169

---
+++ Samples/SqlReaderSample/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Hosting;
+using System.Configuration;
+using ImageResizer.Plugins.SqlReader;
+
+namespace DatabaseSampleCSharp
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            //Configure Sql Backend
+            SqlReaderSettings s = new SqlReaderSettings();
+            s.ConnectionString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+            s.PathPrefix = "~/databaseimages";
+            s.ImageIdType = System.Data.SqlDbType.Int;
+            s.ImageBlobQuery = "SELECT Content FROM Images WHERE ImageID=@id";
+            s.ModifiedDateQuery = "Select ModifiedDate, CreatedDate From Images WHERE ImageID=@id";
+            s.ImageExistsQuery = "Select COUNT(ImageID) From Images WHERE ImageID=@id";
+
+            //Register it as a virtual path provider. 
+            HostingEnvironment.RegisterVirtualPathProvider(new SqlReaderPlugin(s));
+        }
+
+       
+    }
+}
