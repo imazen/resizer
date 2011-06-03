b7c67bdca408d45d90301300c3a5d869504efc2e
Nathanael Jones <nathanael.jones@gmail.com>
2011-06-03 11:39:30 +0200

    Added docs
    Former-commit-id: e5b5dab72861f00df2ecc8549f42947e9d12cfe1

--- Samples/ScriptAccess/advancedScript.js
+++ Samples/ScriptAccess/advancedScript.js
@@ -1,15 +1,20 @@
+//The first step is to create a configuration object (where you register plugins, configure stuff, etc.)
 var c = new ActiveXObject("ImageResizer.Configuration.Config");
 
+//This is how you install a plugin
 var p = new ActiveXObject("ImageResizer.Plugins.PrettyGifs.PrettyGifs");
-
 p.Install(c);
 
-
+//This is how you generate an image
 c.BuildImage("..\\Images\\quality-original.jpg","grass.gif", "rotate=3&width=600&format=gif&colors=128");
-//c.WriteDiagnosticsTo("advancedScript-1.txt");
 
+// You can even get diagnostics output if something's not working
+// c.WriteDiagnosticsTo("advancedScript-1.txt");
 
+//You can create different configurations
 var c2 = new ActiveXObject("ImageResizer.Configuration.Config");
 
+//And use them separately
 c2.BuildImage("..\\Images\\quality-original.jpg","grass-ugly.gif", "rotate=3&width=600&format=gif");
+
 //c2.WriteDiagnosticsTo("advancedScript-2.txt");
