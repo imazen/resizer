65823ce8d2acd38f9ea8bde338441150ee334bba
Nathanael Jones <nathanael.jones@gmail.com>
2011-06-04 15:56:21 +0200

    Added support for embedding SHA-1 hashes into each DLL, and adding hotfix notation to Informational version.
    Former-commit-id: 60da8b5612b4560e4673d6fc5778be5a7514a418

---
+++ Tests/Builder/Classes/GitManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+namespace ImageResizer.ReleaseBuilder.Classes {
+    public class GitManager {
+
+        string gitExe = null;
+        string repoRoot = null;
+        public GitManager(string repoRoot) {
+            gitExe = FindGit();
+            this.repoRoot = repoRoot;
+        }
+        /// <summary>
+        /// Searchs all ProgramFiles locations. Looks for \Git\bin\git.exe
+        /// </summary>
+        /// <returns></returns>
+        public string FindGit() {
+            //C:\Program Files\Git\bin\git.exe
+            //"ProgramFiles", "ProgramFiles(x86)","ProgramW6432"
+            string[] vars = new string[] { "ProgramFiles", "ProgramFiles(x86)", "ProgramW6432" };
+            foreach (string v in vars) {
+                string progFiles = Environment.GetEnvironmentVariable(v).TrimEnd('\\', '/');
+                string path = progFiles + @"\Git\bin\git.exe".Replace('\\', System.IO.Path.DirectorySeparatorChar);
+                if (File.Exists(path)) return path;
+            }
+            Console.WriteLine("Failed to find git.exe.");
+            return null;
+
+        }
+
+        public bool CanExecute { get { return repoRoot != null && gitExe != null; } }
+
+        public string GetHeadHash() {
+            return exec("log -1 --pretty=format:%h");
+        }
+
+        public string exec(string command) {
+            var psi = new ProcessStartInfo(gitExe);
+            psi.Arguments = ' ' + command;
+            psi.WorkingDirectory = repoRoot;
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+            
+            psi.RedirectStandardError = true;
+            Console.WriteLine("Executing " + psi.FileName + " " + psi.Arguments);
+            var p = Process.Start(psi);
+            
+            p.WaitForExit();
+            return p.StandardOutput.ReadToEnd();
+        }
+    }
+}
