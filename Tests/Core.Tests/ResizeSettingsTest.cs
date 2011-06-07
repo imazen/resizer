5af8d4a8ecb6598623fe6b2b0e2fc8e81a59c2a6
Nathanael Jones <nathanael.jones@gmail.com>
2011-06-07 10:05:52 +0200

    !Fixed int, float, double parsing bug - Affects all numeric querystring values. width,height,maxwidth,maxheight,shadowWidth, angle, rotate.  The bug caused invalid numbers to default to '0' instead of -1. Didn't affect unspecified values, just those with incorrect syntax.
    Added test for subset of problem.
    
    
    Former-commit-id: 5c517b02d1837d12e9cbd5a00051d1998d86d0ab

--- Tests/Core.Tests/ResizeSettingsTest.cs
+++ Tests/Core.Tests/ResizeSettingsTest.cs
@@ -10,11 +10,19 @@ namespace ImageResizer.Core.Tests {
     [TestFixture]
     public class ResizeSettingsTest {
         [Test]
-        public void Test() {
+        public void TestCropValues() {
             ResizeSettings s = new ResizeSettings();
             s.CropBottomRight = new System.Drawing.PointF(50, 50);
             s.CropTopLeft = new System.Drawing.PointF(0, 0);
             Assert.AreEqual<string>( "?crop=(0,0,50,50)", s.ToString());
+        }
+
+
+        [Test]
+        public void TestIntParsing() {
+            ResizeSettings s = new ResizeSettings("maxwidth=100px&maxheight=50.6");
+            Assert.AreEqual<int>(-1, s.MaxWidth);
+            Assert.AreEqual<int>(-1,s.MaxHeight);
         }
     }
 }
