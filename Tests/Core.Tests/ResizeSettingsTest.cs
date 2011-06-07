38b56b3488a053e86ec31d381a868c1fb1ff344d
Nathanael Jones <nathanael.jones@gmail.com>
2011-06-07 08:52:37 +0200

    Added unit test for ResizeSettings
    Former-commit-id: 5b103468ba089bbb839030c4848b1ab27bfff2aa

---
+++ Tests/Core.Tests/ResizeSettingsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+
+namespace ImageResizer.Core.Tests {
+    [TestFixture]
+    public class ResizeSettingsTest {
+        [Test]
+        public void Test() {
+            ResizeSettings s = new ResizeSettings();
+            s.CropBottomRight = new System.Drawing.PointF(50, 50);
+            s.CropTopLeft = new System.Drawing.PointF(0, 0);
+            Assert.AreEqual<string>( "?crop=(0,0,50,50)", s.ToString());
+        }
+    }
+}
