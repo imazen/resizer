7084f02890cb246a8842b9fc9ce6647b13b53fc6
Nathanael Jones <nathanael.jones@gmail.com>
2011-06-16 11:59:18 +0200

    Added upload and generate version sample code.

--- Samples/ComplexWebApplication/UploadSample.aspx.cs
+++ Samples/ComplexWebApplication/UploadSample.aspx.cs
@@ -19,7 +19,7 @@ namespace ComplexWebApplication {
             //Loop through each uploaded file
             foreach (string fileKey in HttpContext.Current.Request.Files.Keys) {
                 HttpPostedFile file = HttpContext.Current.Request.Files[fileKey];
-                if (file.ContentLength <= 0) continue; //Yep, it happens all the time
+                if (file.ContentLength <= 0) continue; //Skip unused file controls.
 
                 //Get the physical path for the uploads folder
                 string uploadFolder = MapPath("~/uploads");
@@ -33,12 +33,9 @@ namespace ComplexWebApplication {
                 //Generate a filename (GUIDs are safest).
                 string fileName = Path.Combine(uploadFolder, System.Guid.NewGuid().ToString());
 
-                //What final type of file will we have? This can depend on whether the resizeCropSettings sepcifies a format,
-                // and what the original format was (if resizecropSettings doesn't specify one)
-                fileName += "." + ImageBuilder.Current.EncoderProvider.GetEncoder(resizeCropSettings, fileUpload.PostedFile.FileName).Extension;
+                //Let the image builder add the correct extension based on the output file type (which may differ from the original file type).
+                fileName = ImageBuilder.Current.Build(file, fileName, resizeCropSettings, false, true);
 
-                //Resize the image
-                ImageBuilder.Current.Build(file, fileName, resizeCropSettings);
             }
 
             //Here's an example of getting a byte array for sending to SQL
@@ -57,6 +54,55 @@ namespace ComplexWebApplication {
             //        //Upload the byte array to SQL: ms.ToArray();
             //    }
             //}
+        }
+
+        protected void btnUploadAndGenerate_Click(object sender, EventArgs e) {
+            Dictionary<string, string> versions = new Dictionary<string, string>();
+            //Define the version to generate
+            versions.Add("_thumb", "width=100&height=100&crop=auto&format=jpg"); //Crop to square thumbnail
+            versions.Add("_medium", "maxwidth=400&maxheight=400format=jpg"); //Fit inside 400x400 area, jpeg
+            versions.Add("_large", "maxwidth=1900&maxheight=1900&format=jpg"); //Fit inside 1900x1200 area
+
+            //Loop through each uploaded file
+            foreach (string fileKey in HttpContext.Current.Request.Files.Keys) {
+                HttpPostedFile file = HttpContext.Current.Request.Files[fileKey];
+                if (file.ContentLength <= 0) continue; //Skip unused file controls.
+
+                //Get the physical path for the uploads folder and make sure it exists
+                string uploadFolder = MapPath("~/uploads");
+                if (!Directory.Exists(uploadFolder)) Directory.CreateDirectory(uploadFolder);
+
+                //Generate each version
+                foreach (string suffix in versions.Keys) {
+                    //Generate a filename (GUIDs are best).
+                    string fileName = Path.Combine(uploadFolder, System.Guid.NewGuid().ToString() + suffix);
+
+                    //Let the image builder add the correct extension based on the output file type
+                    fileName = ImageBuilder.Current.Build(file, fileName, new ResizeSettings(versions[suffix]), false, true);
+                }
+
+            }
+        }
+
+        public IList<string> GenerateVersions(string original) {
+            Dictionary<string, string> versions = new Dictionary<string, string>();
+            //Define the versions to generate and their filename suffixes.
+            versions.Add("_thumb", "width=100&height=100&crop=auto&format=jpg"); //Crop to square thumbnail
+            versions.Add("_medium", "maxwidth=400&maxheight=400format=jpg"); //Fit inside 400x400 area, jpeg
+            versions.Add("_large", "maxwidth=1900&maxheight=1900&format=jpg"); //Fit inside 1900x1200 area
+
+
+            string basePath = ImageResizer.Util.PathUtils.RemoveExtension(original);
+
+            //To store the list of generated paths
+            List<string> generatedFiles = new List<string>();
+
+            //Generate each version
+            foreach (string suffix in versions.Keys)
+                //Let the image builder add the correct extension based on the output file type
+                generatedFiles.Add(ImageBuilder.Current.Build(original, basePath + suffix, new ResizeSettings(versions[suffix]), false, true));
+
+            return generatedFiles;   
         }
 
       
