8171dcf10c118889ce6f9cf60d448108e90b786b
Jose Carlos <jcarballomiret@gmail.com>
2011-06-24 13:13:00 -0400

    - BackgroundWorkers for export and replace existing save modes has been moved to a class. - Double confirmation of zip file exists and override is fixed. - Switched ResizeAndPad and ResizeAndCrop fixed.
    Former-commit-id: 4dc24214d9333c276c8fadf3debe7d435fe05b97

--- Samples/ImageResizerGUI/Code/SaveMode.cs
+++ Samples/ImageResizerGUI/Code/SaveMode.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ImageResizerGUI
+﻿namespace ImageResizerGUI
 {
     /// <summary>
     /// Enumerate the saving modes for the UI.
