df6377265f139ecc44ae079ff843dbc0ca8e5aa7
Nathanael Jones <nathanael.jones@gmail.com>
2011-07-22 18:04:53 +0200

    Added first draft of RemoteReader plugin, and fixed some other project's build configurations for Trial mode.
    Former-commit-id: 2583025766d5dc054304017273802dc098b2774f

---
+++ Plugins/RemoteReader/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImageResizer.Plugins.RemoteReader")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("18c798fe-096c-4af5-8992-d2304f24c9f4")]
---
+++ Plugins/RemoteReader/RemoteReaderPlugin.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Specialized;
+using ImageResizer.Configuration;
+using System.Net;
+using ImageResizer.Util;
+using System.Security.Cryptography;
+using ImageResizer.Configuration.Issues;
+using System.IO;
+
+namespace ImageResizer.Plugins.RemoteReader {
+
+    public class RemoteReaderPlugin:IPlugin, IVirtualImageProvider, IIssueProvider {
+
+        private static string base64UrlKey = "urlb64";
+
+        public static string Base64UrlKey
+        {
+          get { return RemoteReaderPlugin.base64UrlKey; }
+        }
+        private static string hmacKey = "hmac";
+
+public static string HmacKey
+{
+  get { return RemoteReaderPlugin.hmacKey; }
+}
+
+        protected string remotePrefix = "~/remote";
+        Config c;
+        public RemoteReaderPlugin() {
+            remotePrefix = Util.PathUtils.ResolveAppRelative(remotePrefix);
+        }
+
+        public IPlugin Install(Configuration.Config c) {
+            this.c = c;
+            c.Plugins.add_plugin(this);
+            c.Pipeline.RewriteDefaults += Pipeline_RewriteDefaults;
+            c.Pipeline.PostRewrite += Pipeline_PostRewrite;
+            return this;
+        }
+
+
+        public bool Uninstall(Configuration.Config c) {
+            c.Plugins.remove_plugin(this);
+            c.Pipeline.RewriteDefaults -= Pipeline_RewriteDefaults;
+            c.Pipeline.PostRewrite -= Pipeline_PostRewrite;
+            return true;
+        }
+
+
+        void Pipeline_RewriteDefaults(System.Web.IHttpModule sender, System.Web.HttpContext context, IUrlEventArgs e) {
+            //Set the XXX of /remote.XXX to the real extension used by the remote file.
+            //Allows the output extension and mime-type default to be determined correctly
+            if (IsRemotePath(e.VirtualPath) && 
+                    !string.IsNullOrEmpty(e.QueryString[Base64UrlKey])) {
+                string ext = PathUtils.GetExtension(PathUtils.FromBase64UToString(e.QueryString[Base64UrlKey]));
+                e.VirtualPath = PathUtils.SetExtension(e.VirtualPath, ext);
+            }
+        }
+
+        void Pipeline_PostRewrite(System.Web.IHttpModule sender, System.Web.HttpContext context, IUrlEventArgs e) {
+            if (IsRemotePath(e.VirtualPath)) {
+                //Force images to be processed - don't allow them to only cache it.
+                e.QueryString["process"] = ProcessWhen.Always.ToString();
+            }
+        }
+
+
+        /// <summary>
+        /// Returns the currently registered RemoteReaderPlugin, or 'null'.
+        /// </summary>
+        public static RemoteReaderPlugin Current {
+            get {
+                return Config.Current.Plugins.Get<RemoteReaderPlugin>();
+            }
+        }
+
+        /// <summary>
+        /// Generates a signed domain-relative URL in the form "/app/remote.jpg.ashx?width=200&urlb64=aHnSh3haSh...&hmac=913f3KJGK3hj"
+        /// </summary>
+        /// <param name="remoteUrl"></param>
+        /// <param name="settings"></param>
+        /// <returns></returns>
+        public string CreateSignedUrl(string remoteUrl, NameValueCollection settings) {
+            settings[Base64UrlKey] = PathUtils.ToBase64U(remoteUrl);
+            settings[HmacKey] = SignData(settings[Base64UrlKey]);
+            return remotePrefix + ".jpg.ashx" + PathUtils.BuildQueryString(settings);
+        }
+
+        public string SignData(string data) {
+
+            string key = c.get("remoteReader.signingKey", String.Empty);
+            if (string.IsNullOrEmpty(key)) throw new ImageResizer.ImageProcessingException("You are required to set a passphrase for securing remote URLs. <resizer><remotereader signingKey=\"put a long and randam passphrase here\" /> </resizer>");
+
+            HMACSHA256 hmac = new HMACSHA256(UTF8Encoding.UTF8.GetBytes(key));
+            byte[] hash = hmac.ComputeHash(UTF8Encoding.UTF8.GetBytes(data));
+            return PathUtils.ToBase64U(hash);
+        }
+
+        public RemoteRequestEventArgs ParseRequest(string virtualPath, NameValueCollection query) {
+            if (!IsRemotePath(virtualPath)) return null;
+
+            RemoteRequestEventArgs args = new RemoteRequestEventArgs();
+            args.SignedRequest = false;
+            args.QueryString = query;
+
+            if (!string.IsNullOrEmpty(query[Base64UrlKey])) {
+                string data = query[Base64UrlKey];
+                string hmac = query[HmacKey];
+                query.Remove(Base64UrlKey);
+                query.Remove(HmacKey);
+                if (!SignData(data).Equals(hmac)) 
+                    throw new ImageProcessingException("Invalid request! This request was not properly signed, or has been tampered with since transmission.");
+                args.RemoteUrl = PathUtils.FromBase64UToString(data);
+                args.SignedRequest = true;
+            } else
+                args.RemoteUrl = "http" + virtualPath.Substring(remotePrefix.Length).TrimStart('/', '\\');
+  
+            if (!Uri.IsWellFormedUriString(args.RemoteUrl, UriKind.Absolute))
+                throw new ImageProcessingException("Invalid request! The specified Uri is invalid: " + args.RemoteUrl);
+            return args;
+        }
+
+
+        public bool IsRemotePath(string virtualPath) {
+            return (virtualPath.StartsWith(remotePrefix, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public bool FileExists(string virtualPath, System.Collections.Specialized.NameValueCollection queryString) {
+            return IsRemotePath(virtualPath);
+        }
+
+        public event RemoteRequest AllowRemoteRequest;
+
+        public IVirtualFile GetFile(string virtualPath, System.Collections.Specialized.NameValueCollection queryString) {
+            RemoteRequestEventArgs request = ParseRequest(virtualPath, queryString);
+            if (request == null) throw new FileNotFoundException();
+
+            if (request.SignedRequest && c.get("remotereader.allowAllSignedRequests", false)) request.DenyRequest = false;
+
+            //Fire event
+            if (AllowRemoteRequest != null) AllowRemoteRequest(this, request);
+
+            if (request.DenyRequest) throw new ImageProcessingException(403, "The specified remote URL is not permitted.");
+
+            return new RemoteSiteFile(virtualPath,request,this);
+        }
+
+        public IEnumerable<IIssue> GetIssues() {
+            List<IIssue> issues = new List<IIssue>();
+            string key = c.get("remoteReader.signingKey", String.Empty);
+            if (string.IsNullOrEmpty(key))
+                issues.Add(new Issue("You are required to set a passphrase for securing remote URLs. Example: <resizer><remotereader signingKey=\"put a long and randam passphrase here\" /> </resizer>"));
+            return issues;
+
+        }
+    }
+
+    public class RemoteSiteFile : IVirtualFile {
+
+        protected string virtualPath;
+        protected RemoteReaderPlugin parent;
+        private RemoteRequestEventArgs request;
+
+        public RemoteSiteFile(string virtualPath, RemoteRequestEventArgs request, RemoteReaderPlugin parent) {
+            this.virtualPath = virtualPath;
+            this.request = request;
+            this.parent = parent;
+        }
+
+        public string VirtualPath {
+            get { return virtualPath; }
+        }
+
+        public System.IO.Stream Open() {
+            HttpWebResponse response = null;
+            try {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(new Uri(this.request.RemoteUrl));
+                request.Timeout = 15000; //Default to 15 seconds. Browser timeout is usually 30.
+
+                //This is IDisposable, but only disposes the stream we are returning. So we can't dispose it, and don't need to
+                response = request.GetResponse() as HttpWebResponse;
+                return response.GetResponseStream();
+            } catch {
+                if (response != null) response.Close();
+                throw;
+            }
+
+        }
+    }
+}
