cabd56f70ebbf0b26178431ea22b007fea72d48b
Nathanael Jones <nathanael.jones@gmail.com>
2011-08-02 17:49:59 +0200

    Fixed bug -  Global.asax.cs referenced asp.net 4.0 assemblies

--- Samples/SampleAspSite/global.asax.cs
+++ Samples/SampleAspSite/global.asax.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
 using ImageResizer;
 using System.Drawing;
 using ImageResizer.Configuration;
