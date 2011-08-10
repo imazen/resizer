4e65e090c831c2d5259d9ef942f0824a5429bd25
Nathanael Jones <nathanael.jones@gmail.com>
2011-08-10 20:53:51 +0200

    Working on COM installer - still broken
    Former-commit-id: 263bc57efdadb2a8b093db871cae418ebbb9a1f4

--- Samples/ScriptAccess/unregisterdll.bat
+++ Samples/ScriptAccess/unregisterdll.bat
@@ -4,12 +4,19 @@ cd /d %batchdir%
 cd ..
 cd ..
 cd dlls
-cd release
-set dllpath=%cd%\ImageResizer.dll
+cd debug
 
-echo Path: %dllpath%
+set regasm32=%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\regasm.exe
+set regasm64=%SystemRoot%\Microsoft.NET\Framework64\v2.0.50727\regasm.exe
 
+echo Path:%cd%
+dir /B ImageResizer*.dll
 
 pause
-%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\regasm.exe %dllpath% /codebase /u
+
+
+FOR /f %%G IN ('dir /B ImageResizer*.dll') DO %regasm32% "%cd%\%%G" /codebase /u
+
+IF EXIST %regasm64% FOR /F %%G IN ('dir /B ImageResizer*.dll') DO %regasm64% "%cd%\%%G" /codebase .u
+
 pause 
