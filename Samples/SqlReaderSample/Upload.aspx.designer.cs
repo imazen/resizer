008edf65a0ea6774c3fb37d165a510ece33f223e
Nathanael Jones <nathanael.jones@gmail.com>
2011-08-19 18:21:01 +0200

    SqlReaderSample: Added support for uploading files as-is

--- Samples/SqlReaderSample/Upload.aspx.designer.cs
+++ Samples/SqlReaderSample/Upload.aspx.designer.cs
@@ -47,5 +47,14 @@ namespace DatabaseSampleCSharp {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.FileUpload FileUpload3;
+        
+        /// <summary>
+        /// btnUploadAsIs control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnUploadAsIs;
     }
 }
