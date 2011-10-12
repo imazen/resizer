34981f922159513d139d3116dba74b91bf090e8e
Nathanael Jones <nathanael.jones@gmail.com>
2011-10-12 02:56:49 +0200

    First draft of new Watermark plugin.
    Former-commit-id: 2ea33e2b7df9e66adf19bcb47b4b788d952cf913

---
+++ Plugins/Watermark/TextLayer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Specialized;
+using System.Drawing;
+using ImageResizer.Util;
+
+namespace ImageResizer.Plugins.Watermark {
+    public class TextLayer:Layer {
+
+        public TextLayer(){}
+        public TextLayer(NameValueCollection attrs)
+            : base(attrs) {
+                Text = attrs["text"];
+                Vertical = "true".Equals(attrs["vertical"], StringComparison.OrdinalIgnoreCase);
+                TextColor = Utils.parseColor(attrs["color"], Color.Black);
+        }
+
+        private string _text = "";
+        /// <summary>
+        /// The text to display
+        /// </summary>
+        public string Text {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+        private bool _vertical = false;
+        /// <summary>
+        /// If true, text will be displayed verticallt
+        /// </summary>
+        public bool Vertical {
+            get { return _vertical; }
+            set { _vertical = value; }
+        }
+
+
+        private Color _textColor = Color.Black;
+        /// <summary>
+        /// The color to draw the text
+        /// </summary>
+        public Color TextColor {
+            get { return _textColor; }
+            set { _textColor = value; }
+        }
+
+        public Font GetFont() {
+            return new Font(FontFamily.GenericSansSerif, 12, FontStyle.Bold);
+        }
+        public StringFormat GetFormat() {
+            System.Drawing.StringFormat drawFormat = new System.Drawing.StringFormat();
+            if (Vertical) drawFormat.FormatFlags = StringFormatFlags.DirectionVertical;
+            return drawFormat;
+        }
+        public Brush GetBrush() {
+            return new SolidBrush(TextColor);
+        }
+
+        public override void RenderTo(Resizing.ImageState s) {
+            if (string.IsNullOrEmpty(Text)) return;
+             RectangleF bounds = this.CalculateLayerCoordinates(s, delegate(double maxwidth, double maxheight) {
+                 using (Font f= GetFont()){
+                    return PolygonMath.RoundPoints(s.destGraphics.MeasureString(Text,f,new PointF(),GetFormat()));
+                 }
+             }, true);
+            using(Font f = GetFont()){
+                using (Brush b = GetBrush()) {
+                    s.destGraphics.DrawString(Text, f, b, bounds.Location, GetFormat());
+                }
+            }
+        }
+    }
+}
