4f8b04d5ccf5555ad113787471504d46bcf554ac
Nathanael Jones <nathanael.jones@gmail.com>
2011-10-15 21:29:45 +0200

    Diagnostic: Added unused plugin assembly detection.

--- Core/Plugins/Basic/DiagnosticPageHandler.cs
+++ Core/Plugins/Basic/DiagnosticPageHandler.cs
@@ -74,30 +74,58 @@ namespace ImageResizer.Plugins.Basic {
 
 			sb.AppendLine("IntegratedPipeline=" + (HttpRuntime.UsingIntegratedPipeline).ToString());
 
-
+            //List loaded assemblies, and also detect plugin assemblies that are not being used.
 			sb.AppendLine("\nLoaded assemblies:\n");
+
+            StringBuilder unusedPlugins = new StringBuilder();
+            Dictionary<string, bool> usedAssemblies = new Dictionary<string,bool>(StringComparer.OrdinalIgnoreCase);
+            foreach (IPlugin p in c.Plugins.AllPlugins) 
+                usedAssemblies[p.GetType().Assembly.FullName] = true;
+            
 			Assembly[] asms = AppDomain.CurrentDomain.GetAssemblies();
 			foreach (Assembly a in asms) {
+                StringBuilder asb = new StringBuilder();
+
 				AssemblyName assemblyName = new AssemblyName(a.FullName);
-				sb.Append(assemblyName.Name.PadRight(40, ' '));
+				asb.Append(assemblyName.Name.PadRight(40, ' '));
 
-				sb.Append(" Assembly: " + assemblyName.Version.ToString().PadRight(15));
-
+				asb.Append(" Assembly: " + assemblyName.Version.ToString().PadRight(15));
 
 
                 object[] attrs;
                 
 				attrs = a.GetCustomAttributes(typeof(AssemblyFileVersionAttribute), false);
-				if (attrs != null && attrs.Length > 0) sb.Append(" File: " + ((AssemblyFileVersionAttribute)attrs[0]).Version.PadRight(15));
+				if (attrs != null && attrs.Length > 0) asb.Append(" File: " + ((AssemblyFileVersionAttribute)attrs[0]).Version.PadRight(15));
 
 				attrs = a.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false);
-				if (attrs != null && attrs.Length > 0) sb.Append(" Info: " + ((AssemblyInformationalVersionAttribute)attrs[0]).InformationalVersion);
+				if (attrs != null && attrs.Length > 0) asb.Append(" Info: " + ((AssemblyInformationalVersionAttribute)attrs[0]).InformationalVersion);
 
 				attrs = a.GetCustomAttributes(typeof(CommitAttribute), false);
-				if (attrs != null && attrs.Length > 0) sb.Append("  Commit: " + ((CommitAttribute)attrs[0]).Value);
+				if (attrs != null && attrs.Length > 0) asb.Append("  Commit: " + ((CommitAttribute)attrs[0]).Value);
 
-				sb.AppendLine();
+				asb.AppendLine();
+
+
+                if (assemblyName.Name.StartsWith("ImageResizer.Plugins", StringComparison.OrdinalIgnoreCase) && !usedAssemblies.ContainsKey(a.FullName)) {
+                    unusedPlugins.Append(asb.ToString());
+                }
+               
+                sb.Append(asb.ToString());
 			}
+
+            if (unusedPlugins.Length > 0) {
+                sb.AppendLine("\nThe following plugin assemblies are loaded but do not seem to be in use. " +
+                    "You should remove them (and especially their dependencies (unless used elsewhere)) from the /bin folder to improve application load times:\n");
+                sb.Append(unusedPlugins.ToString());
+
+                sb.AppendLine("\nReference list of plugin dependencies - so you know what additional dlls to remove when removing a plugin. (may not be up-to-date):\n");
+                sb.AppendLine("The FreeImage plugin has the following dependencies: FreeImage.dll and FreeImageNet.dll\n" +
+                    "The Logging plugin depends on: NLog.dll\n" +
+                    "The Watermark and WhitespaceTrimmer plugins depend on: AForge.dll, AForge.Math.dll, AForge.Imaging.dll, and AForge.Imaging.Formats.dll\n" +
+                    "The PsdReader plugin depends on: PsdFile.dll\n" +
+                    "The S3Reader plugin depends on: LitS3.dll\n" +
+                    "The BatchZipper plugin depends on: Ionic.Zip.Reduced.dll\n");
+            }
 			return sb.ToString();
 
 		}
