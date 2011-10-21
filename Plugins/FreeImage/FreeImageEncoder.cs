aafa22167c57fd3d8799994945eb77560583ed1a
Nathanael Jones <nathanael.jones@gmail.com>
2011-10-21 20:35:05 +0200

    FreeImageEncoder: No longer acts as default. Only encodes if &encoder=freeimage is specified. Added support for &quality=10..100, &subsampling=411|420|422|444  and &progressive=true|false. PNG and GIF support is still terrible.
    Former-commit-id: 19c015bac391c61514b4defa4044abf8eee30fa9

--- Plugins/FreeImage/FreeImageEncoder.cs
+++ Plugins/FreeImage/FreeImageEncoder.cs
@@ -9,6 +9,11 @@ using System.Drawing.Imaging;
 
 namespace ImageResizer.Plugins.FreeImageEncoder {
     public class FreeImageEncoderPlugin : IPlugin, IEncoder {
+
+        FREE_IMAGE_FORMAT format = FREE_IMAGE_FORMAT.FIF_JPEG;
+        FREE_IMAGE_SAVE_FLAGS encodingOptions = FREE_IMAGE_SAVE_FLAGS.DEFAULT;
+   
+
         public FreeImageEncoderPlugin(ResizeSettings settings, object original) {
             ImageFormat originalFormat = DefaultEncoder.GetOriginalFormat(original);
             if (!IsValidOutputFormat(originalFormat)) originalFormat = ImageFormat.Jpeg;//No valid info available about the original format. Use Jpeg.
@@ -19,11 +24,37 @@ namespace ImageResizer.Plugins.FreeImageEncoder {
                 throw new ArgumentException("An unrecognized or unsupported output format (" + (settings.Format != null ? settings.Format : "(null)") + ") was specified in 'settings'.");
             this.format =  FreeImage.GetFormat(requestedFormat);
 
-            //TODO: parse quality settings.
+            //Parse JPEG settings.
+            int quality = 90;
+            if (string.IsNullOrEmpty(settings["quality"]) || !int.TryParse(settings["quality"], out quality)) quality = 90;
+            if (format == FREE_IMAGE_FORMAT.FIF_JPEG) {
+                if (quality >= 100) encodingOptions |= FREE_IMAGE_SAVE_FLAGS.JPEG_QUALITYSUPERB;
+                else if (quality >= 75) encodingOptions |= FREE_IMAGE_SAVE_FLAGS.JPEG_QUALITYGOOD;
+                else if (quality >= 50) encodingOptions |= FREE_IMAGE_SAVE_FLAGS.JPEG_QUALITYNORMAL;
+                else if (quality >= 25) encodingOptions |= FREE_IMAGE_SAVE_FLAGS.JPEG_QUALITYAVERAGE;
+                else encodingOptions |= FREE_IMAGE_SAVE_FLAGS.JPEG_QUALITYBAD;
+
+
+                if ("true".Equals(settings["progressive"])) encodingOptions |= FREE_IMAGE_SAVE_FLAGS.JPEG_PROGRESSIVE;
+
+                if ("411".Equals(settings["subsampling"])) encodingOptions |= FREE_IMAGE_SAVE_FLAGS.JPEG_SUBSAMPLING_411;
+                if ("420".Equals(settings["subsampling"])) encodingOptions |= FREE_IMAGE_SAVE_FLAGS.JPEG_SUBSAMPLING_420;
+                if ("422".Equals(settings["subsampling"])) encodingOptions |= FREE_IMAGE_SAVE_FLAGS.JPEG_SUBSAMPLING_422;
+                if ("444".Equals(settings["subsampling"])) encodingOptions |= FREE_IMAGE_SAVE_FLAGS.JPEG_SUBSAMPLING_444;
+            }
+            if (string.IsNullOrEmpty(settings["colors"]) || !int.TryParse(settings["colors"], out colors)) colors = -1;
+
+
+            if (format == FREE_IMAGE_FORMAT.FIF_GIF) {
+                //encodingOptions = FREE_IMAGE_SAVE_FLAGS.
+            }
+            
+
         }
+        int colors = -1;
 
         public FreeImageEncoderPlugin() {
-            // TODO: Complete member initialization
+           
         }
 
 
@@ -41,6 +72,7 @@ namespace ImageResizer.Plugins.FreeImageEncoder {
             if (!FreeImage.IsAvailable()) return null;
             ImageFormat requestedFormat = DefaultEncoder.GetRequestedFormat(settings.Format, ImageFormat.Jpeg);
             if (requestedFormat == null || !IsValidOutputFormat(requestedFormat)) return null; //An unsupported format was explicitly specified.
+            if (!"freeimage".Equals(settings["encoder"], StringComparison.OrdinalIgnoreCase)) return null;
             return new FreeImageEncoderPlugin(settings, original);
         }
 
@@ -54,14 +86,18 @@ namespace ImageResizer.Plugins.FreeImageEncoder {
         }
 
 
-        FREE_IMAGE_FORMAT format = FREE_IMAGE_FORMAT.FIF_JPEG;
-        FREE_IMAGE_SAVE_FLAGS encodingOptions = FREE_IMAGE_SAVE_FLAGS.DEFAULT;
-   
         public void Write(System.Drawing.Image i, System.IO.Stream s) {
             if (!(i is Bitmap)) throw new ArgumentException("FreeImageEncoder only works with bitmaps");
             FIBITMAP bit = FreeImage.CreateFromBitmap(i as Bitmap);
             if (bit.IsNull) throw new ImageProcessingException("FreeImageEncoder failed to convert System.Drawing.Bitmap to FIBITMAP");
-
+            if (format == FREE_IMAGE_FORMAT.FIF_GIF || ( format == FREE_IMAGE_FORMAT.FIF_PNG && colors != -1)){
+                FreeImage.SetTransparent(bit, true);
+                FIBITMAP old = bit;
+                //TODO - ColorQuantizeEx returns null no matter what we do.. Is it because the image is 32-bit?
+                bit = FreeImage.ColorQuantizeEx(bit, FREE_IMAGE_QUANTIZE.FIQ_NNQUANT, 256,1, RGBQUAD.ToRGBQUAD(new Color[] { Color.Transparent }));
+                if (bit.IsNull) bit = old;
+                else if (bit != old) FreeImage.UnloadEx(ref old);
+            }
             FreeImage.SaveToStream(ref bit, s, format, encodingOptions, true);
         }
 
