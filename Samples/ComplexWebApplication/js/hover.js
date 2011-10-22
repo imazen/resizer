7dddf9ad220de4c7baf89b6d9d23008068399992
Nathanael Jones <nathanael.jones@gmail.com>
2011-10-23 01:34:45 +0200

    ComplexWebApplication: Added Filters.htm to test and compare SimpleFilters and AdvancedFilters
    Former-commit-id: 1c89e6e58717929f12f330695dacd8f93ce1d939

---
+++ Samples/ComplexWebApplication/js/hover.js
@@ -0,0 +1,36 @@
+﻿this.imagePreview = function () {
+    /* CONFIG */
+
+    xOffset = 10;
+    yOffset = 30;
+
+    // these 2 variable determine popup's distance from the cursor
+    // you might want to adjust to get the right result
+
+    /* END CONFIG */
+    $("a.preview").hover(function (e) {
+        this.t = this.title;
+        this.title = "";
+        var c = (this.t != "") ? "<br/>" + this.t : "";
+        $("body").append("<p id='preview'><img src='" + this.href + "' alt='Image preview' />" + c + "</p>");
+        $("#preview")
+			.css("top", (e.pageY - xOffset) + "px")
+			.css("left", (e.pageX + yOffset) + "px")
+			.fadeIn("fast");
+    },
+	function () {
+	    this.title = this.t;
+	    $("#preview").remove();
+	});
+    $("a.preview").mousemove(function (e) {
+        $("#preview")
+			.css("top", (e.pageY - xOffset) + "px")
+			.css("left", (e.pageX + yOffset) + "px");
+    });
+};
+
+
+// starting the script on page load
+$(document).ready(function () {
+    imagePreview();
+});
