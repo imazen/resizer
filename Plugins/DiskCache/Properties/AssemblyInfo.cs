7b94b0464bcb20250b7e6b58abde15866c41a4a4
Nathanael Jones <nathanael.jones@gmail.com>
2011-11-29 13:40:39 -0500

    Added metadata to plugins with bundle information

--- Plugins/DiskCache/Properties/AssemblyInfo.cs
+++ Plugins/DiskCache/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
+using ImageResizer.Util;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -10,3 +10,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c5c6c0da-22c0-4c1e-8871-7cf1610d81b1")]
 
+
+// So the ImageResizer knows which bundle this assembly belongs to
+[assembly: Bundle("R3Bundle1")]
