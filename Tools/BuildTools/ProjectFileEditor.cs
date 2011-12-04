d87ccbd53ec2b291a57d46ea462545efd67343ae
Nathanael Jones <nathanael.jones@gmail.com>
2011-12-04 11:47:25 -0500

    Builder: Fixed project file editing code

--- Tools/BuildTools/ProjectFileEditor.cs
+++ Tools/BuildTools/ProjectFileEditor.cs
@@ -21,20 +21,34 @@ namespace BuildTools {
         public void ReplaceAllProjectReferencesWithDllReferences(string dllFolder) {
 
             XDocument d = XDocument.Load(Path);
-            
+            XNamespace ns = d.Root.Name.NamespaceName;
             bool didsomething = false;
-            //if (d.Descendants("{http://schemas.microsoft.com/developer/msbuild/2003}ProjectReference").Count() < 1) return;
 
-            foreach (XElement e in d.Descendants("{http://schemas.microsoft.com/developer/msbuild/2003}ProjectReference").ToList()) {
-                string name = e.Descendants("{http://schemas.microsoft.com/developer/msbuild/2003}Name").First().Value;
 
+            // A search which finds the ItemGroup which has Reference 
+            // elements and returns the ItemGroup XElement.
+            XElement referenceGroup = d.Descendants().Where(p => p.Name.LocalName == "ItemGroup"
+                && p.Descendants().First<XElement>().Name.LocalName == "Reference").FirstOrDefault<XElement>();
+
+            //Create it if it is missing
+            if (referenceGroup == default(XElement)){
+                referenceGroup = new XElement(ns + "ItemGroup");
+                d.Root.Add(referenceGroup);
+            }
+
+
+            foreach (XElement e in d.Descendants(ns + "ProjectReference").ToList()) {
+                string name = e.Descendants(ns + "Name").First().Value;
                 
-                var replacement = new XElement("Reference");
+
+                XElement replacement = new XElement(ns + "Reference",
+                    new XElement(ns + "SpecificVersion", bool.FalseString),
+                    new XElement(ns + "HintPath", System.IO.Path.Combine(dllFolder,name + ".dll")));
+
+                referenceGroup.Add(replacement);
                 replacement.SetAttributeValue("Include",name);
-                var path = new XElement("HintPath");
-                path.SetValue(System.IO.Path.Combine(dllFolder,name + ".dll"));
-                replacement.Add(path);
-                e.ReplaceWith(replacement);
+
+                e.Remove();
                 didsomething = true;
             }
             if (didsomething) d.Save(Path);
