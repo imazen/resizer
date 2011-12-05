fc2c72a97c8f0445f5de7d5f463052b28818381f
Nathanael Jones <nathanael.jones@gmail.com>
2011-12-05 01:21:27 -0500

    VirtualFolder: Fixed fallback path consolidation to support multiple \..\..\ segments
    Former-commit-id: 7e27f8e4378ea5b66fc1d7427cadd6e02dd74b45

--- Core/Plugins/Basic/VirtualFolder.cs
+++ Core/Plugins/Basic/VirtualFolder.cs
@@ -125,22 +125,31 @@ namespace ImageResizer.Plugins.Basic {
             }
         }
 
-        protected string collapsePath(string path){
+        protected string collapsePath(string path) {
             string oldPath = path;
-            do{
+            do {
                 oldPath = path;
                 path = collapseOneLevel(oldPath);
-            }while(oldPath != path);
+            } while (oldPath != path);
             return path;
         }
 
         protected string collapseOneLevel(string path) {
-            int up = path.IndexOf(Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar);
-            if (up < 0) return path;
-            int prevSlash = path.LastIndexOf(Path.DirectorySeparatorChar, up - 1);
-            if (prevSlash > -1) {
-                return path.Substring(0, prevSlash) + path.Substring(up + 3);
-            }
+            int up = path.Length - 1;
+            do {
+                up = path.LastIndexOf(System.IO.Path.DirectorySeparatorChar + ".." + System.IO.Path.DirectorySeparatorChar, up);
+                if (up < 0) return path;
+                int prevSlash = path.LastIndexOf(System.IO.Path.DirectorySeparatorChar, up - 1);
+                if (prevSlash < 0) return path;
+                string segment = path.Substring(prevSlash + 1, up - prevSlash - 1);
+                if (segment.Equals("..", StringComparison.OrdinalIgnoreCase)) {
+                    //We can't combine \..\..\, just keep looking closer to the beginning of the string. We already adjusted 'up'
+                } else if (segment.Equals(".", StringComparison.OrdinalIgnoreCase)) {
+                    return path.Substring(0, prevSlash) + path.Substring(up); //Just remove \.\ sections
+                } else {
+                    return path.Substring(0, prevSlash) + path.Substring(up + 3); //If it's not \.\ or \..\, remove both it and the following \..\
+                }
+            } while (up > 0);
             return path;
         }
         /// <summary>
