47c3c3368928a1974958f0b8587c0456724eb8be
Nathanael Jones <nathanael.jones@gmail.com>
2011-12-07 03:45:45 -0500

    Got primitive MvcSample working

--- Samples/MvcSample/Global.asax.cs
+++ Samples/MvcSample/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using ImageResizer.Configuration;
+using ImageResizer.Util;
 
 namespace MvcSample {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
@@ -20,9 +22,19 @@ namespace MvcSample {
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Photos", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
+            Config.Current.Pipeline.Rewrite += new UrlRewritingEventHandler(Pipeline_Rewrite);
+            Config.Current.Pipeline.AuthorizeImage += new UrlAuthorizationEventHandler(Pipeline_AuthorizeImage);
+        }
 
+        static void Pipeline_AuthorizeImage(IHttpModule sender, HttpContext context, IUrlAuthorizationEventArgs e) {
+            if (e.VirtualPath.StartsWith(PathUtils.ResolveAppRelative("~/App_Data/photos/"), StringComparison.OrdinalIgnoreCase)) e.AllowAccess = true;
+        }
+
+        static void Pipeline_Rewrite(IHttpModule sender, HttpContext context, IUrlEventArgs e) {
+            if (e.VirtualPath.StartsWith(PathUtils.ResolveAppRelative("~/photos/"), StringComparison.OrdinalIgnoreCase)) 
+                e.VirtualPath = PathUtils.ResolveAppRelative("~/App_Data/") + e.VirtualPath.Substring(PathUtils.AppVirtualPath.Length).TrimStart('/');
         }
 
         protected void Application_Start() {
