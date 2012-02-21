24be765bc101e4a2447cc7dd24cfe1378f4b53c9
Nathanael Jones <nathanael.jones@gmail.com>
2012-02-21 12:07:54 -0500

    First drafe of MongoReader
    Former-commit-id: 5cac4a901769746b95b06bebc05dcf99e829c0ad

---
+++ Plugins/MongoReader/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using ImageResizer.Util;
+
+// So the ImageResizer knows which bundle this assembly belongs to
+[assembly: Bundle("R3Bundle3")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImageResizer.Plugins.MongoReader")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0ef75b2d-36b2-42af-a313-3232869ea33f")]
+
