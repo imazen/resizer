1794802d14ff325af1abee9958fd7c134f77e566
Nathanael Jones <nathanael.jones@gmail.com>
2012-02-29 18:35:32 -0500

    Core: Implemented EventCountingDictionary, started work on SourceMemCache
    Former-commit-id: 9b27fed43e94c4ad694f3f673ce09c49859c83d5

---
+++ Core/Plugins/IVirtualFileCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Hosting;
+using System.Collections.Specialized;
+
+namespace ImageResizer.Plugins {
+    /// <summary>
+    /// Implement this if you are caching files provided by a virtual image provider (For example, remote or s3-hosted images).
+    /// </summary>
+    public interface IVirtualFileCache {
+
+        IVirtualFile GetFileIfCached(string virtualPath, NameValueCollection queryString, IVirtualFile original);
+    }
+}
