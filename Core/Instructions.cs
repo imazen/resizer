53070601eb096c3e93739744f8dd1a0afab8724f
Nathanael Jones <nathanael.jones@gmail.com>
2012-03-13 13:24:33 -0400

    Core: Added draft ImageInfoAPI plugin and stub Instructions class.

---
+++ Core/Instructions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Specialized;
+using ImageResizer.Util;
+
+namespace ImageResizer {
+    /// <summary>
+    /// A name/value collection of image processsing instructions. Replaces ResizeSettings.
+    /// </summary>
+    public class Instructions: NameValueCollection {
+        /// <summary>
+        /// Creates an empty settings collection. 
+        /// </summary>
+        public Instructions() : base() { }
+        /// <summary>
+        /// Copies the specified collection into a new Instructions instance.
+        /// </summary>
+        /// <param name="col"></param>
+        public Instructions(NameValueCollection col) : base(col) { }
+        /// <summary>
+        /// Parses the specified querystring into name/value pairs. leading ? not required.
+        /// </summary>
+        /// <param name="queryString"></param>
+        public Instructions(string queryString) : base(PathUtils.ParseQueryStringFriendlyAllowSemicolons(queryString)) { }
+
+
+
+
+        ///Width
+        ///Height
+        ///Mode
+        ///JpegQuality
+        ///AutoRotate
+        ///SourceRotate
+        ///SourceFlip
+        ///FinalRotate
+        ///FinalFlip
+        ///CropRectangle
+        ///CropXUnits
+        ///CropYUnits
+        ///Format
+        ///Zoom
+        ///BackgroundColor
+        ///Frame
+        ///Page
+        ///FallbackUrl 
+        ///
+    }
+}
