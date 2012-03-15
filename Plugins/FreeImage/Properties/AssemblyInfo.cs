879503fdae6295ad52f2b5fd1ae0389f5ca94392
Nathanael Jones <nathanael.jones@gmail.com>
2012-03-14 20:24:35 -0400

    FreeImage: Updated to support native dependency mgr.
    Former-commit-id: f4ed62c8ebe707729e2ddb4c7e4913631e02c2c7

--- Plugins/FreeImage/Properties/AssemblyInfo.cs
+++ Plugins/FreeImage/Properties/AssemblyInfo.cs
@@ -6,6 +6,9 @@ using ImageResizer.Util;
 // So the ImageResizer knows which bundle this assembly belongs to
 [assembly: Bundle("R3Bundle2")]
 
+//Inform NativeDependencyManager where to find the download manifest
+[assembly: NativeDependencies("Native.xml")]
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
