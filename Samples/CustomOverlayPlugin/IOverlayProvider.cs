a02b8624152663f90609466840bae98f08d6d72a
Nathanael Jones <nathanael.jones@gmail.com>
2012-03-15 21:38:48 -0400

    CustomOverlay: first draft

---
+++ Samples/CustomOverlayPlugin/IOverlayProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Specialized;
+
+namespace ImageResizer.Plugins.CustomOverlay {
+    public interface IOverlayProvider {
+        /// <summary>
+        /// Provides an Overlay object for the specified request parameters
+        /// </summary>
+        /// <param name="virtualPath"></param>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        Overlay GetOverlayInfo(string virtualPath, NameValueCollection query);
+    }
+}
---
+++ Samples/CustomOverlayPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImageResizer.Plugins.CustomOverlay")]
+[assembly: AssemblyDescription("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45c7740f-e710-40f2-804d-b42e234c9b64")]
