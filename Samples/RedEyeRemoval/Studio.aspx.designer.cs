926933c185349e348319cefa09dc98df4fb12600
Nathanael Jones <nathanael.jones@gmail.com>
2012-04-02 14:03:13 -0400

    RedEyeSample: Added image picker to studio

--- Samples/RedEyeRemoval/Studio.aspx.designer.cs
+++ Samples/RedEyeRemoval/Studio.aspx.designer.cs
@@ -22,12 +22,12 @@ namespace ComplexWebApplication {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// caption control.
+        /// images control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputText caption;
+        protected global::System.Web.UI.WebControls.Literal images;
     }
 }
