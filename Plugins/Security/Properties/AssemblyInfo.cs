e33527320a28a646ec2acca0b476f4cf9ff4b2e8
Nathanael Jones <nathanael.jones@gmail.com>
2012-04-02 19:56:06 -0400

    Removed Caption and FriendlyUrls plugins. Renamed RequestLimiting to Security.

--- Plugins/Security/Properties/AssemblyInfo.cs
+++ Plugins/Security/Properties/AssemblyInfo.cs
@@ -1,11 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImageResizer.Plugins.Caption")]
+[assembly: AssemblyTitle("ImageResizer.Plugins.RequestLimiting")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("92cfd693-cb9a-48fb-9f92-8902dc1c39bc")]
+[assembly: Guid("d4fe7bad-2ffa-42b0-a18d-4360cdf66f03")]
