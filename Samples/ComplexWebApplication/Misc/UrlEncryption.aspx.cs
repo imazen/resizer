71448627adea632689ebb81420e7248e548c0a6a
Nathanael Jones <nathanael.jones@gmail.com>
2012-04-04 22:48:46 -0400

    Encryption plugin: Completed first draft and added example
    Former-commit-id: a05ac1104420bfe3f4340bead121748b5495b732

---
+++ Samples/ComplexWebApplication/Misc/UrlEncryption.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ImageResizer.Plugins.Encrypted;
+
+namespace ComplexWebApplication.Misc {
+    public partial class UrlEncryption : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            img.ImageUrl = EncryptedPlugin.First.EncryptPathAndQuery("~/fountain-small.jpg?width=100");
+        }
+    }
+}
