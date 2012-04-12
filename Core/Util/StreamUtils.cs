cf9cc25cb76e669125631792cbe1beff3ae5724f
Nathanael Jones <nathanael.jones@gmail.com>
2012-04-12 08:11:01 -0400

    StreamUtils: Replaced CopyTo overloads with type detection, added optional parameters entireStream and chunkSize.

--- Core/Util/StreamUtils.cs
+++ Core/Util/StreamUtils.cs
@@ -7,9 +7,9 @@ using System.Diagnostics;
 namespace ImageResizer.Util {
     public class StreamUtils {
 
-        public static MemoryStream CopyStream(Stream s) {
-            MemoryStream ms = new MemoryStream(s.CanSeek ? ((int)s.Length + 8) : 4096);
-            CopyTo(s, ms);
+        public static MemoryStream CopyStream(Stream s, bool entireStream = false, int chunkSize = 0x4000) {
+            MemoryStream ms = new MemoryStream(s.CanSeek ? ((int)s.Length + 8 - (entireStream ? 0 : (int)s.Position)) : chunkSize);
+            CopyTo(s, ms, entireStream, chunkSize);
             ms.Position = 0;
             return ms;
         }
@@ -20,10 +20,31 @@ namespace ImageResizer.Util {
         /// </summary>
         /// <param name="src"></param>
         /// <param name="dest"></param>
+        public static void CopyTo(Stream src, Stream dest, bool entireStream = false, int chunkSize = 0x4000) {
+            if (src is MemoryStream && src.CanSeek) {
+                try {
+                    int pos = entireStream ? 0 : (int)src.Position;
+                    dest.Write(((MemoryStream)src).GetBuffer(), pos, (int)(src.Length - pos));
+                    return;
+                }catch(UnauthorizedAccessException) //If we can't slice it, then we read it like a normal stream
+                {}
+            }
+            if (dest is MemoryStream && src.CanSeek) {
+                try {
+                    int srcPos = entireStream ? 0 : (int)src.Position;
+                    int pos = (int)dest.Position;
+                    int length = (int)(src.Length - srcPos) + pos;
+                    dest.SetLength(length);
 
-
-        public static void CopyTo(Stream src, Stream dest) {
-            int size = (src.CanSeek) ? Math.Min((int)(src.Length - src.Position), 0x2000) : 0x2000;
+                    var data = ((MemoryStream)dest).GetBuffer();
+                    while (pos < length) {
+                        pos += src.Read(data, pos, length - pos);
+                    }
+                    return;
+                }catch(UnauthorizedAccessException) //If we can't write directly, fall back
+                {}
+            }
+            int size = (src.CanSeek) ? Math.Min((int)(src.Length - (entireStream ? 0 : (int)src.Position)), 0x2000) : 0x2000;
             byte[] buffer = new byte[size];
             int n;
             do {
@@ -32,44 +53,31 @@ namespace ImageResizer.Util {
             } while (n != 0);
         }
 
-        public static void CopyTo(MemoryStream src, Stream dest) {
-            dest.Write(src.GetBuffer(), (int)src.Position, (int)(src.Length - src.Position));
-        }
-
-        public static void CopyTo(Stream src, MemoryStream dest) {
-            if (src.CanSeek) {
-                int pos = (int)dest.Position;
-                int length = (int)(src.Length - src.Position) + pos;
-                dest.SetLength(length);
-
-                while (pos < length)
-                    pos += src.Read(dest.GetBuffer(), pos, length - pos);
-            } else
-                CopyTo(src,(Stream)dest);
-        }
         /// <summary>
         /// Copies the remaining portion of the specified stream to a byte array of exact size.
         /// </summary>
         /// <param name="src"></param>
         /// <returns></returns>
-        public static byte[] CopyToBytes(Stream src) {
+        public static byte[] CopyToBytes(Stream src, bool entireStream = false, int chunkSize = 0x4000) {
             byte[] bytes;
             if (src is MemoryStream) {
                 //Slice from 
                 MemoryStream ms = src as MemoryStream;
                 try{
                     byte[] buffer = ms.GetBuffer();
-                    long count =src.Length - src.Position;
-                    bytes = new byte[src.Length - src.Position];
-                    Array.Copy(buffer, src.Position, bytes, 0, count);
+                    long pos = entireStream ? 0 : src.Position;
+                    long count =src.Length - pos;
+                    bytes = new byte[count];
+                    Array.Copy(buffer, pos, bytes, 0, count);
                     return bytes;
                 }catch(UnauthorizedAccessException) //If we can't slice it, then we read it like a normal stream
                 {}
             }
             
             if (src.CanSeek) {
+                long pos = entireStream ? 0 : src.Position;
                 // Read the source file into a byte array.
-                int numBytesToRead = (int)(src.Length - src.Position);
+                int numBytesToRead = (int)(src.Length - pos);
                 bytes = new byte[numBytesToRead];
                 int numBytesRead = 0;
                 while (numBytesToRead > 0) {
@@ -88,9 +96,9 @@ namespace ImageResizer.Util {
             } else {
                 //No seeking, so we have to buffer to an intermediate memory stream
                 var ms = new MemoryStream();
-                CopyTo(src,(Stream) ms);
+                CopyTo(src, ms,  entireStream,  chunkSize);
                 ms.Seek(0, SeekOrigin.Begin);
-                return CopyToBytes(ms);
+                return CopyToBytes(ms,  entireStream,  chunkSize);
             }
         }
     }
