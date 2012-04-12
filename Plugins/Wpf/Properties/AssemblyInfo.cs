9299e5aa136b8e3cb042fcfb8b6110c3f5b50339
Nathanael Jones <nathanael.jones@gmail.com>
2012-04-12 08:17:54 -0400

    Wpf: Fixed assemblyinfo and core reference
    Former-commit-id: 7b6b82949d642d8999d68172e1ecfef703e098c2

---
+++ Plugins/Wpf/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImageResizer.Plugins.Wpf")]
+[assembly: AssemblyDescription("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("28f5a8b3-65bc-45fb-8d08-9ef0c010fd4d")]
