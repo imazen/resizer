4a7a06120d0f13e5149e101db556281208cefe53
Nathanael Jones <nathanael.jones@gmail.com>
2012-04-12 08:39:34 -0400

    StreamUtils: Moved stream code to ExtensionMethods.StreamExtensions, fixed broken chunckSize and entireStream support.
    Former-commit-id: 0007da4791921a75bd676ea92794100bff88e8e5

--- Core/ExtensionMethods/Stream.cs
+++ Core/ExtensionMethods/Stream.cs
@@ -3,34 +3,178 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using ImageResizer.Util;
+using System.Diagnostics;
 
 namespace ImageResizer.ExtensionMethods {
+    /// <summary>
+    /// Provides extension methods for copying streams
+    /// </summary>
     public static class StreamExtensions {
 
+        /// <summary>
+        /// Copies the remaining data in the current stream to a new MemoryStream instance.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
         public static MemoryStream CopyToMemoryStream(this Stream s) {
-            return StreamUtils.CopyStream(s, false);
+            return CopyToMemoryStream(s, false);
         }
-
+        /// <summary>
+        /// Copies the current stream into a new MemoryStream instance.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="entireStream">True to copy entire stream if seeakable, false to only copy remaining data</param>
+        /// <returns></returns>
         public static MemoryStream CopyToMemoryStream(this Stream s, bool entireStream) {
-            return StreamUtils.CopyStream(s, false);
+            return CopyToMemoryStream(s, entireStream,0x4000);
         }
 
+        /// <summary>
+        /// Copies the current stream into a new MemoryStream instance.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="entireStream">True to copy entire stream if seeakable, false to only copy remaining data</param>
+        /// <param name="chunkSize">The buffer size to use (in bytes) if a buffer is required. Default: 4KiB</param>
+        /// <returns></returns>
+        public static MemoryStream CopyToMemoryStream(this Stream s, bool entireStream, int chunkSize) {
+            MemoryStream ms = new MemoryStream(s.CanSeek ? ((int)s.Length + 8 - (entireStream ? 0 : (int)s.Position)) : chunkSize);
+            CopyToStream(s, ms, entireStream, chunkSize);
+            ms.Position = 0;
+            return ms;
+        }
+
+        /// <summary>
+        /// Copies the remaining data in the current stream to a byte[] array of exact size.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
         public static byte[] CopyToBytes(this Stream s) {
-            return StreamUtils.CopyToBytes(s, false);
+            return CopyToBytes(s, false);
         }
-
+        /// <summary>
+        /// Copies the current stream into a byte[] array of exact size.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="entireStream">True to copy entire stream if seeakable, false to only copy remaining data</param>
+        /// <returns></returns>
         public static byte[] CopyToBytes(this Stream s, bool entireStream) {
-            return StreamUtils.CopyToBytes(s, entireStream);
+            return CopyToBytes(s, entireStream, 0x4000);
         }
-
+        /// <summary>
+        /// Copies the remaining data from the this stream into the given stream.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="other">The stream to write to</param>
         public static void CopyToStream(this Stream s, Stream other) {
-            StreamUtils.CopyTo(s, other, false);
+            CopyToStream(s, other, false);
         }
-
+        /// <summary>
+        /// Copies this stream into the given stream
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="other">The stream to write to</param>
+        /// <param name="entireStream">True to copy entire stream if seeakable, false to only copy remaining data</param>
         public static void CopyToStream(this Stream s, Stream other, bool entireStream) {
-            StreamUtils.CopyTo(s, other, entireStream);
+            CopyToStream(s, other, entireStream, 0x4000);
         }
 
 
+
+
+
+        /// <summary>
+        /// Copies this stream into the given stream
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="dest">The stream to write to</param>
+        /// <param name="entireStream">True to copy entire stream if seeakable, false to only copy remaining data</param>
+        /// <param name="chunkSize">True to copy entire stream if seeakable, false to only copy remaining data</param>
+        public static void CopyToStream(this Stream src, Stream dest, bool entireStream, int chunkSize) {
+            if (entireStream && src.CanSeek) src.Seek(0, SeekOrigin.Begin);
+
+            if (src is MemoryStream && src.CanSeek) {
+                try {
+                    int pos = (int)src.Position;
+                    dest.Write(((MemoryStream)src).GetBuffer(), pos, (int)(src.Length - pos));
+                    return;
+                } catch (UnauthorizedAccessException) //If we can't slice it, then we read it like a normal stream
+                { }
+            }
+            if (dest is MemoryStream && src.CanSeek) {
+                try {
+                    int srcPos = (int)src.Position;
+                    int pos = (int)dest.Position;
+                    int length = (int)(src.Length - srcPos) + pos;
+                    dest.SetLength(length);
+
+                    var data = ((MemoryStream)dest).GetBuffer();
+                    while (pos < length) {
+                        pos += src.Read(data, pos, length - pos);
+                    }
+                    return;
+                } catch (UnauthorizedAccessException) //If we can't write directly, fall back
+                { }
+            }
+            int size = (src.CanSeek) ? Math.Min((int)(src.Length - src.Position), chunkSize) : chunkSize;
+            byte[] buffer = new byte[size];
+            int n;
+            do {
+                n = src.Read(buffer, 0, buffer.Length);
+                dest.Write(buffer, 0, n);
+            } while (n != 0);
+        }
+
+        /// <summary>
+        /// Copies the current stream into a byte[] array of exact size
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="entireStream">True to copy entire stream if seeakable, false to only copy remaining data.</param>
+        /// <param name="chunkSize">The buffer size to use (in bytes) if a buffer is required. Default: 4KiB</param>
+        /// <returns></returns>
+        public static byte[] CopyToBytes(this Stream src, bool entireStream, int chunkSize) {
+            byte[] bytes;
+            if (src is MemoryStream) {
+                //Slice from 
+                MemoryStream ms = src as MemoryStream;
+                try {
+                    byte[] buffer = ms.GetBuffer();
+                    long pos = entireStream ? 0 : src.Position;
+                    long count = src.Length - pos;
+                    bytes = new byte[count];
+                    Array.Copy(buffer, pos, bytes, 0, count);
+                    return bytes;
+                } catch (UnauthorizedAccessException) //If we can't slice it, then we read it like a normal stream
+                { }
+            }
+
+            if (src.CanSeek) {
+                long pos = entireStream ? 0 : src.Position;
+                if (entireStream) src.Seek(0, SeekOrigin.Begin);
+
+                // Read the source file into a byte array.
+                int numBytesToRead = (int)(src.Length - pos);
+                bytes = new byte[numBytesToRead];
+                int numBytesRead = 0;
+                while (numBytesToRead > 0) {
+                    // Read may return anything from 0 to numBytesToRead.
+                    int n = src.Read(bytes, numBytesRead, numBytesToRead);
+
+                    // Break when the end of the file is reached.
+                    if (n == 0)
+                        break;
+
+                    numBytesRead += n;
+                    numBytesToRead -= n;
+                }
+                Debug.Assert(numBytesRead == bytes.Length);
+                return bytes;
+            } else {
+                //No seeking, so we have to buffer to an intermediate memory stream
+                var ms = new MemoryStream();
+                CopyToStream(src, ms, entireStream, chunkSize);
+                ms.Seek(0, SeekOrigin.Begin);
+                return CopyToBytes(ms, entireStream, chunkSize);
+            }
+        }
     }
 }
--- Core/Util/StreamUtils.cs
+++ Core/Util/StreamUtils.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Diagnostics;
+using ImageResizer.ExtensionMethods;
 
 namespace ImageResizer.Util {
     public class StreamUtils {
 
-        public static MemoryStream CopyStream(Stream s, bool entireStream = false, int chunkSize = 0x4000) {
-            MemoryStream ms = new MemoryStream(s.CanSeek ? ((int)s.Length + 8 - (entireStream ? 0 : (int)s.Position)) : chunkSize);
-            CopyTo(s, ms, entireStream, chunkSize);
-            ms.Position = 0;
-            return ms;
+        public static MemoryStream CopyStream(Stream s) {
+            return s.CopyToMemoryStream();
         }
 
 
@@ -20,37 +18,8 @@ namespace ImageResizer.Util {
         /// </summary>
         /// <param name="src"></param>
         /// <param name="dest"></param>
-        public static void CopyTo(Stream src, Stream dest, bool entireStream = false, int chunkSize = 0x4000) {
-            if (src is MemoryStream && src.CanSeek) {
-                try {
-                    int pos = entireStream ? 0 : (int)src.Position;
-                    dest.Write(((MemoryStream)src).GetBuffer(), pos, (int)(src.Length - pos));
-                    return;
-                }catch(UnauthorizedAccessException) //If we can't slice it, then we read it like a normal stream
-                {}
-            }
-            if (dest is MemoryStream && src.CanSeek) {
-                try {
-                    int srcPos = entireStream ? 0 : (int)src.Position;
-                    int pos = (int)dest.Position;
-                    int length = (int)(src.Length - srcPos) + pos;
-                    dest.SetLength(length);
-
-                    var data = ((MemoryStream)dest).GetBuffer();
-                    while (pos < length) {
-                        pos += src.Read(data, pos, length - pos);
-                    }
-                    return;
-                }catch(UnauthorizedAccessException) //If we can't write directly, fall back
-                {}
-            }
-            int size = (src.CanSeek) ? Math.Min((int)(src.Length - (entireStream ? 0 : (int)src.Position)), 0x2000) : 0x2000;
-            byte[] buffer = new byte[size];
-            int n;
-            do {
-                n = src.Read(buffer, 0, buffer.Length);
-                dest.Write(buffer, 0, n);
-            } while (n != 0);
+        public static void CopyTo(Stream src, Stream dest) {
+            src.CopyToStream(dest);
         }
 
         /// <summary>
@@ -58,48 +27,8 @@ namespace ImageResizer.Util {
         /// </summary>
         /// <param name="src"></param>
         /// <returns></returns>
-        public static byte[] CopyToBytes(Stream src, bool entireStream = false, int chunkSize = 0x4000) {
-            byte[] bytes;
-            if (src is MemoryStream) {
-                //Slice from 
-                MemoryStream ms = src as MemoryStream;
-                try{
-                    byte[] buffer = ms.GetBuffer();
-                    long pos = entireStream ? 0 : src.Position;
-                    long count =src.Length - pos;
-                    bytes = new byte[count];
-                    Array.Copy(buffer, pos, bytes, 0, count);
-                    return bytes;
-                }catch(UnauthorizedAccessException) //If we can't slice it, then we read it like a normal stream
-                {}
-            }
-            
-            if (src.CanSeek) {
-                long pos = entireStream ? 0 : src.Position;
-                // Read the source file into a byte array.
-                int numBytesToRead = (int)(src.Length - pos);
-                bytes = new byte[numBytesToRead];
-                int numBytesRead = 0;
-                while (numBytesToRead > 0) {
-                    // Read may return anything from 0 to numBytesToRead.
-                    int n = src.Read(bytes, numBytesRead, numBytesToRead);
-
-                    // Break when the end of the file is reached.
-                    if (n == 0)
-                        break;
-
-                    numBytesRead += n;
-                    numBytesToRead -= n;
-                }
-                Debug.Assert(numBytesRead == bytes.Length);
-                return bytes;
-            } else {
-                //No seeking, so we have to buffer to an intermediate memory stream
-                var ms = new MemoryStream();
-                CopyTo(src, ms,  entireStream,  chunkSize);
-                ms.Seek(0, SeekOrigin.Begin);
-                return CopyToBytes(ms,  entireStream,  chunkSize);
-            }
+        public static byte[] CopyToBytes(Stream src) {
+            return src.CopyToBytes();
         }
     }
 }
