8301b607b0d369011317a99dd9e1499ee864f1e8
Nathanael Jones <nathanael.jones@gmail.com>
2012-04-12 08:19:58 -0400

    Core: Added stub UrlOptions class. Added extension methods for customized Enum parsing/serialization, NameValueCollection value parsing/serialization, and Stream copying.

---
+++ Core/UrlOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Specialized;
+
+namespace ImageResizer {
+    public class UrlOptions : NameValueCollection {
+
+        //Host - cloud front or image server
+        //Semicolons 
+        //PathKind - RemoteImage, WebsiteScreenshot, Gradient,
+
+
+
+    }
+}
