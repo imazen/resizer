5bcacc6ff3fa94a141ed830484926a6b45d37ee3
Nathanael Jones <nathanael.jones@gmail.com>
2012-04-12 08:19:58 -0400

    Core: Added stub UrlOptions class. Added extension methods for customized Enum parsing/serialization, NameValueCollection value parsing/serialization, and Stream copying.
    Former-commit-id: c26e3cd919d7a7efcd48602a68e5ce61af423efc

---
+++ Core/ExtensionMethods/ExtensionAttribute.cs
@@ -0,0 +1,7 @@
+﻿
+
+namespace System.Runtime.CompilerServices {
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class
+         | AttributeTargets.Method)]
+    public sealed class ExtensionAttribute : Attribute { }
+}
---
+++ Core/ExtensionMethods/Stream.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ImageResizer.Util;
+
+namespace ImageResizer.ExtensionMethods {
+    public static class StreamExtensions {
+
+        public static MemoryStream CopyToMemoryStream(this Stream s) {
+            return StreamUtils.CopyStream(s, false);
+        }
+
+        public static MemoryStream CopyToMemoryStream(this Stream s, bool entireStream) {
+            return StreamUtils.CopyStream(s, false);
+        }
+
+        public static byte[] CopyToBytes(this Stream s) {
+            return StreamUtils.CopyToBytes(s, false);
+        }
+
+        public static byte[] CopyToBytes(this Stream s, bool entireStream) {
+            return StreamUtils.CopyToBytes(s, entireStream);
+        }
+
+        public static void CopyToStream(this Stream s, Stream other) {
+            StreamUtils.CopyTo(s, other, false);
+        }
+
+        public static void CopyToStream(this Stream s, Stream other, bool entireStream) {
+            StreamUtils.CopyTo(s, other, entireStream);
+        }
+
+
+    }
+}
