784c0f1529357319e9db765018df51456dabb9e5
Nathanael Jones <nathanael.jones@gmail.com>
2012-04-16 10:14:48 -0400

    Bumped version to 3.2.0
    Former-commit-id: 395078ee7b7d68728cb2df8c0b4a8d86af0ce265

--- Core/SharedAssemblyInfo.cs
+++ Core/SharedAssemblyInfo.cs
@@ -28,9 +28,9 @@ using ImageResizer.Util;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:/*
-[assembly: AssemblyVersion("3.1.6.*")]
-[assembly: AssemblyFileVersion("3.1.6.*")]
-[assembly: AssemblyInformationalVersion("3-1-6")]
+[assembly: AssemblyVersion("3.2.0.*")]
+[assembly: AssemblyFileVersion("3.2.0.*")]
+[assembly: AssemblyInformationalVersion("3-2-0")]
 
 [assembly: Commit("git-commit-guid-here")]
 
@@ -38,5 +38,5 @@ using ImageResizer.Util;
 
 // These commented out settings are for the build script to access
 // [assembly: PackageName("Resizer")]
-// [assembly: NugetVersion("3.1.6")]
+// [assembly: NugetVersion("3.2.0")]
 // [assembly: DownloadServer("http://downloads.imageresizing.net/")]
