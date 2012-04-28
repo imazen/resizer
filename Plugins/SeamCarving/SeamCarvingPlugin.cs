2a53e321ae3df5f40f1a8fb6e5aa7041c5c41a51
Nathanael Jones <nathanael.jones@gmail.com>
2012-04-28 14:15:10 -0400

    SeamCarving: Added alpha-level carve.data support

--- Plugins/SeamCarving/SeamCarvingPlugin.cs
+++ Plugins/SeamCarving/SeamCarvingPlugin.cs
@@ -39,15 +39,72 @@ namespace ImageResizer.Plugins.SeamCarving {
             Backward = 0,
             Forward = 1
         }
-              
+
+        private const string CarveData = "CarveData";
+
+        protected override RequestedAction LayoutImage(ImageState s) {
+            if (s.sourceBitmap == null) return RequestedAction.None;
+
+
+            //Parse carve data bitmap
+            if (!string.IsNullOrEmpty(s.settings["carve.data"])) {
+                string[] parts = s.settings["carve.data"].Split('|');
+                //Parse block count and string
+                var block_count = int.Parse(parts[0]);
+                var carveString = new LzwDecoder("012").Decode(PathUtils.FromBase64UToBytes(parts[1]));
+
+                float block_size = (int)Math.Floor(Math.Sqrt(s.originalSize.Width * s.originalSize.Height / (double)block_count));
+
+                var carveData = new CarveDataPlotter() {
+                    BlockCount=block_count,
+                    Stride = (int)Math.Ceiling((float)s.originalSize.Width / block_size),
+                    Rows = (int)Math.Ceiling((float)s.originalSize.Height / block_size)
+                };
+
+                carveData.Init(carveString);
+
+                Size remove = carveData.GetRemovalSpace(s.originalSize.Width,s.originalSize.Height,(int)block_size);
+
+                if (remove.Width / s.originalSize.Width > remove.Height / s.originalSize.Height) {
+                    s.originalSize = new Size(s.originalSize.Width - remove.Width, s.originalSize.Height);
+                } else {
+                    s.originalSize = new Size(s.originalSize.Width, s.originalSize.Height - remove.Height);
+                }
+                
+                //Save later
+                s.Data[CarveData] = carveData;
+            }
+
+
+
+            return RequestedAction.None;
+
+        }
+
+
+        protected override RequestedAction PostLayoutImage(ImageState s) {
+            
+            return RequestedAction.None;
+        }
+
+
 
         protected override RequestedAction PreRenderImage(ImageState s) {
             //Skip this when we are doing simulations
             if (s.destGraphics == null) return RequestedAction.None;
 
+            //Parse carve algorithm kind
             FilterType ftype = s.settings.Get<FilterType>("carve", FilterType.None);
             if ("true".Equals(s.settings["carve"], StringComparison.OrdinalIgnoreCase)) ftype = FilterType.Prewitt;
             if (string.IsNullOrEmpty(s.settings["carve"]) && s.settings.Mode == FitMode.Carve) ftype = FilterType.Prewitt;
+
+            //If we have carve data
+            CarveDataPlotter carveData = s.Data[CarveData] as CarveDataPlotter;
+            if (carveData != null && ftype == FilterType.None) ftype = FilterType.Prewitt;
+
+            RectangleF copyRect = s.copyRect;
+
+            if (carveData != null) copyRect = new RectangleF(new PointF(0, 0), s.sourceBitmap.Size);
 
             if (ftype == FilterType.None) return RequestedAction.None; //Only override rendering when carving is requested.
 
@@ -59,7 +116,7 @@ namespace ImageResizer.Plugins.SeamCarving {
             //The size of the temporary bitmap. 
             //We want it larger than the size we'll use on the final copy, so we never upscale it
             //- but we also want it as small as possible so processing is fast.
-            SizeF tempSize = PolygonMath.ScaleOutside(targetSize, s.copyRect.Size);
+            SizeF tempSize = PolygonMath.ScaleOutside(targetSize, copyRect.Size);
             int tempWidth = (int)Math.Ceiling(tempSize.Width);
             int tempHeight = (int)Math.Ceiling(tempSize.Height);
 
@@ -75,6 +132,8 @@ namespace ImageResizer.Plugins.SeamCarving {
                     //Unless it's already been done for us by FreeImageResize or something
                     if (s.preRenderBitmap != null && (tempWidth - s.preRenderBitmap.Width < 50 && tempHeight - s.preRenderBitmap.Height < 50)) {
                         s.preRenderBitmap.Save(tempFile, ImageFormat.Bmp);
+                        tempWidth = s.preRenderBitmap.Width;
+                        tempHeight = s.preRenderBitmap.Height;
                     } else {
                         //Create the temporary bitmap and graphics.
                         using (Bitmap temp = new Bitmap(tempWidth, tempHeight, PixelFormat.Format32bppArgb))
@@ -90,7 +149,7 @@ namespace ImageResizer.Plugins.SeamCarving {
                             if (s.preRenderBitmap != null) {
                                 g.DrawImage(s.preRenderBitmap, new Rectangle(0, 0, tempWidth, tempHeight), 0, 0, s.preRenderBitmap.Width, s.preRenderBitmap.Height, GraphicsUnit.Pixel, ia);
                             } else {
-                                g.DrawImage(s.sourceBitmap, new Rectangle(0, 0, tempWidth, tempHeight), s.copyRect.X, s.copyRect.Y, s.copyRect.Width, s.copyRect.Height, GraphicsUnit.Pixel, ia);
+                                g.DrawImage(s.sourceBitmap, new Rectangle(0, 0, tempWidth, tempHeight), copyRect.X, copyRect.Y, copyRect.Width, copyRect.Height, GraphicsUnit.Pixel, ia);
                             }
                             g.Flush(FlushIntention.Flush);
                             //Save
@@ -98,14 +157,23 @@ namespace ImageResizer.Plugins.SeamCarving {
                         }
                     }
 
-                    Size intTargetSize = new Size((int)targetSize.Width, (int)targetSize.Height);
-                    CairJob job = new CairJob();
-                    job.SourcePath = tempFile;
-                    job.DestPath = outputTempFile;
-                    job.Size = intTargetSize;
-                    job.Filter = ftype;
-                    job.Timeout = 5000;
-                    cair.CairyIt(job);
+                    string maskFile = carveData != null ? Path.GetTempFileName() : null;
+                    try {
+                        if (carveData != null)
+                            carveData.SaveBitmapAs(maskFile, tempWidth, tempHeight);
+                        
+                        Size intTargetSize = new Size((int)targetSize.Width, (int)targetSize.Height);
+                        CairJob job = new CairJob();
+                        if (maskFile != null) job.WeightPath = maskFile;
+                        job.SourcePath = tempFile;
+                        job.DestPath = outputTempFile;
+                        job.Size = intTargetSize;
+                        job.Filter = ftype;
+                        job.Timeout = 5000;
+                        cair.CairyIt(job);
+                    } finally {
+                        if (maskFile != null) File.Delete(maskFile);
+                    }
 
                 } finally {
                     File.Delete(tempFile);
