e0c57a07b71165621322ef465c4b865c80a51d3b
Nathanael Jones <nathanael.jones@gmail.com>
2012-05-28 14:30:11 -0400

    Re-namespaced ResizingCanceledException

--- Plugins/RedEye/ResizingCanceledException.cs
+++ Plugins/RedEye/ResizingCanceledException.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ImageResizer.Resizing {
+namespace ImageResizer.Plugins.RedEye {
     public class ResizingCanceledException:Exception {
         public byte[] ResponseData { get; set; }
         public string ContentType { get; set; }
