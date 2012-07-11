4801f34a5dde8a40bdef0040e6dda7978bdc5dc2
Nathanael Jones <nathanael.jones@gmail.com>
2012-07-10 21:33:32 -0400

    Faces: First working draft

---
+++ Plugins/ImageResizer.Plugins.Faces/FacesPlugin.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ImageResizer.Resizing;
+using System.Globalization;
+using ImageResizer.Util;
+using System.Drawing.Imaging;
+using System.Drawing;
+using ImageResizer.Configuration;
+using ImageResizer.ExtensionMethods;
+using ImageResizer.Caching;
+using System.IO;
+using System.Collections.Specialized;
+
+namespace ImageResizer.Plugins.Faces {
+    public class FacesPlugin:BuilderExtension,IPlugin,IQuerystringPlugin {
+        public FacesPlugin() {
+        }
+
+
+        protected Config c;
+        public IPlugin Install(Configuration.Config c) {
+            c.Plugins.add_plugin(this);
+            this.c = c;
+            c.Pipeline.PreHandleImage += Pipeline_PreHandleImage;
+            return this;
+        }
+
+        public bool Uninstall(Configuration.Config c) {
+            c.Plugins.remove_plugin(this);
+            c.Pipeline.PreHandleImage -= Pipeline_PreHandleImage;
+            return true;
+        }
+
+       
+ 
+        protected override RequestedAction PostRenderImage(ImageState s) {
+
+            if (s.destBitmap == null) return RequestedAction.None;
+            string str = null;
+            int i = 0;
+
+             if ("true".Equals(s.settings["f.show"], StringComparison.OrdinalIgnoreCase)) {
+                 List<Face> eyes = new FaceDetection() { MinFaces = 1, MaxFaces = 1 }.DetectFeatures(s.sourceBitmap);
+                 List<PointF> points = new List<PointF>();
+                 foreach(Face r in eyes) { points.Add(new PointF(r.X,r.Y)); points.Add(new PointF(r.X2,r.Y2));}
+                 PointF[] newPoints = c.CurrentImageBuilder.TranslatePoints(points.ToArray(),s.originalSize,new ResizeSettings(s.settings));
+                 using (Graphics g = Graphics.FromImage(s.destBitmap)){
+                     for(i =0; i < newPoints.Length -1; i+=2){
+                         float x1 = newPoints[i].X;
+                         float y1 = newPoints[i].Y;
+                         float x2 = newPoints[i + 1].X;
+                         float y2 = newPoints[i + 1].Y;
+                         float t; 
+                         if (x1 > x2){ t = x2; x2  =x1; x1 = t;}
+                         if (y1 > y2){ t = y1; y1 = y2; y2 = t;} 
+                         
+                         g.DrawRectangle(Pens.Green,new Rectangle((int)x1,(int)y1,(int)(x2-x1),(int)(y2-y1)));
+                     }
+                 }
+             }
+
+            return RequestedAction.None;
+        }
+
+        class RedEyeData {
+            public float dx;
+            public float dy;
+            public float dw;
+            public float dh;
+            public float ow;
+            public float oh;
+            public float cropx;
+            public float cropy;
+            public float cropw;
+            public float croph;
+            public List<Face> features;
+            public string message;
+        }
+
+
+        protected override RequestedAction Render(ImageState s) {
+            bool detect = NameValueCollectionExtensions.Get(e.RewrittenQuerystring, "f.detect", false);
+            bool getlayout = NameValueCollectionExtensions.Get(e.RewrittenQuerystring, "f.getlayout", false);
+            if (!detect && !getlayout) return RequestedAction.None;
+
+
+            var ex = new ResizingCanceledException("Resizing was canceled as JSON data was requested instead");
+
+            RedEyeData d = new RedEyeData();
+            try {
+                //Only detect eyes if it was requested.
+                if (detect) d.features = ConfigureDetection(s.settings).DetectFeatures(s.sourceBitmap);
+            } catch (TypeInitializationException e) {
+                throw e;
+            } catch (Exception e) {
+                d.message = e.Message;
+            }
+            d.ow = s.originalSize.Width;
+            d.oh = s.originalSize.Height;
+            d.cropx = s.copyRect.X;
+            d.cropy = s.copyRect.Y;
+            d.cropw = s.copyRect.Width;
+            d.croph = s.copyRect.Height;
+            RectangleF dest = PolygonMath.GetBoundingBox(s.layout["image"]);
+            d.dx = dest.X;
+            d.dy = dest.Y;
+            d.dw = dest.Width;
+            d.dh = dest.Height;
+            ex.ContentType = "application/json; charset=utf-8";
+            StringWriter sw = new StringWriter();
+            new Newtonsoft.Json.JsonSerializer().Serialize(sw, d);
+            string prefix = string.IsNullOrEmpty(s.settings["callback"]) ? "" : s.settings["callback"] + "(";
+            string suffix = string.IsNullOrEmpty(prefix) ? "" : ");";
+            ex.ResponseData = UTF8Encoding.UTF8.GetBytes(prefix + sw.ToString() + suffix);
+            ex.StatusCode = 200;
+
+            throw ex;
+        }
+
+        /// <summary>
+        /// This is where we hijack the resizing process, interrupt it, and send back the json data we created.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="context"></param>
+        /// <param name="e"></param>
+        void Pipeline_PreHandleImage(System.Web.IHttpModule sender, System.Web.HttpContext context, Caching.IResponseArgs e) {
+            bool detect = NameValueCollectionExtensions.Get(e.RewrittenQuerystring, "f.detect", false);
+            bool getlayout = NameValueCollectionExtensions.Get(e.RewrittenQuerystring, "f.getlayout", false);
+            if (!detect && !getlayout) return;
+
+            ResponseArgs ra = e as ResponseArgs;
+            e.ResponseHeaders.ContentType = "application/json; charset=utf-8";
+
+            var old = ra.ResizeImageToStream;
+            ra.ResizeImageToStream = new ResizeImageDelegate(delegate(Stream s) {
+                try {
+                    old(s);
+                } catch (ResizingCanceledException rce) {
+                    s.Write(rce.ResponseData, 0, rce.ResponseData.Length);
+                }
+            });
+        }
+
+        public FaceDetection ConfigureDetection(NameValueCollection s) {
+            
+            var f = new FaceDetection();
+
+            //Parse min/max faces
+            int[] count = NameValueCollectionExtensions.GetList<int>(s,"f.faces",null,1,2);
+            if (count == null) {
+                f.MinFaces = 1;
+                f.MaxFaces = 1;
+            }else if (count.Length > 0{
+                f.MinFaces = f.MaxFaces = count[0];
+                if (count.Length > 1) f.MaxFaces = count[1];
+            }
+
+            //Parse min/default thresholds
+            int[] threshold = NameValueCollectionExtensions.GetList<int>(s,"f.threshold",null,1,2);
+            if (threshold != null && threshold.Length > 0){
+                f.MinConfidenceLevel = f.ConfidenceLevelThreshold = threshold[0];
+                if (threshold.Length > 1) f.ConfidenceLevelThreshold = threshold[1];
+            }
+
+            //Parse min size percent
+            f.MinSizePercent = NameValueCollectionExtensions.Get<float>(s,"f.minsize",f.MinSizePercent);
+
+            return f;
+        }
+
+        public override IEnumerable<string> GetSupportedQuerystringKeys() {
+            return new string[] { "f.show", "f.detect","f.faces", "f.threshold", "f.minsize"};
+        }
+    }
+}
---
+++ Plugins/ImageResizer.Plugins.Faces/FeatureDetectionBase.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using OpenCvSharp;
+using System.IO;
+
+namespace ImageResizer.Plugins.Faces {
+    public static class OpenCvExtensions {
+        public static RectangleF ToRectangleF(this CvRect rect) {
+            return new RectangleF(rect.X, rect.Y, rect.Width, rect.Height);
+        }
+        public static CvAvgComp[] ToArrayAndDispose(this CvSeq<CvAvgComp> seq) {
+            CvAvgComp[] arr = seq.ToArray();
+            seq.Dispose();
+            return arr;
+        }
+
+        public static CvRect Offset(this CvRect rect, CvPoint offset) {
+            return new CvRect(rect.X + offset.X, rect.Y + offset.Y, rect.Width, rect.Height);
+        }
+    }
+    public interface IFeature {
+        float X { get; set; }
+        float Y { get; set; }
+        float X2 { get; set; }
+        float Y2 { get; set; }
+    }
+
+    /// <summary>
+    /// Not thread safe. 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class FeatureDetectionBase<T> : IDisposable where T : IFeature {
+        public FeatureDetectionBase() {
+            searchFolders.Add(System.IO.Path.GetDirectoryName(new Uri(this.GetType().Assembly.CodeBase).LocalPath));
+            searchFolders.Add(System.IO.Path.GetDirectoryName(this.GetType().Assembly.Location));
+            searchFolders.Add(@"C:\Users\Administrator\Documents\resizer\Plugins\Libs\OpenCV");
+        }
+        public FeatureDetectionBase(string xmlFolder):this() {
+            if (xmlFolder != null) searchFolders.Insert(0,xmlFolder);
+        }
+
+        protected List<string> searchFolders = new List<string>() { };
+
+        protected Dictionary<string, string> fileNames = new Dictionary<string,string>(){ 
+            {"FaceCascade",@"haarcascade_frontalface_default.xml"}, 
+            {"LeftEyeCascade" , @"haarcascade_mcs_lefteye.xml"},
+            {"RightEyeCascade" , @"haarcascade_mcs_righteye.xml"},
+            {"EyePair45" , @"haarcascade_mcs_eyepair_big.xml"},
+            {"EyePair22" , @"haarcascade_mcs_eyepair_small.xml"},
+            {"Eye" , @"haarcascade_eye.xml"},
+        };
+
+        protected Dictionary<string, string> Files = null;
+        protected Dictionary<string, CvHaarClassifierCascade> Cascades = null;
+        private void LoadFiles() {
+            if (Files != null) return;
+
+            var f = new Dictionary<string, string>();
+            Cascades = new Dictionary<string, CvHaarClassifierCascade>();
+            foreach (string key in fileNames.Keys) {
+                string resolvedPath = null;
+                foreach (string basePath in searchFolders) {
+                    string full = basePath.TrimEnd('\\') + '\\' + fileNames[key];
+                    if (File.Exists(full)) {
+                        resolvedPath = Path.GetFullPath(full);
+                        Cascades[key] = Cv.Load<CvHaarClassifierCascade>(resolvedPath);
+                        break;
+                    }
+                }
+                if (resolvedPath == null) throw new FileNotFoundException("Failed to find " + fileNames[key] + " in any of the search directories. Verify the XML files have been copied to the same folder as ImageResizer.Plugins.RedEye.dll.");
+                f[key] = resolvedPath;
+            }
+
+            Files = f;
+        }
+
+        /// <summary>
+        /// Large images will be scaled down to less than scaledBounds X scaledBounds for feature detection.
+        /// Defaults to 1000
+        /// </summary>
+        protected int scaledBounds = 1000;
+
+        public List<T> DetectFeatures(Bitmap b) {
+            LoadFiles();
+            List<T> features;
+            //Type Intializer Exception occurs if you reuse an appdomain. Always restart the server.
+            using (IplImage orig = OpenCvSharp.BitmapConverter.ToIplImage(b))
+            using (IplImage gray = new IplImage(orig.Size, BitDepth.U8, 1)) {
+
+                //Make grayscale version
+                Cv.CvtColor(orig, gray, ColorConversion.BgrToGray);
+
+                int w = orig.Width; int h = orig.Height;
+                double ratio = (double)w / (double)h;
+                double scale = 1;
+                if (ratio > 1) scale = (double)w / (double)scaledBounds;
+                if (ratio <= 1) scale = (double)h / (double)scaledBounds;
+                scale = Math.Min(1, 1 / scale);
+
+
+                using (IplImage small = new IplImage(new CvSize(Cv.Round(w * scale), Cv.Round(h * scale)), BitDepth.U8, 1)) {
+                    //Resize to smaller version
+                    Cv.Resize(gray, small, Interpolation.Area);
+                    //Equalize histogram
+                    Cv.EqualizeHist(gray, gray);
+
+                    using (CvMemStorage storage = new CvMemStorage()) {
+                        storage.Clear();
+                        features = DetectFeatures(small, storage);
+                    }
+                }
+                //Scale all rectangles by factor to restore to original resolution
+                for (int i = 0; i < features.Count; i++) {
+                    IFeature e = features[i];
+                    e.Y = (float)Math.Min(h, e.Y / scale);
+                    e.X = (float)Math.Min(w, e.X / scale);
+                    e.Y2 = (float)Math.Min(h, e.Y2 / scale);
+                    e.X2 = (float)Math.Min(w, e.X2 / scale);
+                    
+                }
+            }
+            return features;
+        }
+
+        protected  abstract List<T> DetectFeatures(IplImage img, CvMemStorage storage);
+
+        protected int CompareByNeighbors(CvAvgComp a, CvAvgComp b) {
+            return b.Neighbors.CompareTo(a.Neighbors);
+        }
+
+        /// <summary>
+        /// Disposes all loaded cascades
+        /// </summary>
+        public void Dispose() {
+            foreach (string s in Cascades.Keys) {
+                if (Cascades[s] != null) {
+                    Cascades[s].Dispose();
+                    Cascades[s] = null;
+                }
+            }
+        }
+    }
+}
