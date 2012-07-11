f1233fae66a03ab9880362d4a428cef32060b2dd
Nathanael Jones <nathanael.jones@gmail.com>
2012-07-10 21:33:32 -0400

    Faces: First working draft
    Former-commit-id: 6365632e4e9a032b11b72be8774b300933bdc7c3

---
+++ Plugins/ImageResizer.Plugins.Faces/FaceDetection.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenCvSharp;
+using System.Drawing;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace ImageResizer.Plugins.Faces {
+
+    public class Face:IFeature {
+        public Face(RectangleF rect, float accuracy) {
+            this.X = rect.X;
+            this.Y = rect.Y;
+            this.X2 = rect.Right;
+            this.Y2 = rect.Bottom;
+            this.Accuracy = accuracy;
+        }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float X2 { get; set; }
+        public float Y2 { get; set; }
+        public float Accuracy { get; set; }
+    }
+
+    public class FaceDetection:FeatureDetectionBase<Face>{
+
+        public FaceDetection():base(){
+            MinFaces = 1;
+            MaxFaces = 10;
+            MinSizePercent = 3;
+            ConfidenceLevelThreshold = 2;
+            MinConfidenceLevel = 1;
+
+            fileNames = new Dictionary<string, string>(){ 
+            {"FaceCascade",@"haarcascade_frontalface_default.xml"} };
+        }
+        /// <summary>
+        /// The minimum number of faces expected
+        /// </summary>
+        public int MinFaces { get; set; }
+        /// <summary>
+        /// The maximum number of faces wanted
+        /// </summary>
+        public int MaxFaces { get; set; }
+
+        /// <summary>
+        /// The smallest face that will be detected, represented in a 1..100 percentage of the (smaller of width and height).
+        /// Defaults to 3 percent (3.0f)
+        /// </summary>
+        public float MinSizePercent { get; set; }
+
+        /// <summary>
+        /// The minimum number of agreeing matches required for a face rectangle to be returned. 
+        /// This rule isn't applied if we don't have [MinFaces] number of faces.
+        /// </summary>
+        public int ConfidenceLevelThreshold { get; set; }
+
+        /// <summary>
+        /// The minimum number of agreeing matches required for a face rectangle to be evaluated
+        /// </summary>
+        public int MinConfidenceLevel { get; set; }
+       
+        /// <summary>
+        /// Detects features on a grayscale image.
+        /// </summary>
+        /// <param name="img"></param>
+        /// <param name="storage"></param>
+        /// <returns></returns>
+        protected override List<Face> DetectFeatures(IplImage img, CvMemStorage storage) {
+            //Determine minimum face size
+            var minSize = (int)Math.Round((double)MinSizePercent / 100.0 * Math.Min(img.Width, img.Height));
+
+            
+            //Detect faces (frontal). TODO: side
+            Stopwatch watch = Stopwatch.StartNew();
+            CvAvgComp[] faces = Cv.HaarDetectObjects(img, Cascades["FaceCascade"], storage, 1.0850, MinConfidenceLevel, 0, new CvSize(minSize, minSize)).ToArrayAndDispose();
+            watch.Stop();
+            Debug.WriteLine("Face detection time = " + watch.ElapsedMilliseconds);
+
+            //Sort by accuracy
+            Array.Sort<CvAvgComp>(faces, CompareByNeighbors);
+
+            //Convert into feature objects list
+            List<Face> features = new List<Face>(faces.Length);
+            foreach (CvAvgComp face in faces) features.Add(new Face(face.Rect.ToRectangleF(), face.Neighbors));
+            
+            //Unless we're below MinFaces, filter out the low confidence matches.
+            while (features.Count > MinFaces && features[features.Count - 1].Accuracy < ConfidenceLevelThreshold) features.RemoveAt(features.Count - 1);
+
+
+            //Never return more than [MaxFaces]
+            return (features.Count > MaxFaces) ? features.GetRange(0, MaxFaces) : features;
+        }
+    }
+}
---
+++ Plugins/ImageResizer.Plugins.Faces/readme.md
@@ -0,0 +1,50 @@
+﻿
+ImageResizer.Plugins.Faces
+
+
+Face Search
+
+
+f.show=true will highlight faces. 
+f.detect=true will return the data in JSON form.
+
+
+Tuning parameters
+
+* max faces desired detected
+* min faces desired detected
+* min face size (percentage)
+* min confidence level
+* default confidence level threshold
+
+URL Configuration for Tuning: 
+
+* f.faces=min,max OR f.faces=target //You must specify how many faces you are wanting to enhance precision. You can specify a range in min,max form, or just specify a certain count, like f.faces=1
+* f.threshold=min,target OR f.threshold=min //You can adjust the confidence threshold from (1) lowest to (200) extremely high. No need to specify 'target' unless you are also specifying a range with 'f.faces'.
+* f.minsize=3.0 //You can require that detected faces be a minimum percentage of the image size. Defaults to 3% (3.0)
+
+Managed: See FaceDetection class
+
+
+
+
+
+API
+
+* Locate faces in image, returning rectangles in both source and final coordinates. (Managed & json API req.d)
+* Create CropUp style cropping that takes a point or rectangle to center auto-cropping around. Allow adjustable zoom
+* Offer integrated API that combines both...
+
+
+
+Orig. Spec:
+
+
+Features:
+1. Offer JSON and Managed API for detecting faces in any given image, and returning 1 or more rectangles in both source image and final image coordinates.
+2. Offer automatic cropping mode that uses a provided 'focus rectangle' to crop to a given square or rectangle size. Cropping directly to a face rect will often chop off foreheads and necks, so it's better to center the frame around the detected head and only shrink to a provided percentage away from the 'focus rectangle'.
+3. Offer automatic cropping mode that integrates #1 and #2 in a single request 
+
+Parameters
+
+1. Optimize based on expected face count to eliminate false positives
