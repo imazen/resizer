19bf024fb84cd924b428f13b2b6aa7e087f7b779
Nathanael Jones <nathanael.jones@gmail.com>
2012-07-12 12:20:42 -0400

    Moved Faces plugin to appropriate directory, added to build.
    Former-commit-id: 178ad6a17072b9b85f2cd638e4a858ea34c001ef

---
+++ Plugins/Faces/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using ImageResizer.Util;
+
+// So the ImageResizer knows which bundle this assembly belongs to
+[assembly: Bundle("R3SupportOnly")]
+
+//Inform NativeDependencyManager where to find the download manifest
+[assembly: NativeDependencies("Native.xml")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImageResizer.Plugins.Faces")]
+
+
+[assembly: ComVisibleAttribute(true)]
