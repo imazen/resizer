0d7e47fca96530d2c4ac8ca42412bc0d4f6eb2a1
Nathanael Jones <nathanael.jones@gmail.com>
2012-08-06 11:06:31 -0400

    Added ICurrentConfigProvider support. Added plugin loading hint for .NET 4+ for plugin names that match the assembly.
    Former-commit-id: fadaa08ffdce219471942b6963e6250f7ed0b07f

---
+++ Core/Plugins/ICurrentConfigProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ImageResizer.Configuration;
+using System.Web;
+
+namespace ImageResizer.Plugins {
+    /// <summary>
+    /// Allows multi-tenancy support. The 'root' config only needs one plugin, which implements this interface.
+    /// </summary>
+    public interface ICurrentConfigProvider {
+        /// <summary>
+        /// Returns a Config instance appropriate for the current request. If null is returned, the default/root instance will be used.
+        /// Implementations MUST return the same instance of Config for two identical requests. Multiple Config instances per tenant/area will cause problems.
+        /// MUST be thread-safe, concurrent calls WILL ocurr, and WILL occur during initial call. 
+        /// </summary>
+        /// <returns></returns>
+        Config GetCurrentConfig();
+    }
+}
