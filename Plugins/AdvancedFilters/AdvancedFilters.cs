bed6d3742b221e51419b6cd5c163dc9e085171b1
Nathanael Jones <nathanael.jones@gmail.com>
2012-09-07 20:33:44 -0400

    AdvancedFilters: Implemented support for prerender filters, implemented first draft of &a.featheredges=100&a.featherin=.96&featherout=0.3 commands for MeM.
    Former-commit-id: 03ed2c41f3f05f0aac9aa7ad5b1c36e466c595e2

--- Plugins/AdvancedFilters/AdvancedFilters.cs
+++ Plugins/AdvancedFilters/AdvancedFilters.cs
@@ -10,6 +10,7 @@ using System.Globalization;
 using ImageResizer.Util;
 using System.Drawing;
 using ImageResizer.ExtensionMethods;
+using System.Drawing.Imaging;
 
 namespace ImageResizer.Plugins.AdvancedFilters {
     public class AdvancedFilters:BuilderExtension, IPlugin, IQuerystringPlugin {
@@ -46,8 +47,52 @@ namespace ImageResizer.Plugins.AdvancedFilters {
             return -1;
 
         }
+
+        protected override RequestedAction PreRenderImage(ImageState s) {
+            if (s.sourceBitmap == null) return RequestedAction.None;
+            if (!s.settings.WasOneSpecified("a.featheredges")) return RequestedAction.None;
+
+
+            Bitmap cropped = null;
+            try {
+                //Make sure cropping is applied, and use existing prerendered bitmap if present.
+                if (s.preRenderBitmap != null) cropped = s.preRenderBitmap;
+                else if (s.copyRect.X != 0 || s.copyRect.Y != 0 || s.copyRect.Width != s.originalSize.Width || s.copyRect.Height != s.originalSize.Height || s.sourceBitmap.PixelFormat != PixelFormat.Format32bppArgb) {
+                    cropped = s.sourceBitmap.Clone(s.copyRect, PixelFormat.Format32bppArgb);
+                    cropped.MakeTransparent();
+                } else {
+                    cropped = s.sourceBitmap;
+                }
+                ApplyPreFiltersTo(ref cropped, s);
+                s.preRenderBitmap = cropped;
+
+            } finally {
+                if (cropped != null & cropped != s.sourceBitmap && cropped != s.preRenderBitmap) cropped.Dispose();
+            }
+            return RequestedAction.None;
+        }
+
         protected override RequestedAction PostRenderImage(ImageState s) {
             if (s.destBitmap == null) return RequestedAction.None;
+            Bitmap b = s.destBitmap;
+            ApplyFiltersTo(ref b, s);
+            s.destBitmap = b;
+            return RequestedAction.None;
+        }
+
+        protected void ApplyPreFiltersTo(ref Bitmap b, ImageState s) {
+            string str = null;
+            int i = 0;
+
+            double units = s.settings.Get<double>("a.radiusunits", 1000);
+
+            float fin = s.settings.Get<float>("a.featherin", 1);
+            float fout = s.settings.Get<float>("a.featherout", 0);
+
+            i = GetRadius(s, "a.featheredges", null, units * (Util.PolygonMath.GetShortestPair(s.layout["image"]) / Math.Min(b.Width,b.Height)));
+            if (i > 0) new FeatherEdge(fout, fin, i).ApplyInPlace(b);
+        }
+        protected void ApplyFiltersTo(ref Bitmap b, ImageState s){
 
             //TODO: if the image is unrotated, use a rectangle to limit the effect to the desired area
 
@@ -58,19 +103,19 @@ namespace ImageResizer.Plugins.AdvancedFilters {
             double units = s.settings.Get<double>("a.radiusunits",1000);
            
             i = GetRadius(s, "blur", "a.blur", units);
-            if (i > 0) new GaussianBlur(1.4, i).ApplyInPlace(s.destBitmap);
+            if (i > 0) new GaussianBlur(1.4, i).ApplyInPlace(b);
 
             i = GetRadius(s, "sharpen", "a.sharpen", units);
-            if (i > 0) new GaussianSharpen(1.4, Math.Min(11,i)).ApplyInPlace(s.destBitmap);
+            if (i > 0) new GaussianSharpen(1.4, Math.Min(11,i)).ApplyInPlace(b);
 
             i = GetRadius(s, "a.oilpainting", null, units);
-            if (i > 0) new OilPainting(i).ApplyInPlace(s.destBitmap);
+            if (i > 0) new OilPainting(i).ApplyInPlace(b);
 
             if ("true".Equals(s.settings["a.removenoise"], StringComparison.OrdinalIgnoreCase)) {
-                new ConservativeSmoothing(3).ApplyInPlace(s.destBitmap);
+                new ConservativeSmoothing(3).ApplyInPlace(b);
             } else {
                 i = GetRadius(s, "a.removenoise", null, units);
-                if (i > 0) new ConservativeSmoothing(i).ApplyInPlace(s.destBitmap);
+                if (i > 0) new ConservativeSmoothing(i).ApplyInPlace(b);
             }
 
 
@@ -78,36 +123,41 @@ namespace ImageResizer.Plugins.AdvancedFilters {
             //Sobel only supports 8bpp grayscale images.
             //true/false
             if ("true".Equals(s.settings["a.sobel"], StringComparison.OrdinalIgnoreCase)){
-                using (s.destBitmap){
-                    s.destBitmap = Grayscale.CommonAlgorithms.Y.Apply(s.destBitmap);
-                    
+                Bitmap old = b;
+                try{
+                    b = Grayscale.CommonAlgorithms.Y.Apply(b);
+                }finally{
+                    if (old != s.sourceBitmap) old.Dispose();
                 }
-                new SobelEdgeDetector().ApplyInPlace(s.destBitmap);
+                
+                new SobelEdgeDetector().ApplyInPlace(b);
 
                 str = s.settings["a.threshold"]; //radius
                 if (!string.IsNullOrEmpty(str) && int.TryParse(str, NumberStyles.Integer, NumberFormatInfo.InvariantInfo, out i) && i > 0)
-                 new Threshold(i).ApplyInPlace(s.destBitmap);
+                 new Threshold(i).ApplyInPlace(b);
     
             }
             //Canny Edge Detector only supports 8bpp grayscale images.
             //true/false
             if ("true".Equals(s.settings["a.canny"], StringComparison.OrdinalIgnoreCase)) {
-                using (s.destBitmap) {
-                    s.destBitmap = Grayscale.CommonAlgorithms.Y.Apply(s.destBitmap);
-
+                Bitmap old = b;
+                try {
+                    b = Grayscale.CommonAlgorithms.Y.Apply(b);
+                } finally {
+                    if (old != s.sourceBitmap) old.Dispose();
                 }
-                new CannyEdgeDetector().ApplyInPlace(s.destBitmap);
+                new CannyEdgeDetector().ApplyInPlace(b);
 
 
             }
 
             //true/false - duplicate with SimpleFilters?
             if ("true".Equals(s.settings["a.sepia"], StringComparison.OrdinalIgnoreCase))
-                new Sepia().ApplyInPlace(s.destBitmap);
+                new Sepia().ApplyInPlace(b);
             
             //true/false
             if ("true".Equals(s.settings["a.equalize"], StringComparison.OrdinalIgnoreCase))
-                new HistogramEqualization().ApplyInPlace(s.destBitmap);
+                new HistogramEqualization().ApplyInPlace(b);
 
             ///White balance adjustment
             var whiteAlg = s.settings.Get<HistogramThresholdAlgorithm>("a.balancewhite");
@@ -115,9 +165,9 @@ namespace ImageResizer.Plugins.AdvancedFilters {
 
 
             if (whiteAlg != null || whiteVal != null) {
-                var b = new AutoWhiteBalance(whiteAlg ?? HistogramThresholdAlgorithm.Area);
-                if (whiteVal != null) b.LowThreshold = b.HighThreshold = whiteVal.Value / 100;
-                b.ApplyInPlace(s.destBitmap);
+                var bal = new AutoWhiteBalance(whiteAlg ?? HistogramThresholdAlgorithm.Area);
+                if (whiteVal != null) bal.LowThreshold = bal.HighThreshold = whiteVal.Value / 100;
+                bal.ApplyInPlace(b);
             }
 
             str = s.settings["a.posterize"]; //number of colors to merge
@@ -126,7 +176,7 @@ namespace ImageResizer.Plugins.AdvancedFilters {
                 if (i < 1) i = 1; 
                 if (i > 255) i = 255;
                 sp.PosterizationInterval =(byte)i;
-                sp.ApplyInPlace(s.destBitmap); 
+                sp.ApplyInPlace(b); 
             }
 
             //Pixellate doesn't support 32-bit images, only 24-bit
@@ -145,7 +195,7 @@ namespace ImageResizer.Plugins.AdvancedFilters {
             if (contrast != 0 || brightness != 0 || saturation != 0){
                 HSLLinear adjust = new HSLLinear();
                 AdjustContrastBrightnessSaturation(adjust, contrast, brightness, saturation, "true".Equals(s.settings["a.truncate"]));
-                adjust.ApplyInPlace(s.destBitmap);
+                adjust.ApplyInPlace(b);
             }
             //TODO - add grayscale?
 
@@ -153,7 +203,6 @@ namespace ImageResizer.Plugins.AdvancedFilters {
 
             //For trimming solid-color whitespace, use Shrink
 
-            return RequestedAction.None;
         }
 
         /// <summary>
@@ -199,7 +248,7 @@ namespace ImageResizer.Plugins.AdvancedFilters {
         public IEnumerable<string> GetSupportedQuerystringKeys() {
             return new string[] { "blur", "sharpen" , "a.blur", "a.sharpen", "a.oilpainting", "a.removenoise", 
                                 "a.sobel", "a.threshold", "a.canny", "a.sepia", "a.equalize", "a.posterize", 
-                                "a.contrast", "a.brightness", "a.saturation","a.truncate","a.balancewhite", "a.balancethreshold"};
+                                "a.contrast", "a.brightness", "a.saturation","a.truncate","a.balancewhite", "a.balancethreshold", "a.featheredges"};
         }
     }
 }
