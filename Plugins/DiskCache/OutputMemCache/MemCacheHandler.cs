52e6c9ac1b6830ea75f2a1349a960f0d60302ed2
Nathanael Jones <nathanael.jones@gmail.com>
2012-09-07 20:37:53 -0400

    Implemented first draft of MemCache plugin
    Former-commit-id: 81329306cc5b1ed89e4b60fb96ca0f0bb82c0a2c

---
+++ Plugins/DiskCache/OutputMemCache/MemCacheHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using ImageResizer.Caching;
+
+namespace ImageResizer.Plugins.MemCache {
+        /// <summary>
+    /// Implements IHttpHandler, serves content for the NoCache plugin
+    /// </summary>
+    public class MemCacheHandler : IHttpHandler {
+        private IResponseArgs e;
+        private byte[] data;
+
+        public MemCacheHandler(IResponseArgs e, byte[] data) {
+            this.e = e;
+            this.data = data;
+        }
+
+        public bool IsReusable {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context) {
+            context.Response.StatusCode = 200;
+            context.Response.BufferOutput = false; 
+            e.ResponseHeaders.ApplyDuringPreSendRequestHeaders = false;
+            e.ResponseHeaders.ApplyToResponse(e.ResponseHeaders, context);
+            for (int i = 0; i < data.Length; i += 4096) {
+                context.Response.OutputStream.Write(this.data, i, Math.Min(4096, data.Length - i));
+            }
+
+        }
+    }
+}
