4ba087eb20767e6c26d4d71eda7a9a08b0b1793e
Nathanael Jones <nathanael.jones@gmail.com>
2012-09-07 20:33:44 -0400

    AdvancedFilters: Implemented support for prerender filters, implemented first draft of &a.featheredges=100&a.featherin=.96&featherout=0.3 commands for MeM.

---
+++ Plugins/AdvancedFilters/FeatherEdge.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AForge.Imaging.Filters;
+using System.Drawing.Imaging;
+using AForge.Imaging;
+using System.Drawing;
+using ImageResizer.ExtensionMethods;
+
+namespace ImageResizer.Plugins.AdvancedFilters {
+    public class FeatherEdge : BaseInPlacePartialFilter {
+
+        public FeatherEdge(double outerAlpha, double innerAlpha, double width ):this() {
+            this.OuterAlpha = outerAlpha;
+            this.InnerAlpha = innerAlpha;
+            this.Width = width;
+
+        }
+
+        public double OuterAlpha { get; set; }
+
+        public double InnerAlpha { get; set; }
+
+        public double Width { get; set; }
+
+        private Dictionary<PixelFormat, PixelFormat> formatTranslations = new Dictionary<PixelFormat, PixelFormat>();
+        public override Dictionary<PixelFormat, PixelFormat> FormatTranslations {
+            get { return formatTranslations; }
+        }
+
+
+
+        public FeatherEdge() {
+            formatTranslations[PixelFormat.Format32bppArgb] = PixelFormat.Format32bppArgb;
+        }
+
+
+
+
+
+        protected override unsafe void ProcessFilter(UnmanagedImage image, Rectangle rect) {
+            int pixelSize = (image.PixelFormat == PixelFormat.Format8bppIndexed) ? 1 :
+                (image.PixelFormat == PixelFormat.Format24bppRgb) ? 3 : 4;
+
+            if (pixelSize != 4) throw new InvalidImagePropertiesException();
+
+            int startX = rect.Left;
+            int startY = rect.Top;
+            int stopX = startX + rect.Width;
+            int stopY = startY + rect.Height;
+            int stride = image.Stride;
+            int offset = stride - rect.Width * pixelSize;
+
+            int numberOfPixels = (stopX - startX) * (stopY - startY);
+
+            // color image
+            byte* ptr = (byte*)image.ImageData.ToPointer();
+            // allign pointer to the first pixel to process
+            ptr += (startY * stride + startX * pixelSize);
+
+
+            double width = Width;
+            double inner = InnerAlpha;
+            double outer = OuterAlpha;
+            double diff =  OuterAlpha - InnerAlpha;
+            const short a = RGB.A;
+            int w = (int)Math.Round(width);
+
+
+            for (int y = startY; y < stopY; y++) {
+                int ydist = Math.Max(0, Math.Max(startY + w - y, y - (stopY - 1 - w)));
+
+                for (int x = startX; x < stopX; x++, ptr += pixelSize) {
+                    int xdist = Math.Max(0, Math.Max(startX + w - x, x - (stopX - 1 - w)));
+                    double dist = xdist > 0 && ydist > 0 ? Math.Round(Math.Sqrt(xdist * xdist + ydist * ydist)): Math.Max(xdist,ydist);
+
+                    if (dist <= 0 || w == 0) {
+                        ptr[a] = (byte)Math.Round((double)ptr[a] * inner);
+                    } else if (dist > w){
+                        ptr[a] = (byte)Math.Round((double)ptr[a] * outer);
+                    } else {
+                        double t = dist / width;
+                        //t = Math.Sin(Math.PI * t / 2);
+                        t = 3 * t * t - 2 * t * t * t;
+                        //t = 6 * Math.Pow(t, 5) - 15 * Math.Pow(t, 4) + 10 * Math.Pow(t, 3);
+                        ptr[a] = (byte)Math.Round((double)ptr[a] * (inner + diff * t));
+                    }
+                }
+                ptr += offset;
+            }
+        }
+
+    }
+}
