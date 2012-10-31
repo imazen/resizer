eb4a99ef346fe039806dc287ffb1d5f16783091e
Nathanael Jones <nathanael.jones@gmail.com>
2012-10-31 18:32:10 -0400

    Faces: Renamed ResizingCanceledException to AlternateResponseException and CropAround.cs to CropAroundPlugin.cs

--- Plugins/Faces/AlternateResponseException.cs
+++ Plugins/Faces/AlternateResponseException.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace ImageResizer.Plugins.Faces {
-    public class ResizingCanceledException:Exception {
+    public class AlternateResponseException:Exception {
         public byte[] ResponseData { get; set; }
         public string ContentType { get; set; }
         public int StatusCode { get; set; }
-        public ResizingCanceledException(string message) : base(message) { }
+        public AlternateResponseException(string message) : base(message) { }
 
     }
 }
