cf21354cfab937ae656c76710afac443d77e2103
Nathanael Jones <nathanael.jones@gmail.com>
2012-12-19 20:49:37 -0500

    Fixed AzureReader2 bundle designation

--- Plugins/AzureReader2/Properties/AssemblyInfo.cs
+++ Plugins/AzureReader2/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using ImageResizer.Util;
 
 // So the ImageResizer knows which bundle this assembly belongs to
-[assembly: Bundle("R3Bundle3b")]
+[assembly: Bundle("R3Bundle3")]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
