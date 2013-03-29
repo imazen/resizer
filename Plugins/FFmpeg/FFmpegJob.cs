d57b95ae7411d714758139b3c6a4515018626ec3
Nathanael Jones <nathanael.jones@gmail.com>
2013-03-29 19:26:22 -0400

    Added first draft of FFmpeg plugin

---
+++ Plugins/FFmpeg/FFmpegJob.cs
@@ -0,0 +1,61 @@
+﻿using ImageResizer.Util;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageResizer.Plugins.FFmpeg
+{
+    public class FFmpegJob
+    {
+
+        public FFmpegJob()
+        {
+            Timeout = 15000; //15 seconds
+        }
+
+
+        public FFmpegJob(NameValueCollection query) :this()
+        {
+
+            Seconds = ParseUtils.ParsePrimitive<double>(query["ffmpeg.seconds"]);
+            Percent = ParseUtils.ParsePrimitive<double>(query["ffmpeg.percent"]);
+
+            SkipBlankFrames = ParseUtils.ParsePrimitive<bool>(query["ffmpeg.skipblankframes"]);
+
+        }
+
+
+        public string SourcePath { get; set; }
+
+        public Stream Result { get; set; }
+
+        /// <summary>
+        /// If true, frames will be energy-anlayzed to verify they're not blank. 
+        /// </summary>
+        public bool? SkipBlankFrames { get; set; }
+
+        /// <summary>
+        /// How many seconds to FF when a blank frame is found
+        /// </summary>
+        public double? IncrementWhenBlank { get; set; }
+
+        /// <summary>
+        /// How many seconds within the video to grab the frame
+        /// </summary>
+        public double? Seconds { get; set; }
+
+        /// <summary>
+        /// What percentage within the video to grab a frame. Using this instead of seconds will result in slower execution, as the video size will have to be retrieved first.
+        /// </summary>
+        public double? Percent { get; set; }
+
+        /// <summary>
+        /// How many milliseconds to wait before timing out
+        /// </summary>
+        public int Timeout { get; set; }
+    }
+}
---
+++ Plugins/FFmpeg/FFmpegPlugin.cs
@@ -0,0 +1,153 @@
+﻿using ImageResizer.Configuration;
+using ImageResizer.Resizing;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageResizer.Plugins.FFmpeg
+{
+    public class FFmpegPlugin : IVirtualImageProvider, IPlugin, IFileExtensionPlugin, IQuerystringPlugin
+
+    {
+
+        Config c;
+        public IPlugin Install(Config c)
+        {
+            this.c = c;
+            c.Plugins.add_plugin(this);
+            return this;
+        }
+
+        public bool Uninstall(Config c)
+        {
+            c.Plugins.remove_plugin(this);
+            return true;
+        }
+        private FFmpegManager mgr = new FFmpegManager();
+
+
+        public IEnumerable<string> GetSupportedFileExtensions()
+        {
+            return new string[] {"avi", "mp4","avchd","flv","fla","swf","mpg","mpeg","mpe","mov","m4v","mkv","wmv" };
+        }
+
+        /// <summary>
+        /// Returns true if the specified file and querystring indicate a PSD composition request
+        /// </summary>
+        /// <param name="virtualPath"></param>
+        /// <param name="queryString"></param>
+        /// <returns></returns>
+        public bool FileExists(string virtualPath, NameValueCollection queryString)
+        {
+            return IsPathVideoFileWithOurCommands(virtualPath, queryString) && c.Pipeline.FileExists(virtualPath, new NameValueCollection());
+        }
+
+        /// <summary>
+        /// Returns a virtual file instance for the specified specified file and querystring, if they indicate a PSD composition request. 
+        /// Otherwise, null is returned.
+        /// </summary>
+        /// <param name="virtualPath"></param>
+        /// <param name="queryString"></param>
+        /// <returns></returns>
+        public IVirtualFile GetFile(string virtualPath, NameValueCollection queryString)
+        {
+            if (IsPathVideoFileWithOurCommands(virtualPath, queryString) && c.Pipeline.FileExists(virtualPath, new NameValueCollection()))
+                return new FFmpegVirtualFile(virtualPath, queryString, this);
+            else
+                return null;
+        }
+
+
+        public IEnumerable<string> GetSupportedQuerystringKeys()
+        {
+            return new string[] {"ffmpeg.seconds" ,"ffmpeg.percent","ffmpeg.skipblankframes" };
+        }
+
+ 
+
+        /// <summary>
+        /// True if the file is a .psd.jpeg, .psd.png, etc file.
+        /// </summary>
+        protected bool IsPathVideoFileWithOurCommands(string virtualPath, NameValueCollection queryString = null)
+        {
+            var exts = GetSupportedFileExtensions();
+
+            var full = ImageResizer.Util.PathUtils.GetFullExtension(virtualPath).ToLowerInvariant().TrimStart('.');
+            var parts = full.Split('.');
+            if (parts.Length < 1) return false;
+            //Only accept the file if our querystring is on it.
+            if (exts.Contains(parts[parts.Length - 1]))
+            {
+                if (queryString == null) queryString = c.Pipeline.ModifiedQueryString;
+                foreach (string s in GetSupportedQuerystringKeys())
+                {
+                    if (!string.IsNullOrEmpty(queryString[s])) return true;
+                }
+            }
+            return false;
+        }
+
+
+        public Stream GetFrameStream(string virtualPath, NameValueCollection queryString)
+        {
+           return mgr.GetFrameStream(c,virtualPath, queryString);
+        }
+
+    }
+
+
+    public class FFmpegVirtualFile : IVirtualFile
+    {
+
+        private FFmpegPlugin provider;
+
+        private Nullable<bool> _exists = null;
+        //private Nullable<DateTime> _fileModifiedDate = null;
+
+        /// <summary>
+        /// Returns true if the row exists. 
+        /// </summary>
+        public bool Exists
+        {
+            get
+            {
+                if (_exists == null) _exists = provider.FileExists(this.VirtualPath, this.Query);
+                return _exists.Value;
+            }
+        }
+
+        public FFmpegVirtualFile(string virtualPath, NameValueCollection query, FFmpegPlugin provider)
+        {
+            this.provider = provider;
+            this._virtualPath = virtualPath;
+            this._query = query;
+
+        }
+
+        private string _virtualPath = null;
+
+        public string VirtualPath
+        {
+            get { return _virtualPath; }
+        }
+        private NameValueCollection _query;
+
+        public NameValueCollection Query
+        {
+            get { return _query; }
+            set { _query = value; }
+        }
+
+        /// <summary>
+        /// Returns a stream of the encoded file bitmap using the current request querystring.
+        /// </summary>
+        /// <returns></returns>
+        public Stream Open() { return provider.GetFrameStream(this.VirtualPath, this.Query); }
+
+    }
+}
