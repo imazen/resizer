ca06a381e271386624357f29c656ac9330a43732
Nathanael Jones <nathanael.jones@gmail.com>
2013-09-02 14:03:43 -0400

    Added DictionaryUtils

---
+++ Core/Util/DictionaryUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageResizer.Util
+{
+    class DictionaryUtils
+    {
+        protected internal static K GetValueOrDefault<K>(IDictionary<string,object> d, string key, K defaultValue) 
+        {
+            return d.ContainsKey(key) ? (K)d[key] : defaultValue;
+        }
+    }
+}
