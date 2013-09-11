596b37caa6b3597d226a0652ac379ffec3a05e8f
Nathanael Jones <nathanael.jones@gmail.com>
2013-09-10 22:53:05 -0400

    Added first draft of S3Reader2
    Former-commit-id: 58df14c2475c923a2cfe1827cd0cdb985f20a4bf

---
+++ Plugins/S3Reader2/ImageUploadHelperExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageResizer
+{
+    public static class ImageUploadHelperS3Extensions
+    {
+    }
+}
