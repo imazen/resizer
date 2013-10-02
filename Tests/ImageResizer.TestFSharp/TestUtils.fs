ecfb5930b634e7a723c97c0552d2df245d9a42dd
Nathanael Jones <nathanael.jones@gmail.com>
2013-10-01 22:39:18 -0400

    Got F# unit tests working
    Former-commit-id: 62a92df40b9db00ea230cb2842514c4645afa7e1

--- Tests/ImageResizer.TestFSharp/TestUtils.fs
+++ Tests/ImageResizer.TestFSharp/TestUtils.fs
@@ -3,6 +3,9 @@
 open System.Resources
 open System.Reflection
 
-let get800x600 ()=
-        Assembly.GetExecutingAssembly().GetManifestResourceStream("ImageResizer.TestFSharp.800x600white.png")
+exception MissingResource of string
 
+let get800x600 ()=
+  let stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("800x600white.png")
+  if stream = null then raise (MissingResource("Failed to load embedded resource"))
+  stream
