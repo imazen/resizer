3b6770d318cc1f53f3d88c1cf714ee4f8e9bb8cb
Nathanael Jones <nathanael.jones@gmail.com>
2013-10-01 23:04:23 -0400

    Tests: Simplified regression tests

--- Tests/ImageResizer.TestFSharp/RegressionTests.fs
+++ Tests/ImageResizer.TestFSharp/RegressionTests.fs
@@ -12,20 +12,21 @@ type ``Given a 800x600 bitmap`` ()=
   let b = get800x600()
   let c = new Config()
 
-  [<Fact>] 
-  member this.``manual cropping should correctly affect the final size of the image``() =
-      let i = new Instructions("cropxunits=100;cropyunits=100;crop=10,30,90,60")
-      let result = c.CurrentImageBuilder.Build(new ImageJob(b, typedefof<Bitmap>, i)).Result :?> Bitmap
-      result.Size |> should equal (new Size(640,180))
+  member test.getResultSize(source, instructions:Instructions) = 
+    use result = c.CurrentImageBuilder.Build(new ImageJob(source, typedefof<Bitmap>, instructions)).Result :?> Bitmap
+    result.Size 
+
 
   [<Fact>] 
-  member this.``manual cropping should correctly affect the final size of the image when srotate=0``() =
-      let i = new Instructions("cropxunits=100;cropyunits=100;crop=10,30,90,60;srotate=0")
-      let result = c.CurrentImageBuilder.Build(new ImageJob(b, typedefof<Bitmap>, i)).Result :?> Bitmap
-      result.Size |> should equal (new Size(640,180))
+  member test.``manual cropping should correctly affect the final size of the image``() =
+      test.getResultSize(b, new Instructions("cropxunits=100;cropyunits=100;crop=10,30,90,60")) |> should equal (new Size(640,180))
 
   [<Fact>] 
-  member this.``manual cropping should correctly affect the final size of the image when srotate=90``() =
-      let i = new Instructions("cropxunits=100;cropyunits=100;crop=10,30,90,60;srotate=90")
-      let result = c.CurrentImageBuilder.Build(new ImageJob(b, typedefof<Bitmap>, i)).Result :?> Bitmap
-      result.Size |> should equal (new Size(480, 240))
+  member test.``manual cropping should correctly affect the final size of the image when srotate=0``() =
+      test.getResultSize(b, new Instructions("cropxunits=100;cropyunits=100;crop=10,30,90,60;srotate=0")) |> should equal (new Size(640,180))
+
+  [<Fact>] 
+  member test.``manual cropping should correctly affect the final size of the image when srotate=90``() =
+      test.getResultSize(b, new Instructions("cropxunits=100;cropyunits=100;crop=10,30,90,60;srotate=90")) |> should equal (new Size(480, 240))
+
+
