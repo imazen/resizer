da043c30c42eedf7e04c817374db95de966c376b
Nathanael Jones <nathanael.jones@gmail.com>
2013-10-01 22:39:18 -0400

    Got F# unit tests working

--- Tests/ImageResizer.TestFSharp/RegressionTests.fs
+++ Tests/ImageResizer.TestFSharp/RegressionTests.fs
@@ -12,10 +12,20 @@ type ``Given a 800x600 bitmap`` ()=
   let b = get800x600()
   let c = new Config()
 
-  [<Fact>] member test.
-    ``manual cropping should correctly affect the final size of the image``() =
-      let j = new ImageJob(b, new Instructions("cropxunits=100;cropyunits=100;crop=10,30,90,60"))
-      let expect = new Size(640,180)
-        
-      //c.Build(j).Result.Size |> should equal new Size(640,180)
+  [<Fact>] 
+  member this.``manual cropping should correctly affect the final size of the image``() =
+      let i = new Instructions("cropxunits=100;cropyunits=100;crop=10,30,90,60")
+      let result = c.CurrentImageBuilder.Build(new ImageJob(b, typedefof<Bitmap>, i)).Result :?> Bitmap
+      result.Size |> should equal (new Size(640,180))
 
+  [<Fact>] 
+  member this.``manual cropping should correctly affect the final size of the image when srotate=0``() =
+      let i = new Instructions("cropxunits=100;cropyunits=100;crop=10,30,90,60;srotate=0")
+      let result = c.CurrentImageBuilder.Build(new ImageJob(b, typedefof<Bitmap>, i)).Result :?> Bitmap
+      result.Size |> should equal (new Size(640,180))
+
+  [<Fact>] 
+  member this.``manual cropping should correctly affect the final size of the image when srotate=90``() =
+      let i = new Instructions("cropxunits=100;cropyunits=100;crop=10,30,90,60;srotate=90")
+      let result = c.CurrentImageBuilder.Build(new ImageJob(b, typedefof<Bitmap>, i)).Result :?> Bitmap
+      result.Size |> should equal (new Size(480, 240))
