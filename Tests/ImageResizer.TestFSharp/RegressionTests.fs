d2ff4caefa385de147c677e4816ce3161540a956
Nathanael Jones <nathanael.jones@gmail.com>
2013-10-01 21:09:05 -0400

    Added stub F# testing project and core testing solution
    Former-commit-id: c1025bfdedf44adafe44f1923e99d126ce0bdda0

---
+++ Tests/ImageResizer.TestFSharp/RegressionTests.fs
@@ -0,0 +1,21 @@
+﻿
+namespace ImageResizer.TestFSharp
+
+open Xunit
+open FsUnit.Xunit
+open ImageResizer
+open ImageResizer.Configuration
+open ImageResizer.TestUtils
+open System.Drawing
+
+type ``Given a 800x600 bitmap`` ()=
+  let b = get800x600()
+  let c = new Config()
+
+  [<Fact>] member test.
+    ``manual cropping should correctly affect the final size of the image``() =
+      let j = new ImageJob(b, new Instructions("cropxunits=100;cropyunits=100;crop=10,30,90,60"))
+      let expect = new Size(640,180)
+        
+      //c.Build(j).Result.Size |> should equal new Size(640,180)
+
---
+++ Tests/ImageResizer.TestFSharp/TestUtils.fs
@@ -0,0 +1,8 @@
+﻿module ImageResizer.TestUtils
+
+open System.Resources
+open System.Reflection
+
+let get800x600 ()=
+        Assembly.GetExecutingAssembly().GetManifestResourceStream("ImageResizer.TestFSharp.800x600white.png")
+
