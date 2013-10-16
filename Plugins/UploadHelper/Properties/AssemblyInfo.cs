fea735363a223c7799593d624d82de3a01e1a1c4
Nathanael Jones <nathanael.jones@gmail.com>
2013-10-16 17:23:43 -0400

    Moved unreleased ImageUploadHelper to its own plugin.
    Former-commit-id: 23ceb0f6c45eeb6e4df3059756d4abea0f61700d

---
+++ Plugins/UploadHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using ImageResizer.Util;
+
+// So the ImageResizer knows which bundle this assembly belongs to
+[assembly: Edition("R3Performance")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImageResizer.Plugins.UploadHelper")]
+
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3d977d1e-cc6e-41d1-aa0d-a9ddfc5b1b9a")]
+
