69838c128330d0b5dd5ba4a9742b89a012384a7e
Nathanael Jones <nathanael.jones@gmail.com>
2013-10-16 17:23:02 -0400

    Tag TinyCache as part of Performance Edition.
    Former-commit-id: bd557271e70e4dd89476df49402820f0511f1c96

--- Plugins/TinyCache/Properties/AssemblyInfo.cs
+++ Plugins/TinyCache/Properties/AssemblyInfo.cs
@@ -6,6 +6,7 @@ using ImageResizer.Util;
 
 // So the ImageResizer knows which bundle this assembly belongs to
 [assembly: Bundle("R3Bundle1")]
+[assembly: Edition("R3Performance")]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
