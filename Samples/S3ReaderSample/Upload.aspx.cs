d08d1f88038062c5b7aa56215b82fd1186ca4b9a
Nathanael Jones <nathanael.jones@gmail.com>
2013-11-26 15:03:07 -0500

    Update S3ReaderSample to work with S3Reader2

--- Samples/S3ReaderSample/Upload.aspx.cs
+++ Samples/S3ReaderSample/Upload.aspx.cs
@@ -2,6 +2,7 @@
 using ImageResizer;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -19,11 +20,14 @@ namespace S3ReaderSample
 
                 var client = new Amazon.S3.AmazonS3Client(Request["awsid"], Request["awssecret"], Amazon.RegionEndpoint.EUWest1);
                 
-                
+                //For some reason we have to buffer the file in memory to prevent issues... Need to research further
+                var ms = new MemoryStream();
+                upload.PostedFile.InputStream.CopyTo(ms);
+                ms.Seek(0, SeekOrigin.Begin);
 
-                var request = new Amazon.S3.Model.PutObjectRequest() {  BucketName = this.bucket.Text, Key = name, InputStream = upload.PostedFile.InputStream, CannedACL = Amazon.S3.S3CannedACL.PublicRead };
+                var request = new Amazon.S3.Model.PutObjectRequest() {  BucketName = this.bucket.Text, Key = name, InputStream = ms, CannedACL = Amazon.S3.S3CannedACL.PublicRead };
                
-
+                
                 var response = client.PutObject(request);
                 if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
                 {
