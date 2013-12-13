ca1c463b27bd79cfb25c4ab88f0705c435fe9abb
Jared Reisinger <jaredreisinger@hotmail.com>
2013-12-12 16:59:34 -0800

    Fixes issue #26: Created `SeekableStreamWrapper` that can ensure a source stream is wrapped by a seekable helper if it isn't already seekable.
    Implemented in Core/Util so that (a) it's available to any code with a reference to ImageResizer, (b) it was easy to put tests into Core.Tests, and (c) future enhancements (buffer on-demand, instead of all up-front) can be done in-place without altering calling code.  For now, only RemoteReader is making use of the class, but since it's in Core, it can also be used by the ImageResizer framework if desired.
    
    
    Former-commit-id: 0eb36d358b3bf8cc02bf04a45e67dcfa659a2b2a

---
+++ Core/Util/SeekableStreamWrapper.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ImageResizer.ExtensionMethods;
+
+namespace ImageResizer.Util
+{
+    /// <summary>
+    /// A Stream utility class that helps to provide seekability to any source
+    /// stream.
+    /// </summary>
+    public class SeekableStreamWrapper : MemoryStream
+    {
+        private Stream inner;
+        private bool disposeInner;
+        private bool writable;
+
+        /// <summary>
+        /// Returns a seekable stream for a given source stream.  If wrapped,
+        /// the source stream will be disposed when the returned stream is.
+        /// </summary>
+        /// <param name="sourceStream">The stream to wrap or return.</param>
+        /// <returns>If the source stream is already seekable, it is returned
+        /// unwrapped.  If not, it is wrapped in a <c>SeekableStreamWrapper</c>.</returns>
+        public static Stream FromStream(Stream sourceStream)
+        {
+            bool disposeStream = true;
+            return FromStream(sourceStream, ref disposeStream);
+        }
+
+        /// <summary>
+        /// Returns a seekable stream for a given source stream.  If wrapped,
+        /// the source stream will be disposed when the returned stream is.
+        /// </summary>
+        /// <param name="sourceStream">The stream to wrap or return.</param>
+        /// <param name="disposeStream">Whether to dispose the source stream
+        /// when the wrapper is disposed.  If a wrapper is created, this
+        /// parameter will be <c>true</c> on the return to ensure the wrapper
+        /// gets properly disposed.</param>
+        /// <returns>If the source stream is already seekable, it is returned
+        /// unwrapped.  If not, it is wrapped in a <c>SeekableStreamWrapper</c>.</returns>
+        public static Stream FromStream(Stream sourceStream, ref bool disposeStream)
+        {
+            // If the stream is already seekable, we can just return it directly.
+            if (sourceStream.CanSeek)
+            {
+                return sourceStream;
+            }
+
+            var disposeInner = disposeStream;
+            disposeStream = true;
+
+            return new SeekableStreamWrapper(sourceStream, disposeInner);
+        }
+
+        private SeekableStreamWrapper(Stream inner, bool disposeInner)
+        {
+            this.inner = inner;
+            this.disposeInner = disposeInner;
+
+            // Copy the original stream, and reset the position back to the
+            // beginning.  We *could* immediately dispose the inner stream, but
+            // we're keeping it around so that its lifetime is consistent with
+            // what it would be if the wrapper weren't being used.
+            this.writable = true;
+            StreamExtensions.CopyToStream(inner, this);
+            this.writable = false;
+
+            this.Seek(0, SeekOrigin.Begin);
+        }
+
+        /// <summary>
+        /// Disposes the seekable stream wrapper, and the wrapped stream if
+        /// originally requested.
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.disposeInner)
+                {
+                    this.inner.Dispose();
+                }
+
+                this.inner = null;
+            }
+
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Gets whether the stream wrapper can be written to or not.
+        /// This is only <c>true</c> while the wrapper is being initialized, and
+        /// otherwise <c>false</c> for all other callers.
+        /// </summary>
+        public override bool CanWrite { get { return this.writable; } }
+    }
+}
