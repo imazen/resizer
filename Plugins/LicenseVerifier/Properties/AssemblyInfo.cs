98dd0a0233dfcc2041f809cd6a4dfb48cbe8f65e
Dale Ragan <dale@ragan.io>
2014-03-04 20:10:03 -0500

    Start of the LicenseVerifier plugin.
    Former-commit-id: 93c9ace2ab6406051ad15b204e34ac00ba269c7d

---
+++ Plugins/LicenseVerifier/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImageResizer.Plugins.LicenseVerifier")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c581f7a1-b6ff-4d9d-bcef-2b25972833a7")]
