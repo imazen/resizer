95f2f126fb1db037d516a5baaf578e8c26340926
Dale Ragan <dale@ragan.io>
2014-03-06 13:48:36 -0500

    Changed out Gallio/MbUnit for Fixie. Also, included a failing test.

---
+++ Plugins/LicenseVerifier/LicenseService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageResizer.Plugins.LicenseVerifier {
+    
+    public class LicenseService : ILicenseService {
+
+        public void NotifyUse(string domain, Guid feature) {
+            throw new NotImplementedException();
+        }
+
+        public void SetFriendlyName(Guid id, string featureDisplayName) {
+            throw new NotImplementedException();
+        }
+
+        public long VerifyAuthenticity(Guid feature) {
+            throw new NotImplementedException();
+        }
+
+        public string GetLicensingOverview(bool forceVerification) {
+            throw new NotImplementedException();
+        }
+
+        public IPlugin Install(Configuration.Config c) {
+            throw new NotImplementedException();
+        }
+
+        public bool Uninstall(Configuration.Config c) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/ImageResizer.Plugins.LicenseVerifier.Tests/LicenseServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageResizer.Plugins.LicenseVerifier.Tests {
+    // Each plugin will notify the service of it's use, by calling NotifyUse
+    // The service will then mark the feature/plugin as pending if it's state is unknown.
+    // We will ping and retrieve the license information based on the appid.
+    // Once we have the information, we then go through the list and mark the features accrodingly.
+    // We also cache the licenses retrieved for later use.
+    public class LicenseServiceTests {
+        public void Should_mark_features_we_do_not_know_the_status_of_pending() {
+            var featureOne = Guid.NewGuid();
+            var featureTwo = Guid.NewGuid();
+            var featureThree = Guid.NewGuid();
+
+            var licenseService = new LicenseService();
+            
+            licenseService.NotifyUse("domain.com", featureOne);
+            licenseService.NotifyUse("domain.com", featureTwo);
+            licenseService.NotifyUse("domain.com", featureThree);
+        }
+    }
+}
