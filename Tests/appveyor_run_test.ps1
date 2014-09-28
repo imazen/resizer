aa0bb841f7f0a037b6f769e09b754b3e5a3095fb
GravitasExtra <philip.smith@philipsmith.eu>
2014-09-28 14:40:52 +0100

    xunit 2 update

---
+++ Tests/appveyor_run_test.ps1
@@ -0,0 +1,23 @@
+param([string]$assembly, [switch]$run32bit)
+
+cd Packages/xunit.runners*/Tools
+
+
+$xml_out_file = ".\xunit-results-$($assembly)-$($run32bit).xml"
+
+$dll_path = "..\..\..\Tests\binaries\release\$($assembly).dll"
+
+$runner = ".\xunit.console.exe"
+switch ($run32bit){
+	$true { $runner = ".\xunit.console.x86.exe"; break }
+}
+
+
+& "$($runner)"  $dll_path "-xmlv1" "$($xml_out_file)"
+
+
+# upload results to AppVeyor
+$wc = New-Object 'System.Net.WebClient'
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $xml_out_file))
+
+cd ..\..\..\
