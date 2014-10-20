065ea21b8ddc9028f02ff68a597b97c7e53eb433
Nathanael Jones <nathanael.jones@gmail.com>
2014-10-20 19:53:07 -0400

    Add a test to verify percent concurrency math
    Former-commit-id: 7394a8afe94f3ba9132e55779fac2e64596adb52

--- Tests/Core.Tests/TestPolygonMath.cs
+++ Tests/Core.Tests/TestPolygonMath.cs
@@ -5,12 +5,23 @@ using Xunit;
 
 namespace ImageResizer.Tests {
  
-    public class TestPolygonMath {
-        [Fact]
-        public void Test() {
-            //
-            // TODO: Add test logic here
-            //
+    public static class TestPolygonMath {
+        [Theory]
+        [InlineData(0d, 8d, 5d, 40d)]
+        [InlineData(1d, 8d, 5d, 5d)]
+        [InlineData(-1d, 8d, 5d, 80d)]
+        public static void TestPercentConcurrent(double expected, double degree, double sequentialTime, double parallelTime) {
+
+            var serialized = sequentialTime * degree;
+
+            //var result = ((sequentialTime * degree) - parallelTime) / (sequentialTime * (degree-1));
+            double result;
+            //var result = ((sequentialTime * degree) - parallelTime) / (sequentialTime * (degree-1));
+            result = (serialized - parallelTime) / (serialized - (parallelTime <= serialized ? sequentialTime : 0));
+
+          
+            
+            Assert.InRange(result, expected * (expected > 0 ? 0.99 : 1.01), expected * (expected > 0 ? 1.01 : 0.99));
         }
     }
 }
