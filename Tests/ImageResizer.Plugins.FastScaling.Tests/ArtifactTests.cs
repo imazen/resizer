2dc68c8605a7ebb279bccf4009d58bf33ae67ec0
Nathanael Jones <nathanael.jones@gmail.com>
2014-11-04 15:15:55 -0500

    FastScaling: Add failing LostBorder test
    Former-commit-id: a6fb9f694d04c92004a0430bf5458bdda75efa7f

--- Tests/ImageResizer.Plugins.FastScaling.Tests/ArtifactTests.cs
+++ Tests/ImageResizer.Plugins.FastScaling.Tests/ArtifactTests.cs
@@ -1,4 +1,5 @@
 using ImageResizer.Configuration;
+using ImageResizer.Util;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -24,15 +25,8 @@ namespace ImageResizer.Plugins.FastScaling.Tests
             }
         }
 
-        [Fact]
-        public void CheckForBlurredEdges()
+        public static Bitmap CreateRingedBitmap(int w, int h, Color background, Color inner, int margin )
         {
-            var  background = Color.FromArgb(255, 0, 0, 0);
-            var inner = Color.FromArgb(255, 240, 189, 35);
-            var w = 200;
-            var h = 86;
-            var margin = 6;
-
             var b = new Bitmap(w, h);
             using (var g = Graphics.FromImage(b))
             {
@@ -40,28 +34,92 @@ namespace ImageResizer.Plugins.FastScaling.Tests
 
                 g.FillRectangle(new SolidBrush(inner), margin, margin, w - margin - margin, h - margin - margin);
             }
+            return b;
+        }
+        public static void AssertBorderColor(Color expected, Bitmap b){
+            AssertBorderColor(expected,b,0,0,b.Width,b.Height);
+        }
 
+        public static void AssertBorderColor(Color expected, Bitmap b, int x, int y, int w, int h){
+            Assert.Equal(expected, b.GetPixel(x, y));
+            Assert.Equal(expected, b.GetPixel(x + w / 2, y));
+            Assert.Equal(expected, b.GetPixel(x + w - 1, y));
+            Assert.Equal(expected, b.GetPixel(x + w - 1, y + h / 2));
+            Assert.Equal(expected, b.GetPixel(x + w - 1, y + h -1));
+            Assert.Equal(expected, b.GetPixel(x + w / 2 , y + h -1));
+            Assert.Equal(expected, b.GetPixel(x, y + h -1));
+            Assert.Equal(expected, b.GetPixel(x, y + h / 2));  
+        }
+
+        public static Bitmap BuildWithFastScaling(Bitmap source, Instructions instructions)
+        {
+            var i = new Instructions(instructions);
+            i["fastscale"] = "true";
             var c = new Config();
             new FastScalingPlugin().Install(c);
 
-            var job = new ImageJob(b, typeof(Bitmap), new Instructions() { Width = 50 });
+            var job = new ImageJob(source, typeof(Bitmap), i);
 
             c.Build(job);
+            return job.Result as Bitmap;
+        }
 
-            using (var result = job.Result as Bitmap)
+        [Fact]
+        public void CheckForBlurredEdges()
+        {
+            var  background = Color.FromArgb(255, 0, 5, 0);
+            var inner = Color.FromArgb(255, 240, 189, 35);
+
+            var b = CreateRingedBitmap(200, 86, background, inner, 6);
+
+            using (var result = BuildWithFastScaling(b, new Instructions() { Width = 50 }))
             {
-                Assert.Equal(background, result.GetPixel(0, 0));
-                Assert.Equal(background, result.GetPixel(job.FinalWidth.Value / 2, 0));
-                Assert.Equal(background, result.GetPixel(job.FinalWidth.Value - 1, 0));
-                Assert.Equal(background, result.GetPixel(job.FinalWidth.Value - 1, job.FinalHeight.Value / 2));
-                Assert.Equal(background, result.GetPixel(job.FinalWidth.Value - 1, job.FinalHeight.Value - 1));
-                Assert.Equal(background, result.GetPixel(job.FinalWidth.Value /2 , job.FinalHeight.Value - 1));
-                Assert.Equal(background, result.GetPixel(0, job.FinalHeight.Value - 1));
-                Assert.Equal(background, result.GetPixel(0, job.FinalHeight.Value / 2));
-                
-                
+                AssertBorderColor(background, result);
             }
         }
+
+        [Fact]
+        public void CheckCropping()
+        {
+            var croppedAway = Color.FromArgb(255, 0, 0, 0);
+            var inner = Color.FromArgb(255, 240, 189, 35);
+            var bgcolor = Color.FromArgb(255, 0, 0, 128);
+            var b = CreateRingedBitmap(200, 86, croppedAway, inner, 6);
+
+            var i = new Instructions()
+            {
+                Width = 50,
+                Margin = new BoxEdges(10),
+                CropRectangle = new double[] { 6, 6, b.Width - 12, b.Height - 12 },
+                BackgroundColor = ParseUtils.SerializeColor(bgcolor)
+            };
+            
+
+            using (var result = BuildWithFastScaling(b,i))
+            {
+                AssertBorderColor(bgcolor, result);
+                AssertBorderColor(inner,result, 10,10,result.Width -21, result.Height - 21);
+            }
+        }
+
+        [Fact]
+        public void CheckForLostBorder()
+        {
+            var background = Color.FromArgb(255, 255, 0, 0);
+            var border = Color.FromArgb(255, 0,255,0);
+            var b = CreateRingedBitmap(200, 86, border, background, 1);
+
+            var i = new Instructions()
+            {
+                Width = 50
+            };
+            using (var result = BuildWithFastScaling(b, i))
+            {
+                AssertBorderColor(border, result);
+                AssertBorderColor(background, result, 3, 3, result.Width - 7, result.Height - 7);
+            }
+        }
+
 
     }
 }
