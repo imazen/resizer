4da55b5bbd361e9387d560d9eb3e2c813f1ac4a0
tostercx <tostercx@gmail.com>
2014-12-01 20:09:53 +0200

    clear env vars before prompt

--- Tools/buildandpublish.bat
+++ Tools/buildandpublish.bat
@@ -4,6 +4,9 @@ echo - Updating FAKE...
 nuget Install FAKE -ExcludeVersion && echo - Done! || goto fail
 
 echo - Gathering settings...
+set fb_nuget_url=
+set fb_nuget_key=
+
 set /p q=Publish to a nuget server (y/n)?: 
 if /i {%q%}=={n} (goto :no) 
 set /p fb_nuget_url=Nuget URL (blank for nuget.org): 
