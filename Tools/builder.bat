2c622c71801503c460cc8d03cbf621f0bfd1ac75
tostercx <tostercx@gmail.com>
2014-12-18 23:40:37 +0200

    Builder: remove argument parsing; always use env vars

--- Tools/builder.bat
+++ Tools/builder.bat
@@ -14,24 +14,6 @@ set target=%target:rebuild=clean;build%
 set target=%target:pack_all=pack_nuget;pack_zips;print_stats%
 
 
-:loop
-shift
-set snd=%2
-set substr=%snd:~0,4%
-
-if [%0]==[] goto end
-if [%0]==[push_zips] (
-  shift & set fb_s3_id=%1
-  shift & set fb_s3_key=%2
-  shift & set fb_s3_bucket=%3)
-if [%0]==[push_nuget] (
-  if "%substr%"=="http" (shift & set fb_nuget_url=%2)
-  shift & set fb_nuget_key=%1)
-
-goto loop
-:end
-
-
 %fake% %fsx% custom "targets=%target%"
 goto exit
 
@@ -50,11 +32,11 @@ goto exit
 :help
   echo Usage:
   echo.
-  echo builder ^<command^> [args]
+  echo builder ^<command^>
   echo builder ^<command^>;[command];[command];...
   echo.
   echo A single fake call will be used for the multi-command intarface
-  echo Multi-command can't use help/update
+  echo Multi-command calls can't use help/update
   echo.
   echo Commands:
   echo.
@@ -67,10 +49,9 @@ goto exit
   echo pack_zips   - pack commands will delete old files before running
   echo pack_nuget
   echo pack_all    - pack zips and nuget
-  echo push_zips ^<s3_id^> ^<s3_key^> ^<s3_bucket^>
-  echo push_nuget ^<nuget_key^> [nuget_feed=nuget.org]
-rem  echo patch_commit [hash=cur_head]
-rem  echo patch_ver ^<filever^> ^<nugetver^> ^<infover^> [assemblyver=4.0.0.0]
+  echo push_zips   - env vars: fb_s3_bucket fb_s3_id fb_s3_key
+  echo push_nuget  - env vars: fb_nuget_url fb_nuget_key
+  echo patch_info  - env vars: fb_asmver fb_filever fb_infover fb_nugetver
   goto exit
 
 :exit
