0ed76acc07341dd85f18e45c8835dd9a2a0853da
tostercx <tostercx@gmail.com>
2015-01-15 16:35:56 +0200

    Builder: update batch cli help

--- Tools/builder.bat
+++ Tools/builder.bat
@@ -40,18 +40,22 @@ goto exit
   echo.
   echo Commands:
   echo.
-  echo update      - fetches git submodules, nuget packages, fake
-  echo help        - shows this message
+  echo update              - fetches git submodules, nuget packages, fake
+  echo help                - shows this message
   echo clean
   echo build
-  echo rebuild     - clean and build
+  echo rebuild             - clean and build
   echo test
-  echo pack_zips   - pack commands will delete old files before running
+  echo pack_zips           - pack commands will delete old files before running
   echo pack_nuget
-  echo pack_all    - pack zips and nuget
-  echo push_zips   - env vars: fb_s3_bucket fb_s3_id fb_s3_key
-  echo push_nuget  - env vars: fb_nuget_url fb_nuget_key
+  echo pack_all            - pack zips and nuget
+  echo push_zips           - env vars: fb_s3_bucket fb_s3_id fb_s3_key
+  echo push_nuget          - env vars: fb_nuget_url fb_nuget_key
   echo patch_info
+  echo release ^<semver^>    - marks for publishing as release, ver must match git tag
+  echo.
+  echo Release env vars: fb_s3_rel_bucket, fb_s3_rel_id, fb_s3_rel_key,
+  echo                   fb_nuget_rel_url, fb_nuget_rel_key
   goto exit
 
 :exit
