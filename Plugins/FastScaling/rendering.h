0900505454c7d5fedaffc01311f76ff9fb5b0025
Nathanael Jones <nathanael.jones@gmail.com>
2015-03-10 18:30:49 -0400

    Docs: Add license headers

--- Plugins/FastScaling/rendering.h
+++ Plugins/FastScaling/rendering.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Imazen LLC.
+ * No part of this project, including this file, may be copied, modified,
+ * propagated, or distributed except as permitted in COPYRIGHT.txt.
+ * Licensed under the GNU Affero General Public License, Version 3.0.
+ * Commercial licenses available at http://imageresizing.net/
+ */
 #include "Stdafx.h"
 #include "shared.h"
 
