6c2f55334ea48bd3f667473c8afa4bf6372550f2
Syver Enstad <syver.enstad@gmail.com>
2015-03-12 19:17:49 +0100

    Dummy main program (will be replaced with test framework later)

--- Plugins/FastScaling/fastscaling.cpp
+++ Plugins/FastScaling/fastscaling.cpp
@@ -1,21 +1,7 @@
-#include "Stdafx.h"
-//#include "bitmap_compositing.h"
-/* #include "bitmap_formats.h"
-#include "color_matrix.h"
-#include "color_spaces.h"
-#include "convolution.h"
-#include "fastapprox.h"
-#include "halving.h"
+//#include "Stdafx.h"
 
-#include "rendering.h"
-#include "scaling.h"
-//#include "sharpening.h"
+#include "unmanaged_renderer.h"
 
-//#include "trim_whitespace.h"
-//#include "weighting.h"
-*/
-
-#include "math_functions.h"
 
 int main(int argc, char * argv[]) 
 {
