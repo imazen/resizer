772f4ef19c3b29e707f55b218511839261b1c43c
Syver Enstad <syver.enstad@gmail.com>
2015-03-13 17:35:55 +0100

    Creating .cpp file for dll. Adding rake task to build as .so

---
+++ Plugins/FastScaling/NativeFastScaling/fastscaling.h
@@ -0,0 +1,16 @@
+#ifdef MSC
+#ifndef DLLPROJECT_API 
+#define DLLPROJECT_API __declspec(dllimport)
+#endif
+#else
+#define DLLPROJECT_API
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+DLLPROJECT_API int dummy_function(void); 
+#ifdef __cplusplus
+}
+#endif
---
+++ Plugins/FastScaling/NativeFastScaling/libfastscaling.cpp
@@ -0,0 +1,10 @@
+#ifdef MSC
+#define DLLPROJECT_API __declspec(dllexport)
+#endif
+
+#include "fastscaling.h"
+
+int dummy_function(void) 
+{
+    return 3 + 4;
+}
