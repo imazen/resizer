3720a702e6b93282ba0c52ea3903b3dac3121ba2
Syver Enstad <syver.enstad@gmail.com>
2015-03-14 04:33:22 +0100

    Porting to C. All programs work.

--- Plugins/FastScaling/NativeFastScaling/test.cpp
+++ Plugins/FastScaling/NativeFastScaling/test.cpp
@@ -1,22 +1,22 @@
 #define CATCH_CONFIG_MAIN // tell catch to generate main
 #include "catch.hpp"
 
-#include "unmanaged_renderer.h"
+#include "fastscaling.h"
 
 
 bool test(int sx, int sy, int sbpp, int cx, int cy, int cbpp, InterpolationFilter filter)
 {
-    BitmapBgraPtr source = CreateBitmapBgra(sx, sy, true, sbpp);
-    BitmapBgraPtr canvas = CreateBitmapBgra(cx, cy, true, cbpp);
+    BitmapBgra * source = CreateBitmapBgra(sx, sy, true, sbpp);
+    BitmapBgra * canvas = CreateBitmapBgra(cx, cy, true, cbpp);
 
-    RenderDetailsPtr details = CreateRenderDetails();
+    RenderDetails * details = CreateRenderDetails();
 
     details->interpolation = CreateInterpolation(filter);
 
     details->sharpen_percent_goal = 50;
 
 
-    RendererPtr p = CreateRenderer(source, canvas, details);
+    Renderer * p = CreateRenderer(source, canvas, details);
 
     PerformRender(p);
 
