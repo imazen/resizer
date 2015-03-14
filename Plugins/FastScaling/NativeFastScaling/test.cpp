83b7748e303c1231e10f4234d625fb6a32955ea4
Nathanael Jones <nathanael.jones@gmail.com>
2015-03-14 12:52:23 -0400

    FastScaling Tests: Try to test sRGB roundtrip

--- Plugins/FastScaling/NativeFastScaling/test.cpp
+++ Plugins/FastScaling/NativeFastScaling/test.cpp
@@ -2,6 +2,9 @@
 #include "catch.hpp"
 
 #include "fastscaling.h"
+#include "bitmap_formats.h"
+#include "bitmap_compositing.h"
+#include "scaling.h"
 
 
 bool test(int sx, int sy, int sbpp, int cx, int cy, int cbpp, InterpolationFilter filter)
@@ -36,3 +39,52 @@ TEST_CASE( "Render without crashing", "[fastscaling]") {
 TEST_CASE( "Render with crashing", "[fastscaling]") {
     REQUIRE( test(200, 40, 4, 4000, 3000, 4, (InterpolationFilter)0) );
 }
+
+/* 
+can't access demultiply_alpha or copy_linear_over_srgb
+
+TEST_CASE("sRGB roundtrip", "[fastscaling]"){
+
+    int w = 256;
+    int h = 256;
+
+    BitmapBgra* bit = CreateBitmapBgra(w, h, true, 4);
+
+    for (int y = 1; y < bit->h; y++){
+        for (int x = 0; x < bit->w; x++){
+            uint8_t* pix = bit->pixels + (y * bit->stride) + (x * bit->bpp);
+
+            *pix = x;
+            *(pix + 1) = x;
+            *(pix + 2) = x;
+            *(pix + 3) = y;
+        }
+    }
+
+    BitmapBgra* final = CreateBitmapBgra(w, h, true, 4);
+    BitmapFloat* buf = CreateBitmapFloat(w, h, 4, true);
+
+    convert_srgb_to_linear(bit, 0, buf, 0, h);
+
+    demultiply_alpha(buf, 0, h);
+    copy_linear_over_srgb(buf, 0, final, 0, h, 0, buf->w, false);
+
+    for (int y = 0; y < bit->h; y++){
+        for (int x = 0; x < bit->w; x++){
+            uint8_t* from = bit->pixels + (y * bit->stride) + (x * bit->bpp);
+            uint8_t* to = final->pixels + (y * final->stride) + (x * final->bpp);
+
+            Assert::Equal((*from), (*to));
+            from++; to++;
+            Assert::Equal((*from), (*to));
+            from++; to++;
+            Assert::Equal((*from), (*to));
+            from++; to++;
+            Assert::Equal((*from), (*to));
+            from++; to++;
+
+        }
+    }
+
+}
+*/
