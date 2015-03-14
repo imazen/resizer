7b7280f9e96dd6b909e40b28bbef15c22712e2b8
Nathanael Jones <nathanael.jones@gmail.com>
2015-03-14 13:50:13 -0400

    FastScaling Tests: Get sRGB roundtrip passing.

--- Plugins/FastScaling/NativeFastScaling/test.cpp
+++ Plugins/FastScaling/NativeFastScaling/test.cpp
@@ -44,47 +44,49 @@ TEST_CASE( "Render with crashing", "[fastscaling]") {
 // can't access demultiply_alpha or copy_linear_over_srgb
 
 SCENARIO("sRGB roundtrip", "[fastscaling]") {
-    GIVEN("Bla bla") {
-	int w = 256;
-	int h = 256;
+    GIVEN("A 256x256 image, grayscale gradient along the x axis, alpha along the y") {
+    int w = 256;
+    int h = 256;
 
-	BitmapBgra* bit = CreateBitmapBgra(w, h, true, 4);
+    BitmapBgra* bit = CreateBitmapBgra(w, h, true, 4);
 
-	for (size_t y = 1; y < bit->h; y++){
-	    for (size_t x = 0; x < bit->w; x++){
-		uint8_t* pix = bit->pixels + (y * bit->stride) + (x * bit->bpp);
+    for (size_t y = 1; y < bit->h; y++){
+      for (size_t x = 0; x < bit->w; x++){
+        uint8_t* pix = bit->pixels + (y * bit->stride) + (x * bit->bpp);
 
-		*pix = x;
-		*(pix + 1) = x;
-		*(pix + 2) = x;
-		*(pix + 3) = y;
-	    }
-	}
-
-	BitmapBgra* final = CreateBitmapBgra(w, h, true, 4);
-	BitmapFloat* buf = CreateBitmapFloat(w, h, 4, true);
-
-	WHEN ("we do stuff") {
-	    convert_srgb_to_linear(bit, 0, buf, 0, h);	    
-	    demultiply_alpha(buf, 0, h);
-	    copy_linear_over_srgb(buf, 0, final, 0, h, 0, buf->w, false);
-	}
-	THEN(" and so forth ") {
-	    for (size_t y = 0; y < bit->h; y++){
-		for (size_t x = 0; x < bit->w; x++){
-		    uint8_t* from = bit->pixels + (y * bit->stride) + (x * bit->bpp);
-		    uint8_t* to = final->pixels + (y * final->stride) + (x * final->bpp);
-
-		    // REQUIRE(*from == *to);
-		    // from++; to++;
-		    // REQUIRE(*from == *to);
-		    // from++; to++;
-		    // REQUIRE(*from == *to);
-		    // from++; to++;
-		    // REQUIRE(*from == *to);
-		    // from++; to++;
-		}
-	    }
-	}
+        *pix = x;
+        *(pix + 1) = x;
+        *(pix + 2) = x;
+        *(pix + 3) = y;
+      }
     }
+
+    BitmapBgra* final = CreateBitmapBgra(w, h, true, 4);
+    BitmapFloat* buf = CreateBitmapFloat(w, h, 4, true);
+
+    WHEN ("we do stuff") {
+      convert_srgb_to_linear(bit, 0, buf, 0, h);      
+      demultiply_alpha(buf, 0, h);
+      copy_linear_over_srgb(buf, 0, final, 0, h, 0, buf->w, false);
+    
+      THEN(" and so forth ") {
+
+        for (size_t y = 0; y < bit->h; y++){
+          for (size_t x = 0; x < bit->w; x++){
+              uint8_t* from = bit->pixels + (y * bit->stride) + (x * bit->bpp);
+              uint8_t* to = final->pixels + (y * final->stride) + (x * final->bpp);
+
+              REQUIRE(*from == *to);
+              from++; to++;
+              REQUIRE(*from == *to);
+              from++; to++;
+              REQUIRE(*from == *to);
+              from++; to++;
+              REQUIRE(*from == *to);
+              from++; to++;
+          }
+        }
+      }
+    }
+  }
 }
