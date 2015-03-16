616606ef1c5d9a0bcf8dca07d87d48bb27cffc5f
Nathanael Jones <nathanael.jones@gmail.com>
2015-03-16 07:54:47 -0400

    FastScaling: Try to keep gcc from crashing when it doesn't understand all the flags

--- Plugins/FastScaling/NativeFastScaling/c_build_rake.rb
+++ Plugins/FastScaling/NativeFastScaling/c_build_rake.rb
@@ -19,7 +19,7 @@ CC = MACOS && cc == "gcc" ? "gcc-4.9" : cc;
 
 VALGRIND_OPTS =  MACOS ? "--dsymutil=yes" : ""
 
-COMMON_FLAGS=" -fPIC -O2 -g -Wno-unknown-warning-option -Wpointer-arith -Wcast-qual -Wpedantic -Wall -Wextra -Wuninitialized -Wno-unused-function -Wno-return-type -Wredundant-decls -Wfloat-conversion  -Werror"
+COMMON_FLAGS=" -fPIC -O2 -g -Wno-error=unknown-warning -Wno-unknown-warning-option -Wpointer-arith -Wcast-qual -Wpedantic -Wall -Wextra -Wuninitialized -Wno-unused-function -Wno-return-type -Wredundant-decls -Wfloat-conversion  -Werror"
 CFLAGS="#{COMMON_FLAGS} -std=c11 -Wstrict-prototypes -Wmissing-prototypes -Wc++-compat -Wshadow"
 
 CXXFLAGS="#{COMMON_FLAGS} -std=gnu++11"
