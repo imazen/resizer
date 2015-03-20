09f57385f70706d13d170cc15806c8283cd45b7d
Syver Enstad <syver.enstad@gmail.com>
2015-03-20 01:13:48 +0100

    A very simple but working model based test using the theft library. Be aware that theft must be installed on the machine for the test to work.
    You install by going to the theft directory and run make, the sudo make install

--- Plugins/FastScaling/NativeFastScaling/tests/test_rendering.cpp
+++ Plugins/FastScaling/NativeFastScaling/tests/test_rendering.cpp
@@ -21,14 +21,33 @@ struct TestEnv {
     int max_dimensions;
 };
 
-static theft_trial_res before_and_after_should_match() {
-    return THEFT_TRIAL_ERROR;
+static theft_trial_res before_and_after_should_match(float * bgra_array) {
+    float copy[4];
+    memcpy(copy, bgra_array, sizeof copy);
+    linear_to_luv(copy);
+    luv_to_linear(copy);
+    for (int i = 0; i < 4; i++) {
+        if (bgra_array[i] != Approx(copy[i])) {
+            return THEFT_TRIAL_FAIL;
+        }
+    }
+    return THEFT_TRIAL_PASS;
 }
 
     
 void * allocate_random_bitmap(theft * theft, theft_seed seed, void * input) {
-    return NULL;
+    float * bitmap = (float*)calloc(4, sizeof(float));
+    for (int i = 0; i < 4; i++) 
+        bitmap[i] = (float)theft_random_double(theft);
+    return (void*)bitmap;
 }
+
+void free_bitmap(void * bitmap, void * unused) 
+{
+    free(bitmap);
+}
+
+
 
 TEST_CASE("Roundtrip RGB<->LUV property", "[fastscaling][thief]") {
     theft_seed seed;
@@ -36,18 +55,19 @@ TEST_CASE("Roundtrip RGB<->LUV property", "[fastscaling][thief]") {
     Context context;
     Context_initialize(&context);
     theft * t = theft_init(0);
+
     theft_type_info bitmap_type_info;
+    memset(&bitmap_type_info, 0, sizeof bitmap_type_info);
     bitmap_type_info.alloc = allocate_random_bitmap;
-    bitmap_type_info.free = NULL;
-    bitmap_type_info.hash = NULL;
-    bitmap_type_info.shrink = NULL;
-    bitmap_type_info.print = NULL;
+    bitmap_type_info.free = free_bitmap;
+
     theft_trial_report report;
+
     theft_cfg cfg;
     memset(&cfg, 0, sizeof cfg);
     cfg.seed = seed;
     cfg.name = __func__;
-    cfg.fun = before_and_after_should_match;
+    cfg.fun = (theft_propfun*)before_and_after_should_match;
     memset(&(cfg.type_info), 0, sizeof cfg.type_info);
     cfg.type_info[0] = &bitmap_type_info;
     cfg.trials = 10000;
