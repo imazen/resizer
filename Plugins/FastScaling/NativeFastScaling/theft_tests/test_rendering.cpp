c223b71f747af1d8f77ff7f1c000f5f0d8a74549
Syver Enstad <syver.enstad@gmail.com>
2015-03-23 20:28:55 +0100

    Fix the test to remove segfaults and uninitialized memory failures and memory leak

--- Plugins/FastScaling/NativeFastScaling/theft_tests/test_rendering.cpp
+++ Plugins/FastScaling/NativeFastScaling/theft_tests/test_rendering.cpp
@@ -23,24 +23,9 @@ struct TestEnv {
     int max_dimensions;
 };
 
-static theft_trial_res before_and_after_should_match(float * bgra_array) {
-    float copy[4];
-    memcpy(copy, bgra_array, sizeof copy);
-    linear_to_luv(copy);
-    luv_to_linear(copy);
-    for (int i = 0; i < 4; i++) {
-        //printf("bgra_array is %f\n", bgra_array[i]);
-        //printf("copy       is %f\n\n", copy[i]);
-        if (bgra_array[i] != Approx(copy[i])) {
-            return THEFT_TRIAL_FAIL;
-        }
-    }
-    return THEFT_TRIAL_PASS;
-}
-
 static theft_trial_res render_should_succeed(RenderDetails * details, BitmapBgra * source, BitmapBgra * canvas) {
 
-    bool result = RenderDetails_render(c,details,source,canvas);
+    bool result = RenderDetails_render(c, details, source, canvas);
     if (!result) return THEFT_TRIAL_FAIL;
 
     return THEFT_TRIAL_PASS;
@@ -66,11 +51,11 @@ void renderdetails_free(void * details, void * unused)
     RenderDetails_destroy(c, (RenderDetails *)details);
 }
 
-void * BitmapBgra_random(theft * theft, theft_seed seed, void * input) {
+void * BitmapBgra_random_dest(theft * theft, theft_seed seed, void * input) {
     BitmapPixelFormat fmt = (BitmapPixelFormat)(3 + theft_random(theft) % 2);
     int w = theft_random(theft) % 2049;
     int h = theft_random(theft) % 2049;
-    BitmapBgra * b = BitmapBgra_create(c, w,h,false,fmt);
+    BitmapBgra * b = BitmapBgra_create(c, w,h, false, fmt);
     b -> alpha_meaningful = !!(theft_random(theft) % 2);
     b -> can_reuse_space = !!(theft_random(theft) % 2);
     b -> stride_readonly = !!(theft_random(theft) % 2);
@@ -82,6 +67,31 @@ void * BitmapBgra_random(theft * theft, theft_seed seed, void * input) {
     b->compositing_mode = (BitmapCompositingMode)(theft_random(theft) % 3);
     return (void*)b;
 }
+
+void * BitmapBgra_random_zeroed_source(theft * theft, theft_seed seed, void * input) {
+    BitmapPixelFormat fmt = (BitmapPixelFormat)(3 + theft_random(theft) % 2);
+    int w = 1 + (theft_random(theft) % 2048);
+    int h = 1 + (theft_random(theft) % 2048);
+    
+    BitmapBgra * b = BitmapBgra_create(c, w, h, true, fmt);
+    if (!b) {
+        char buffer[1024];
+        printf("BitmapBgra_create failed: %s\n", Context_error_message(c, buffer, sizeof buffer));
+        printf("dimensions: %dx%d format: %u\n", w, h, (unsigned)fmt);
+        return THEFT_ERROR; // or THEFT_SKIP maybe
+    }
+    b -> alpha_meaningful = !!(theft_random(theft) % 2);
+    b -> can_reuse_space = !!(theft_random(theft) % 2);
+    b -> stride_readonly = !!(theft_random(theft) % 2);
+    b->pixels_readonly = false;
+    b->matte_color[0] = theft_random(theft) % 256;
+    b->matte_color[1] = theft_random(theft) % 256;
+    b->matte_color[2] = theft_random(theft) % 256;
+    b->matte_color[3] = theft_random(theft) % 256;
+    b->compositing_mode = (BitmapCompositingMode)(theft_random(theft) % 3);
+    return (void*)b;
+}
+
 void bitmapbgra_free(void * details, void * unused)
 {
     BitmapBgra_destroy(c, (BitmapBgra *)details);
@@ -124,17 +134,6 @@ InterpolationDetails * interpolation;
 
 
 
-void * allocate_random_bitmap(theft * theft, theft_seed seed, void * input) {
-    float * bitmap = (float*)calloc(4, sizeof(float));
-    for (int i = 0; i < 4; i++)
-        bitmap[i] = (float)theft_random_double(theft);
-    return (void*)bitmap;
-}
-
-void free_bitmap(void * bitmap, void * unused)
-{
-    free(bitmap);
-}
 
 void reboot_context(){ if (c != NULL){ Context_destroy(c); } c = Context_create();}
 void destroy_context(){ if (c != NULL){ Context_destroy(c); c = NULL;}}
@@ -149,15 +148,20 @@ TEST_CASE("TestRender", "[fastscaling][thief]") {
     if (!get_time_seed(&seed)) REQUIRE(false);
 
 
-    theft_type_info bitmap_type_info;
-    memset(&bitmap_type_info, 0, sizeof bitmap_type_info);
-    bitmap_type_info.alloc = renderdetails_random;
-    bitmap_type_info.free = renderdetails_free;
+    theft_type_info renderdetails_type_info;
+    memset(&renderdetails_type_info, 0, sizeof renderdetails_type_info);
+    renderdetails_type_info.alloc = renderdetails_random;
+    renderdetails_type_info.free = renderdetails_free;
 
-    theft_type_info bitmap_type_info_2;
-    memset(&bitmap_type_info_2, 0, sizeof bitmap_type_info_2);
-    bitmap_type_info_2.alloc = BitmapBgra_random;
-    bitmap_type_info_2.free = bitmapbgra_free;
+    theft_type_info src_type_info;
+    memset(&src_type_info, 0, sizeof src_type_info);
+    src_type_info.alloc = BitmapBgra_random_zeroed_source;
+    src_type_info.free = bitmapbgra_free;
+
+    theft_type_info dest_type_info;
+    memset(&dest_type_info, 0, sizeof dest_type_info);
+    dest_type_info.alloc = BitmapBgra_random_zeroed_source;
+    dest_type_info.free = bitmapbgra_free;
 
     theft_trial_report report;
 
@@ -166,10 +170,10 @@ TEST_CASE("TestRender", "[fastscaling][thief]") {
     cfg.seed = seed;
     cfg.name = __func__;
     cfg.fun = (theft_propfun*)render_should_succeed;
-    cfg.type_info[0] = &bitmap_type_info;
-    cfg.type_info[1] = &bitmap_type_info_2;
-    cfg.type_info[2] = &bitmap_type_info_2;
-    cfg.trials = 1000;
+    cfg.type_info[0] = &renderdetails_type_info;
+    cfg.type_info[1] = &src_type_info;
+    cfg.type_info[2] = &dest_type_info;
+    cfg.trials = 100;
     cfg.report = &report;
 
     theft * t = theft_init(0);
@@ -177,9 +181,36 @@ TEST_CASE("TestRender", "[fastscaling][thief]") {
     theft_free(t);
     printf("\n");
     REQUIRE(THEFT_RUN_PASS == res);
+    free_lookup_tables();
     destroy_context();
 }
 
+static theft_trial_res before_and_after_should_match(float * bgra_array) {
+    float copy[4];
+    memcpy(copy, bgra_array, sizeof copy);
+    linear_to_luv(copy);
+    luv_to_linear(copy);
+    for (int i = 0; i < 4; i++) {
+        //printf("bgra_array is %f\n", bgra_array[i]);
+        //printf("copy       is %f\n\n", copy[i]);
+        if (bgra_array[i] != Approx(copy[i])) {
+            return THEFT_TRIAL_FAIL;
+        }
+    }
+    return THEFT_TRIAL_PASS;
+}
+
+void * allocate_random_bitmap(theft * theft, theft_seed seed, void * input) {
+    float * bitmap = (float*)calloc(4, sizeof(float));
+    for (int i = 0; i < 4; i++)
+        bitmap[i] = (float)theft_random_double(theft);
+    return (void*)bitmap;
+}
+
+void free_bitmap(void * bitmap, void * unused)
+{
+    free(bitmap);
+}
 
 
 TEST_CASE("Roundtrip RGB<->LUV property", "[fastscaling][thief]") {
