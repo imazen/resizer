77a1e9f538a57d4b654713570324925627f486c9
Nathanael Jones <nathanael.jones@gmail.com>
2015-03-24 16:32:40 -0400

    FastScaling: Fix windows .vcxproj files to reflect new structure. Make snprintf patch compile.

--- Plugins/FastScaling/NativeFastScaling/lib/fastscaling_private.h
+++ Plugins/FastScaling/NativeFastScaling/lib/fastscaling_private.h
@@ -277,19 +277,12 @@ static inline int64_t get_profiler_ticks_per_second(void)
 
 #ifdef _MSC_VER
 
+#include <stdio.h>
+#include <stdarg.h>
+
 #define snprintf c99_snprintf
 
-inline int c99_snprintf(char* str, size_t size, const char* format, ...)
-{
-    int count;
-    va_list ap;
 
-    va_start(ap, format);
-    count = c99_vsnprintf(str, size, format, ap);
-    va_end(ap);
-
-    return count;
-}
 
 inline int c99_vsnprintf(char* str, size_t size, const char* format, va_list ap)
 {
@@ -299,6 +292,18 @@ inline int c99_vsnprintf(char* str, size_t size, const char* format, va_list ap)
         count = _vsnprintf_s(str, size, _TRUNCATE, format, ap);
     if (count == -1)
         count = _vscprintf(format, ap);
+
+    return count;
+}
+
+inline int c99_snprintf (char* str, size_t size, const char* format, ...)
+{
+    int count;
+    va_list ap;
+
+    va_start (ap, format);
+    count = c99_vsnprintf (str, size, format, ap);
+    va_end (ap);
 
     return count;
 }
