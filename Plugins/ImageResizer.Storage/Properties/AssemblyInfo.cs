4b7f7a76eb3d228e46657f34e06920810f9614af
Nathanael Jones <nathanael.jones@gmail.com>
2015-03-28 06:22:47 -0400

    ImageResizer.Storage: Align assembly version numbers

--- Plugins/ImageResizer.Storage/Properties/AssemblyInfo.cs
+++ Plugins/ImageResizer.Storage/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 // Copyright (c) Imazen LLC.
+// Copyright (c) Imazen LLC.
 // No part of this project, including this file, may be copied, modified,
 // propagated, or distributed except as permitted in COPYRIGHT.txt.
 // Licensed under the GNU Affero General Public License, Version 3.0.
@@ -6,36 +7,16 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using ImageResizer.Util;
+
+// So the ImageResizer knows which bundle this assembly belongs to
+
+[assembly: Edition("RPerformance")]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ImageResizer.Storage")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImageResizer.Storage")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f32ee366-d3d7-4015-abb0-f4bb675b6242")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
