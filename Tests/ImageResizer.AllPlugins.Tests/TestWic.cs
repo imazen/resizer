8cd5fad6fca9dfbdb5e38472f4490d38a660ff46
Nathanael Jones <nathanael.jones@gmail.com>
2015-03-31 14:05:17 -0400

    Tests: Add a skipped (crashing) test of the WIC pipeline

---
+++ Tests/ImageResizer.AllPlugins.Tests/TestWic.cs
@@ -0,0 +1,25 @@
+﻿using ImageResizer.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ImageResizer.AllPlugins.Tests
+{
+    public class TestWic
+    {
+        [Fact(Skip="Windows WIC is broken")]
+        public void TestWicBasic()
+        {
+            var c = new Config();
+            new ImageResizer.Plugins.WicBuilder.WicBuilderPlugin().Install(c);
+
+            string imgdir = "..\\..\\..\\Samples\\Images\\";
+
+            c.CurrentImageBuilder.Build(imgdir + "red-leaf.jpg", "red-leaf-wic.jpg", new ResizeSettings("&builder=wic&width=200"));
+
+        }
+    }
+}
