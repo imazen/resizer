d243f8a410ebb7210d4d32286bb30001f5df6615
Nathanael Jones <nathanael.jones@gmail.com>
2015-04-08 15:36:08 -0400

    NativeFastScaling: Halving  now switches between integer and colorspace-adjusted float modes based on the context.

--- Plugins/FastScaling/NativeFastScaling/lib/scaling.c
+++ Plugins/FastScaling/NativeFastScaling/lib/scaling.c
@@ -129,8 +129,11 @@ This halves in sRGB space instead of linear. Not significantly faster on modern 
 #define  HALVING_TYPE float
 #define TO_HALVING_TYPE(x)  Context_srgb_to_floatspace (context, x)
 #define FROM_HALVING_TYPE(x)  Context_floatspace_to_srgb (context, x)
+#define HALVE_ROW_NAME HalveRowByDivisorColorSpaceAware
+#define HALVE_INTERNAL_NAME HalveInternalColorSpaceAware
 
-static inline void HalveRowByDivisor (Context * context, const unsigned char* from, HALVING_TYPE * to, const unsigned int to_count, const int divisor, const int step)
+
+static inline void HALVE_ROW_NAME (Context * context, const unsigned char* from, HALVING_TYPE * to, const unsigned int to_count, const int divisor, const int step)
 {
     int to_b, from_b;
     const int to_bytes = to_count * step;
@@ -194,7 +197,7 @@ static inline void HalveRowByDivisor (Context * context, const unsigned char* fr
 }
 
 
-static bool HalveInternal(
+static bool HALVE_INTERNAL_NAME (
     Context * context,
     const BitmapBgra * from,
     BitmapBgra * to,
@@ -230,7 +233,7 @@ static bool HalveInternal(
     for (y = 0; y < to_h; y++) {
         memset(buffer, 0, sizeof(HALVING_TYPE) * to_w_bytes);
         for (d = 0; d < divisor; d++) {
-            HalveRowByDivisor (context, from->pixels + (y * divisor + d) * from->stride, buffer, to_w, divisor, bytes_pp);
+            HALVE_ROW_NAME (context, from->pixels + (y * divisor + d) * from->stride, buffer, to_w, divisor, bytes_pp);
         }
         unsigned char * dest_line = to->pixels + y * to_stride;
 #ifdef ALLOW_SHIFTING_HALVING_TYPE
@@ -263,9 +266,174 @@ static bool HalveInternal(
     return true;
 }
 
+#undef HALVE_ROW_NAME
+#undef HALVE_INTERNAL_NAME
+#undef HALVING_TYPE
+#undef TO_HALVING_TYPE
+#undef FROM_HALVING_TYPE
+
+#define HALVE_ROW_NAME HalveRowByDivisor
+#define HALVE_INTERNAL_NAME HalveInternal
+
+
+#define  HALVING_TYPE unsigned short
+#define TO_HALVING_TYPE(x) x
+#define FROM_HALVING_TYPE(x) x
+#define ALLOW_SHIFTING_HALVING_TYPE
+
+//** Do not edit the following two functions; they are copy/pasted from above. **//
+
+static inline void HALVE_ROW_NAME (Context * context, const unsigned char* from, HALVING_TYPE * to, const unsigned int to_count, const int divisor, const int step)
+{
+    int to_b, from_b;
+    const int to_bytes = to_count * step;
+    const int divisor_stride = step * divisor;
+    if (divisor > 4) {
+        if (step == 3){
+            for (to_b = 0, from_b = 0; to_b < to_bytes; to_b += 3, from_b += divisor_stride) {
+                for (int f = 0; f < divisor_stride; f += 3) {
+                    to[to_b + 0] += TO_HALVING_TYPE (from[from_b + f + 0]);
+                    to[to_b + 1] += TO_HALVING_TYPE (from[from_b + f + 1]);
+                    to[to_b + 2] += TO_HALVING_TYPE (from[from_b + f + 2]);
+                }
+
+            }
+        }
+        else if (step == 4){
+            for (to_b = 0, from_b = 0; to_b < to_bytes; to_b += 4, from_b += divisor_stride) {
+                for (int f = 0; f < divisor_stride; f += 4) {
+                    to[to_b + 0] += TO_HALVING_TYPE (from[from_b + f + 0]);
+                    to[to_b + 1] += TO_HALVING_TYPE (from[from_b + f + 1]);
+                    to[to_b + 2] += TO_HALVING_TYPE (from[from_b + f + 2]);
+                    to[to_b + 3] += TO_HALVING_TYPE (from[from_b + f + 3]);
+                }
+            }
+        }
+        return;
+    }
+
+    if (divisor == 2) {
+        if (to_count % 2 == 0) {
+            for (to_b = 0, from_b = 0; to_b < to_bytes; to_b += 2 * step, from_b += 4 * step) {
+                for (int i = 0; i < 2 * step; i++) {
+                    to[to_b + i] += TO_HALVING_TYPE (from[from_b + i]) + TO_HALVING_TYPE (from[from_b + i + step]);
+                }
+            }
+        }
+        else {
+            for (to_b = 0, from_b = 0; to_b < to_bytes; to_b += step, from_b += 2 * step) {
+                for (int i = 0; i < step; i++) {
+                    to[to_b + i] += TO_HALVING_TYPE (from[from_b + i]) + TO_HALVING_TYPE (from[from_b + i + step]);
+                }
+            }
+        }
+        return;
+    }
+    if (divisor == 3) {
+        for (to_b = 0, from_b = 0; to_b < to_bytes; to_b += step, from_b += 3 * step) {
+            for (int i = 0; i < step; i++) {
+                to[to_b + i] += TO_HALVING_TYPE (from[from_b + i]) + TO_HALVING_TYPE (from[from_b + i + step]) + TO_HALVING_TYPE (from[from_b + i + 2 * step]);
+            }
+        }
+        return;
+    }
+    if (divisor == 4) {
+        for (to_b = 0, from_b = 0; to_b < to_bytes; to_b += step, from_b += 4 * step) {
+            for (int i = 0; i < step; i++) {
+                to[to_b + i] += TO_HALVING_TYPE (from[from_b + i]) + TO_HALVING_TYPE (from[from_b + i + step]) + TO_HALVING_TYPE (from[from_b + i + 2 * step]) + TO_HALVING_TYPE (from[from_b + i + 3 * step]);
+            }
+        }
+        return;
+    }
+}
+
+
+static bool HALVE_INTERNAL_NAME (
+    Context * context,
+    const BitmapBgra * from,
+    BitmapBgra * to,
+    const int to_w,
+    const int to_h,
+    const int to_stride,
+    const int divisor)
+{
+
+    const int to_w_bytes = to_w * BitmapPixelFormat_bytes_per_pixel (to->fmt);
+    HALVING_TYPE *buffer = (HALVING_TYPE *)CONTEXT_calloc (context, to_w_bytes, sizeof (HALVING_TYPE));
+    if (buffer == NULL) {
+        CONTEXT_error (context, Out_of_memory);
+        return false;
+    }
+    //Force the from and to formate to be the same
+    if (from->fmt != to->fmt || (BitmapPixelFormat_bytes_per_pixel (from->fmt) != 3 && BitmapPixelFormat_bytes_per_pixel (from->fmt) != 4)){
+        CONTEXT_error (context, Invalid_internal_state);
+        return false;
+    }
+
+
+    int y, b, d;
+    const unsigned short divisorSqr = divisor * divisor;
+#ifdef ALLOW_SHIFTING_HALVING_TYPE
+    const unsigned int shift = isPowerOfTwo (divisorSqr) ? intlog2 (divisorSqr) : 0;
+#endif
+
+    const uint32_t bytes_pp = BitmapPixelFormat_bytes_per_pixel (from->fmt);
+
+    //TODO: Ensure that from is equal or greater than divisorx to_w and t_h
+    //Ensure that shift > 0 && divisorSqr > 0 && divisor > 0
+    for (y = 0; y < to_h; y++) {
+        memset (buffer, 0, sizeof (HALVING_TYPE) * to_w_bytes);
+        for (d = 0; d < divisor; d++) {
+            HALVE_ROW_NAME (context, from->pixels + (y * divisor + d) * from->stride, buffer, to_w, divisor, bytes_pp);
+        }
+        unsigned char * dest_line = to->pixels + y * to_stride;
+#ifdef ALLOW_SHIFTING_HALVING_TYPE
+        if (shift == 2) {
+            for (b = 0; b < to_w_bytes; b++) {
+                dest_line[b] = FROM_HALVING_TYPE (buffer[b] >> 2);
+            }
+        }
+        else if (shift == 3) {
+            for (b = 0; b < to_w_bytes; b++) {
+                dest_line[b] = FROM_HALVING_TYPE (buffer[b] >> 3);
+            }
+        }
+        else if (shift > 0) {
+            for (b = 0; b < to_w_bytes; b++) {
+                dest_line[b] = FROM_HALVING_TYPE (buffer[b] >> shift);
+            }
+        }
+        if (shift == 0){
+#endif
+
+            for (b = 0; b < to_w_bytes; b++) {
+                dest_line[b] = FROM_HALVING_TYPE (buffer[b] / divisorSqr);
+            }
+#ifdef ALLOW_SHIFTING_HALVING_TYPE
+        }
+#endif
+    }
+
+    CONTEXT_free (context, buffer);
+
+    return true;
+}
+
+
+
+//** Do not edit the above two functions; they are copy/pasted. **//
+
+
 bool Halve(Context * context, const BitmapBgra * from, BitmapBgra * to, int divisor)
 {
-    bool r = HalveInternal(context, from, to, to->w, to->h, to->stride, divisor);
+
+    bool r = false;
+    if (context->colorspace.floatspace == Floatspace_as_is){
+        r = HalveInternal (context, from, to, to->w, to->h, to->stride, divisor);
+    }
+    else{
+        r = HalveInternalColorSpaceAware (context, from, to, to->w, to->h, to->stride, divisor);
+    }
     if (!r){
         CONTEXT_add_to_callstack (context);
     }
@@ -277,7 +445,13 @@ bool HalveInPlace(Context * context, BitmapBgra * from, int divisor)
     int to_w = from->w / divisor;
     int to_h = from->h / divisor;
     int to_stride = to_w * BitmapPixelFormat_bytes_per_pixel (from->fmt);
-    bool r = HalveInternal(context, from, from, to_w, to_h, to_stride, divisor);
+    bool r = false;
+    if (context->colorspace.floatspace == Floatspace_as_is){
+        r = HalveInternal (context, from, from, to_w, to_h, to_stride, divisor);
+    }
+    else{
+       r =  HalveInternalColorSpaceAware (context, from, from, to_w, to_h, to_stride, divisor);
+    }
     if (!r){
         CONTEXT_add_to_callstack (context);
     }
