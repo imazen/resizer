6ba85fdccffa36f9b0e6782c19f5dab900801b8d
Nathanael Jones <nathanael.jones@gmail.com>
2015-10-15 13:44:11 -0400

    Add StaticLicenseProvider
    Add StaticLicenseProvider so users can register licenses without a
    Web.config file.

---
+++ Core/Plugins/StaticLicenseProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageResizer.Configuration;
+
+namespace ImageResizer.Plugins.Basic
+{
+    public class StaticLicenseProvider : ILicenseProvider, IPlugin
+    {
+        public StaticLicenseProvider() { }
+        public StaticLicenseProvider(params string[] licenses)
+        {
+            foreach(string s in licenses)
+                AddLicense(s);
+        }
+
+        private List<String> licenses = new List<string>();
+        
+
+        public void AddLicense(string license)
+        {
+           licenses.Add(license.Trim().Replace(" ", "").Replace("\t", "").Replace("\n", "").Replace("\r", ""));
+        }
+
+        public ICollection<string> GetLicenses()
+        {
+            return licenses.AsReadOnly();
+        }
+
+        public IPlugin Install(Config c)
+        {
+            c.Plugins.add_plugin(this);
+            return this;
+        }
+
+        public bool Uninstall(Config c)
+        {
+            c.Plugins.remove_plugin(this);
+            return true;
+        }
+    }
+}
