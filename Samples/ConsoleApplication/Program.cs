275b1d93be65d1b86ba9285c0ca8e35d1107c281
Lilith River <lilith@imazen.io>
2022-08-04 13:12:36 -0600

    Remove PrettyGifs from ConsoleApplication

--- Samples/ConsoleApplication/Program.cs
+++ Samples/ConsoleApplication/Program.cs
@@ -3,25 +3,17 @@
 // propagated, or distributed except as permitted in COPYRIGHT.txt.
 // Licensed under the Apache License, Version 2.0.
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using ImageResizer.Configuration;
-using ImageResizer.Plugins.PrettyGifs;
 using ImageResizer;
 using System.Diagnostics;
-using ImageResizer.Util;
-using System.IO;
-using System.Drawing;
-using ImageResizer.Plugins.Basic;
-using ImageResizer.Plugins.Watermark;
 
 namespace ConsoleApplication {
     class Program {
 
         public static string imageDir = "..\\..\\Samples\\Images\\";
         static void Main(string[] args) {
-            Config c = new Config(new ResizerSection("<resizer><plugins><add name=\"PrettyGifs\"/></plugins></resizer>"));
+            Config c = new Config(); //new Config(new ResizerSection("<resizer><plugins><add name=\"PrettyGifs\"/></plugins></resizer>"));
             c.Plugins.LoadPlugins();
             string s = c.GetDiagnosticsPage();
             Debug.Assert(c.Plugins.AllPlugins.Any((p) => p.ToString().EndsWith("PrettyGifs")));
