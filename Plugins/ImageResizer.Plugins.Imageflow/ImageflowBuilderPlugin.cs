7947c5c3d518fb8e63d870fab179e77951f13d14
Lilith River <lilith@imazen.io>
2022-08-04 17:01:39 -0600

    Add ImageResizer.Plugins.Imageflow stub plugin

---
+++ Plugins/ImageResizer.Plugins.Imageflow/ImageflowBuilderPlugin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Specialized;
+using ImageResizer.Configuration;
+using ImageResizer.Configuration.Issues;
+using ImageResizer.ExtensionMethods;
+using ImageResizer.Resizing;
+
+namespace ImageResizer.Plugins.Imageflow
+{
+    public class ImageflowBuilderPlugin : BuilderExtension, IPlugin, IIssueProvider, IFileExtensionPlugin
+    {
+        private bool something = false;
+        public ImageflowBuilderPlugin()
+        {
+           
+        }
+        public ImageflowBuilderPlugin(NameValueCollection args) {
+            something =  args.Get<bool>("someSetting", false);
+        }
+        
+        Config c;
+        public IPlugin Install(Configuration.Config c) {
+            c.Plugins.add_plugin(this);
+            this.c = c;
+            return this;
+        }
+
+        public bool Uninstall(Configuration.Config c) {
+            c.Plugins.remove_plugin(this);
+            return true;
+        }
+
+
+        
+    }
+}
