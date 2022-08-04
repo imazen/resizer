ef939bd9d7d47fb4b4e5a71be68522810b537a4a
Lilith River <lilith@imazen.io>
2022-08-04 16:39:27 -0600

    More stub work on HybridCache

---
+++ Plugins/ImageResizer.Plugins.HybridCache/HybridCacheOptions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ImageResizer.Plugins.HybridCache
+{
+    public class HybridCacheOptions
+    {
+        /// <summary>
+        /// Where to store the cached files and the database
+        /// </summary>
+        public string DiskCacheDirectory { get; set; }
+
+        /// <summary>
+        /// How many RAM bytes to use when writing asynchronously to disk before we switch to writing synchronously.
+        /// Defaults to 100MiB. 
+        /// </summary>
+        public long QueueSizeLimitInBytes { get; set; } = 100 * 1024 * 1024;
+
+        /// <summary>
+        /// Defaults to 1 GiB. Don't set below 9MB or no files will be cached, since 9MB is reserved just for empty directory entries.
+        /// </summary>
+        public long CacheSizeLimitInBytes { get; set; } = 1 * 1024 * 1024 * 1024;
+        
+        /// <summary>
+        /// The minimum number of bytes to free when running a cleanup task. Defaults to 1MiB;
+        /// </summary>
+        public long MinCleanupBytes { get; set; } = 1 * 1024 * 1024;
+        
+        /// <summary>
+        /// The minimum age of files to delete. Defaults to 10 seconds.
+        /// </summary>
+        public TimeSpan MinAgeToDelete { get; set; } = TimeSpan.FromSeconds(10);
+
+        /// <summary>
+        /// The number of shards to split the metabase into. More shards means more open log files, slower shutdown.
+        /// But more shards also mean less lock contention and faster start time for individual cached requests.
+        /// Defaults to 8. You have to delete the database directory each time you change this number.
+        /// </summary>
+        public int DatabaseShards { get; set; } = 8;
+
+        public HybridCacheOptions(string cacheDir)
+        {
+            DiskCacheDirectory = cacheDir;
+        }
+    }
+}
--- Plugins/ImageResizer.Plugins.HybridCache/HybridCachePlugin.cs
+++ Plugins/ImageResizer.Plugins.HybridCache/HybridCachePlugin.cs
@@ -1,10 +1,17 @@
 ﻿using ImageResizer.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
+using Imazen.Common.Extensibility.StreamCache;
+using Imazen.Common.Issues;
+using Imazen.HybridCache;
+using Imazen.HybridCache.MetaStore;
+using Microsoft.Extensions.Logging;
 
 namespace ImageResizer.Plugins.HybridCache
 {
@@ -28,6 +35,60 @@ namespace ImageResizer.Plugins.HybridCache
         public bool Uninstall(Config c)
         {
             throw new NotImplementedException();
+        }
+        
+        
+        
+        private readonly Imazen.HybridCache.HybridCache cache;
+        public HybridCachePlugin(HybridCacheOptions options, ILogger logger)
+        {
+
+            var cacheOptions = new Imazen.HybridCache.HybridCacheOptions(options.DiskCacheDirectory)
+            {
+                AsyncCacheOptions = new AsyncCacheOptions()
+                {
+                    MaxQueuedBytes = Math.Max(0,options.QueueSizeLimitInBytes),
+                    WriteSynchronouslyWhenQueueFull = true,
+                    MoveFileOverwriteFunc = (from, to) =>
+                    {
+                        File.Copy(from, to, true);
+                        File.Delete(from);
+                    }
+                },
+                CleanupManagerOptions = new CleanupManagerOptions()
+                {
+                    MaxCacheBytes = Math.Max(0, options.CacheSizeLimitInBytes),
+                    MinCleanupBytes = Math.Max(0, options.MinCleanupBytes),
+                    MinAgeToDelete = options.MinAgeToDelete.Ticks > 0 ? options.MinAgeToDelete : TimeSpan.Zero,
+                }
+            };
+            var database = new MetaStore(new MetaStoreOptions(options.DiskCacheDirectory)
+            {
+                Shards = Math.Max(1, options.DatabaseShards),
+                MaxLogFilesPerShard = 3,
+            }, cacheOptions, null);
+            cache = new Imazen.HybridCache.HybridCache(database, cacheOptions, logger);
+        }
+
+        public IEnumerable<IIssue> GetIssues()
+        {
+            return cache.GetIssues();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return cache.StartAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return cache.StopAsync(cancellationToken);
+        }
+
+        public Task<IStreamCacheResult> GetOrCreateBytes(byte[] key, AsyncBytesResult dataProviderCallback, CancellationToken cancellationToken,
+            bool retrieveContentType)
+        {
+            return cache.GetOrCreateBytes(key, dataProviderCallback, cancellationToken, retrieveContentType);
         }
     }
 }
