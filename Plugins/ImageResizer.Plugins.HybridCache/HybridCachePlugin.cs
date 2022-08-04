80b270ceb81b2bf430bd15f6257166a0d7c25114
Lilith River <lilith@imazen.io>
2022-08-04 15:50:39 -0600

    Add HybridCachePlugin stub

---
+++ Plugins/ImageResizer.Plugins.HybridCache/HybridCachePlugin.cs
@@ -0,0 +1,33 @@
+﻿using ImageResizer.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ImageResizer.Plugins.HybridCache
+{
+    public class HybridCachePlugin : IAsyncTyrantCache, IPlugin
+    {
+        public bool CanProcess(HttpContext current, IAsyncResponsePlan e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPlugin Install(Config c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ProcessAsync(HttpContext current, IAsyncResponsePlan e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Uninstall(Config c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
