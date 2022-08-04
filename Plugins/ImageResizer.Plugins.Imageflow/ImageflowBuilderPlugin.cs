3892ae3d5ff55e91e7b79dcecadbb7421642171c
Lilith River <lilith@imazen.io>
2022-08-04 17:29:47 -0600

    Stub ImageflowBuilderPlugin

--- Plugins/ImageResizer.Plugins.Imageflow/ImageflowBuilderPlugin.cs
+++ Plugins/ImageResizer.Plugins.Imageflow/ImageflowBuilderPlugin.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Specialized;
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
 using ImageResizer.Configuration;
 using ImageResizer.Configuration.Issues;
 using ImageResizer.ExtensionMethods;
@@ -30,6 +31,14 @@ namespace ImageResizer.Plugins.Imageflow
         }
 
 
-        
+        public IEnumerable<IIssue> GetIssues()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<string> GetSupportedFileExtensions()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
