21dfb290b7dae67b296100f1f936cf6542d9634a
Lilith River <lilith@imazen.io>
2022-08-04 20:04:43 -0600

    whitespace

--- Core/ExtensionMethods/NameValueCollection.cs
+++ Core/ExtensionMethods/NameValueCollection.cs
@@ -2,7 +2,7 @@
 // No part of this project, including this file, may be copied, modified,
 // propagated, or distributed except as permitted in COPYRIGHT.txt.
 // Licensed under the Apache License, Version 2.0.
-﻿using System;
+ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Collections.Specialized;
