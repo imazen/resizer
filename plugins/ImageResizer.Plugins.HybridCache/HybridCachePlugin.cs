caacccc5dbfc4f4e0753645098bf3fd7ec71a679
Lilith River <lilith@imazen.io>
2022-08-25 10:03:58 -0600

    Update HybridCachePlugin.cs

--- plugins/ImageResizer.Plugins.HybridCache/HybridCachePlugin.cs
+++ plugins/ImageResizer.Plugins.HybridCache/HybridCachePlugin.cs
@@ -25,14 +25,14 @@ namespace ImageResizer.Plugins.HybridCache
 
         public HybridCachePlugin(HybridCacheOptions options, ILogger logger)
         {
-            cacheOptions = options;
-            this.logger = logger;
+            _cacheOptions = options;
+            this._logger = logger;
         }
 
         private Config _c;
-        private ILogger logger;
-        private Imazen.HybridCache.HybridCache cache;
-        private HybridCacheOptions cacheOptions = new HybridCacheOptions(null);
+        private readonly ILogger _logger;
+        private Imazen.HybridCache.HybridCache _cache;
+        private readonly HybridCacheOptions _cacheOptions = new HybridCacheOptions(null);
 
 
         private string GetLegacyDiskCachePhysicalPath(Config config)
@@ -46,11 +46,11 @@ namespace ImageResizer.Plugins.HybridCache
         
         private void LoadSettings(Config c)
         {
-            cacheOptions.DiskCacheDirectory = c.get("hybridCache.cacheLocation", cacheOptions.DiskCacheDirectory);
-            cacheOptions.CacheSizeLimitInBytes = c.get("hybridCache.cacheMaxSizeBytes", cacheOptions.CacheSizeLimitInBytes);
-            cacheOptions.DatabaseShards = c.get("hybridCache.shardCount", cacheOptions.DatabaseShards);
-            cacheOptions.QueueSizeLimitInBytes = c.get("hybridCache.writeQueueLimitBytes", cacheOptions.QueueSizeLimitInBytes);
-            cacheOptions.MinCleanupBytes = c.get("hybridCache.minCleanupBytes", cacheOptions.MinCleanupBytes);
+            _cacheOptions.DiskCacheDirectory = c.get("hybridCache.cacheLocation", _cacheOptions.DiskCacheDirectory);
+            _cacheOptions.CacheSizeLimitInBytes = c.get("hybridCache.cacheMaxSizeBytes", _cacheOptions.CacheSizeLimitInBytes);
+            _cacheOptions.DatabaseShards = c.get("hybridCache.shardCount", _cacheOptions.DatabaseShards);
+            _cacheOptions.QueueSizeLimitInBytes = c.get("hybridCache.writeQueueLimitBytes", _cacheOptions.QueueSizeLimitInBytes);
+            _cacheOptions.MinCleanupBytes = c.get("hybridCache.minCleanupBytes", _cacheOptions.MinCleanupBytes);
 
             var legacyCache = GetLegacyDiskCachePhysicalPath(c);
             if (Directory.Exists(legacyCache)) throw new ApplicationException("Legacy disk cache directory found at " + legacyCache + ". Please delete this directory (to prevent unauthorized access) and restart the application.");
@@ -133,14 +133,14 @@ namespace ImageResizer.Plugins.HybridCache
         {
             _c = c;
             LoadSettings(c);
-            cacheOptions.DiskCacheDirectory = ResolveCacheLocation(cacheOptions.DiskCacheDirectory);
+            _cacheOptions.DiskCacheDirectory = ResolveCacheLocation(_cacheOptions.DiskCacheDirectory);
 
 
 
             //OK, we have our cacheOptions;
-            this.cache = CreateHybridCacheFromOptions(cacheOptions, logger);
+            this._cache = CreateHybridCacheFromOptions(_cacheOptions, _logger);
             c.Plugins.add_plugin(this);
-            AsyncUtil.RunSync(() => this.cache.StartAsync(CancellationToken.None));
+            AsyncUtil.RunSync(() => this._cache.StartAsync(CancellationToken.None));
             return this;
         }
 
@@ -164,7 +164,7 @@ namespace ImageResizer.Plugins.HybridCache
             //TODO: Otherwise use GetOrCreateBytes
 
             // And respond using the stream
-            await new NewModuleHelpers().ProcessWithStreamCache(logger, this.cache, current, plan);
+            await new NewModuleHelpers().ProcessWithStreamCache(_logger, this._cache, current, plan);
           
         }
 
@@ -200,24 +200,24 @@ namespace ImageResizer.Plugins.HybridCache
 
         public IEnumerable<IIssue> GetIssues()
         {
-            return cache.GetIssues();
+            return _cache.GetIssues();
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            return cache.StartAsync(cancellationToken);
+            return _cache.StartAsync(cancellationToken);
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            return cache.StopAsync(cancellationToken);
+            return _cache.StopAsync(cancellationToken);
         }
 
         public Task<IStreamCacheResult> GetOrCreateBytes(byte[] key, AsyncBytesResult dataProviderCallback,
             CancellationToken cancellationToken,
             bool retrieveContentType)
         {
-            return cache.GetOrCreateBytes(key, dataProviderCallback, cancellationToken, retrieveContentType);
+            return _cache.GetOrCreateBytes(key, dataProviderCallback, cancellationToken, retrieveContentType);
         }
     }
 }
