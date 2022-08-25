717967f41d6289c88b7f2c442a67bb502632016a
Lilith River <lilith@imazen.io>
2022-08-25 10:40:49 -0600

    Add duplicate instance detection/warning for HybridCachePlugin

--- plugins/ImageResizer.Plugins.HybridCache/HybridCachePlugin.cs
+++ plugins/ImageResizer.Plugins.HybridCache/HybridCachePlugin.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Web.Hosting;
 using ImageResizer.Configuration;
 using ImageResizer.Util;
 using Imazen.Common.Extensibility.StreamCache;
+using Imazen.Common.Instrumentation.Support.InfoAccumulators;
 using Imazen.Common.Issues;
 using Imazen.HybridCache;
 using Imazen.HybridCache.MetaStore;
@@ -16,7 +18,7 @@ using Microsoft.Extensions.Logging;
 
 namespace ImageResizer.Plugins.HybridCache
 {
-    public class HybridCachePlugin : IAsyncTyrantCache, IPlugin
+    public class HybridCachePlugin : IAsyncTyrantCache, IPlugin, IPluginInfo, IPluginRequiresShutdown
     {
         //TODO: Fix multiple instances issue (each HttpModule can have many instances)
         public HybridCachePlugin()
@@ -35,7 +37,7 @@ namespace ImageResizer.Plugins.HybridCache
         private readonly HybridCacheOptions _cacheOptions = new HybridCacheOptions(null);
 
 
-        private string GetLegacyDiskCachePhysicalPath(Config config)
+        private static string GetLegacyDiskCachePhysicalPath(Config config)
         {
             var defaultDir = (HostingEnvironment.ApplicationVirtualPath?.TrimEnd('/') ?? "") + "/imagecache";
             var dir = config.get("diskcache.dir", defaultDir);
@@ -43,7 +45,10 @@ namespace ImageResizer.Plugins.HybridCache
             return !string.IsNullOrEmpty(dir) ? HostingEnvironment.MapPath(dir) : null;
         }
         
-        
+        private static readonly ConcurrentDictionary<string, HybridCachePlugin> firstInstancePerPath = new ConcurrentDictionary<string, HybridCachePlugin>();
+
+        private static bool conflictsExist = false;
+
         private void LoadSettings(Config c)
         {
             _cacheOptions.DiskCacheDirectory = c.get("hybridCache.cacheLocation", _cacheOptions.DiskCacheDirectory);
@@ -52,8 +57,14 @@ namespace ImageResizer.Plugins.HybridCache
             _cacheOptions.QueueSizeLimitInBytes = c.get("hybridCache.writeQueueLimitBytes", _cacheOptions.QueueSizeLimitInBytes);
             _cacheOptions.MinCleanupBytes = c.get("hybridCache.minCleanupBytes", _cacheOptions.MinCleanupBytes);
 
+            if (firstInstancePerPath.AddOrUpdate(DiskCacheDirectory, this, (k, v) => v) != this)
+            {
+                conflictsExist = true;
+            }
+
             var legacyCache = GetLegacyDiskCachePhysicalPath(c);
             if (Directory.Exists(legacyCache)) throw new ApplicationException("Legacy disk cache directory found at " + legacyCache + ". Please delete this directory (to prevent unauthorized access) and restart the application.");
+            
         }
 
         private string GetDefaultCacheLocation() {
@@ -198,11 +209,6 @@ namespace ImageResizer.Plugins.HybridCache
             return new Imazen.HybridCache.HybridCache(database, cacheOptions, logger);
         }
 
-        public IEnumerable<IIssue> GetIssues()
-        {
-            return _cache.GetIssues();
-        }
-
         public Task StartAsync(CancellationToken cancellationToken)
         {
             return _cache.StartAsync(cancellationToken);
@@ -219,5 +225,100 @@ namespace ImageResizer.Plugins.HybridCache
         {
             return _cache.GetOrCreateBytes(key, dataProviderCallback, cancellationToken, retrieveContentType);
         }
+
+        private string DiskCacheDirectory => _cacheOptions?.DiskCacheDirectory;
+
+
+        private bool HasNtfsPermission()
+        {
+            try
+            {
+                if (!Directory.Exists(DiskCacheDirectory)) Directory.CreateDirectory(DiskCacheDirectory);
+                string testFile = Path.Combine(DiskCacheDirectory, "TestFile.txt");
+                File.WriteAllText(testFile,
+                    "You may delete this file - it is written and deleted just to verify permissions are configured correctly");
+                File.Delete(testFile);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static string GetExecutingUser() {
+            try {
+                return Thread.CurrentPrincipal.Identity.Name;
+            } catch {
+                return "[Unknown - please check App Pool configuration]";
+            }
+        }
+
+        private bool CacheDriveOnNetwork()
+        {
+            var physicalCache = DiskCacheDirectory;
+            if (!string.IsNullOrEmpty(physicalCache))
+            {
+                return physicalCache.StartsWith("\\\\") || GetCacheDrive()?.DriveType == DriveType.Network;
+            }
+            return false;
+        }
+
+        private DriveInfo GetCacheDrive()
+        {
+            try
+            {
+                var drive = string.IsNullOrEmpty(DiskCacheDirectory) ? null : new DriveInfo(Path.GetPathRoot(DiskCacheDirectory));
+                return (drive?.IsReady == true) ? drive : null;
+            }
+            catch { return null; }
+        }
+
+        public IEnumerable<IIssue> GetIssues() {
+            var issues = new List<IIssue>();
+            if (_cache != null) issues.AddRange(_cache.GetIssues());
+            
+            if (this._c.get("diskcache.dir", null) != null)
+                issues.Add(new Issue("HybridCache", "Remove the <diskcache> element from web.config, as well as the plugin package", IssueSeverity.ConfigurationError));
+            
+            if (!HasNtfsPermission()) 
+                issues.Add(new Issue("HybridCache", "Not working: Your NTFS Security permissions are preventing the application from writing to the disk cache",
+    "Please give user " + GetExecutingUser() + " read and write access to directory \"" + DiskCacheDirectory + "\" to correct the problem. You can access NTFS security settings by right-clicking the aforementioned folder and choosing Properties, then Security.", IssueSeverity.ConfigurationError));
+            
+            //Warn user about setting hashModifiedDate=false in a web garden.
+            if (_cacheOptions.MinCleanupBytes < 1000 * 1000)
+                issues.Add(new Issue("HybridCache", "minCleanupBytes should not be set below 1 megabyte (1,000,000). Found in the <hybridCache /> element in Web.config.",
+                    "Setting a value too low will waste energy and reduce performance", IssueSeverity.ConfigurationError));
+
+            if (_cacheOptions.CacheSizeLimitInBytes < 1000 * 1000 * 100)
+                issues.Add(new Issue("HybridCache", "cacheMaxSizeBytes should not be set below 100 MiB, 1GiB is the suggested minimum . Found in the <hybridCache /> element in Web.config.",
+                    "Setting a value too low will increase latency, increase cache misses, waste energy and reduce server performance.", IssueSeverity.ConfigurationError));
+
+            if (conflictsExist)
+                issues.Add(new Issue("HybridCache", "More than one instance of HybridCache has been created for the same directory, these instances will fight.", IssueSeverity.ConfigurationError));
+
+
+            if (CacheDriveOnNetwork())
+                issues.Add(new Issue("HybridCache", "It appears that the cache directory is located on a network drive.",
+                    "Network drives often have unacceptable latency for response caching; please test yours.", IssueSeverity.Warning));
+                    
+            return issues;
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> GetInfoPairs()
+        {
+            var list = new List<KeyValuePair<string, string>>();
+            list.Add(new KeyValuePair<string, string>("hybridCache_" + "cacheMaxSizeBytes", _cacheOptions.CacheSizeLimitInBytes.ToString()));
+            list.Add(new KeyValuePair<string, string>("hybridCache_" + "writeQueueLimitBytes", _cacheOptions.QueueSizeLimitInBytes.ToString()));
+            list.Add(new KeyValuePair<string, string>("hybridCache_" + "minCleanupBytes", _cacheOptions.MinCleanupBytes.ToString()));
+            list.Add(new KeyValuePair<string, string>("hybridCache_" + "shardCount", _cacheOptions.DatabaseShards.ToString()));
+            list.Add(new KeyValuePair<string, string>("hybridCache_network_drive", CacheDriveOnNetwork() ? "1" : "0"));
+            list.Add(new KeyValuePair<string, string>("hybridCache_filesystem", GetCacheDrive()?.DriveFormat ?? ""));
+            list.Add(new KeyValuePair<string, string>("hybridCache_drive_avail", GetCacheDrive()?.AvailableFreeSpace.ToString() ?? ""));
+            list.Add(new KeyValuePair<string, string>("hybridCache_drive_total", GetCacheDrive()?.TotalSize.ToString() ?? ""));
+            return list;
+        }
+
+
     }
 }
