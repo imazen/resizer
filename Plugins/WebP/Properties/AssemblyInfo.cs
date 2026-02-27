3dec2242900fcc1d7b8de7a5d3a16ce97cd02ce1
Lilith River <lilith@imazen.io>
2026-02-27 16:53:42 -0700

    Remove NativeDependencyManager from WebP plugin
    The WebP plugin already uses Imazen.WebP 11.0.0 and
    Imazen.WebP.NativeRuntime.win-{x86,x64,arm64} 1.6.1 NuGet packages
    for native DLLs. Remove the vestigial Native.xml (which pointed at
    libwebp 0.6.0 on CDN) and [assembly: NativeDependencies] attribute.

--- Plugins/WebP/Properties/AssemblyInfo.cs
+++ Plugins/WebP/Properties/AssemblyInfo.cs
@@ -16,8 +16,4 @@ using ImageResizer.Util;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("85e9c27f-5340-49d6-b1ea-0b614a64772c")]
 
-//Inform NativeDependencyManager where to find the download manifest
-[assembly: NativeDependencies("Native.xml")]
-
-
 [assembly: Edition("R4Elite")]
